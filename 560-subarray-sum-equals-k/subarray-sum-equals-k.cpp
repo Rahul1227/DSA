2b63d0944bd8d1f3e8769da86100778c25acaebd
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-07-18 17:56:40 +0530

    Time: 44 ms (62.64%) | Memory: 47 MB (37.22%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,18 +2,36 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         // brute force approach
-        int result = 0;
-        for(int i=0; i<nums.size(); i++){
-            int currSum = 0;
-            for(int j = i; j<nums.size(); j++){
-                currSum += nums[j];
-                if(currSum == k){
-                    result++;
-                }
+        // int result = 0;
+        // for(int i=0; i<nums.size(); i++){
+        //     int currSum = 0;
+        //     for(int j = i; j<nums.size(); j++){
+        //         currSum += nums[j];
+        //         if(currSum == k){
+        //             result++;
+        //         }
 
+        //     }
+        // }
+        // return result;
+
+
+        // trying the optimal approach
+        int n = nums.size();
+        vector<int> prefix(n+1, 0);
+        unordered_map<int, int> lookUp;
+        lookUp[0] = 1;
+        int answer = 0;
+        for(int i =0; i< nums.size(); i++){
+            prefix[i+1] =  prefix[i] + nums[i];
+            int left = prefix[i+1] - k;
+            if(lookUp.find(left) != lookUp.end()){
+                answer += lookUp[left];
             }
+
+            lookUp[prefix[i+1]]++;
         }
-        return result;
+        return answer;
         
     }
 };
