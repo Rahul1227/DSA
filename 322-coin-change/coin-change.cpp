d93a4ae65ef26eb5b8f247153fa012d2de16ac83
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-08-13 21:57:36 +0530

    Time: 56 ms (17.39%) | Memory: 54 MB (8.96%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,22 @@
 class Solution {
 private:
-    int solve(int n, int amount, vector<int> &coins, vector<vector<int>> &dp) {
-        // If amount is 0, no coins needed
-        if (amount == 0) return 0;
+    int solve (int n, int amount, vector<int> &coins, vector<vector<int>> &dp){
+       
+       if (amount == 0) return 0;
+       if( n==0 ) return INT_MAX/2;
+        
 
-        // If no coins left or amount becomes negative
-        if (n == 0) return INT_MAX - 1;
+        if(dp[n][amount] != -1) return dp[n][amount];
 
-        // Base case: only one coin type left
-        if (n == 1) {
-            if (amount % coins[0] == 0) return amount / coins[0];
-            else return INT_MAX - 1;
+        int noTake = 0 + solve(n-1, amount, coins,dp);
+        int take = INT_MAX -1 ;
+
+        if(amount >= coins[n-1]){
+            take = 1+ solve(n, amount - coins[n-1],coins, dp);
         }
-
-        if (dp[n][amount] != -1) return dp[n][amount];
-
-        // Don't take current coin
-        int noTake = solve(n - 1, amount, coins, dp);
-
-        // Take current coin (stay on same n because unlimited supply)
-        int take = INT_MAX;
-        if (coins[n - 1] <= amount) {
-            take = 1 + solve(n, amount - coins[n - 1], coins, dp);
-        }
+        
+        
+         
 
         return dp[n][amount] = min(take, noTake);
     }
@@ -30,8 +24,10 @@ private:
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
-        int ans = solve(n, amount, coins, dp);
-        return (ans >= INT_MAX - 1) ? -1 : ans; // -1 if impossible
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        int answer = solve(n, amount, coins, dp);
+
+        return answer >= INT_MAX/2 ? -1 : answer;
+        
     }
 };
