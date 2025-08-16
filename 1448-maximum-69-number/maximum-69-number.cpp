cbed268aa6e4bb52b87f26e0f55cf9be3ee52dcb
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-08-16 17:21:34 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (41.82%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        stack<int> st;
-        // pushing the number into the stack
-        while(num){
-            int digit = num % 10;
-            st.push(digit);
-            num = num/10;
-        }
-        bool flag =true;
-        int answer = 0;
-        while(!st.empty()){
-            int topElement = st.top();
-            st.pop();
-            if(topElement == 6 && flag){
-                answer = answer*10 + 9;
-                flag = false;
-            }else {
-                answer = answer * 10 + topElement;
+        
+        // converting number to string and back to integer
+
+        string str = to_string(num);
+        for(auto &c : str){
+            if( c == '6'){
+                c = '9';
+                break;
             }
             
         }
+        return stoi(str);
+        
+        // Stack based solution
 
-        return answer;
+        // stack<int> st;
+        // // pushing the number into the stack
+        // while(num){
+        //     int digit = num % 10;
+        //     st.push(digit);
+        //     num = num/10;
+        // }
+        // bool flag =true;
+        // int answer = 0;
+        // while(!st.empty()){
+        //     int topElement = st.top();
+        //     st.pop();
+        //     if(topElement == 6 && flag){
+        //         answer = answer*10 + 9;
+        //         flag = false;
+        //     }else {
+        //         answer = answer * 10 + topElement;
+        //     }
+            
+        // }
+
+        // return answer;
         
     }
 };
