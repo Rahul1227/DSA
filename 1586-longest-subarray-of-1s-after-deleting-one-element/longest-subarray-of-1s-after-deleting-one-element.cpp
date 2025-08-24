485700b9266242a9548d1e76126c7f70ad78d9a5
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-08-24 19:15:56 +0530

    Time: 4 ms (17.05%) | Memory: 60.1 MB (59.08%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         int n = nums.size();
-        int maxSize = 0;
-        int i = 0, j = 0;
         bool canChange = true;
-
-        while (j < n) {
-            if (nums[j] == 0) {
-                if (canChange) {
+        int i = 0;
+        int j = 0;
+        int maxSize = 0;
+        while(j<n){
+            if(nums[j] == 0){
+                if(canChange){
                     canChange = false;
-                } else {
-                    maxSize = max(maxSize, j - i);
-                    while (nums[i] != 0) {
+                }else{
+                    maxSize = max(maxSize, j-i);
+                    while(nums[i] != 0){
                         i++;
                     }
-                    i++; // skip the zero
+                    i++;
+
+
                 }
             }
             j++;
         }
-
-        maxSize = max(maxSize, j - i); // check last window
-        return maxSize - 1; // must delete exactly one element
+        maxSize = max(maxSize, j-i);
+        return maxSize-1;
+        
     }
 };
