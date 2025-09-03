f038d54e427fe3cb252e3509db620dfdbe5c6ac5
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-09-03 11:27:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (39.15%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
-        unordered_map<int,int> mp;
+    vector<int> luckyNumbers(vector<vector<int>>& matrix) {
+        map<int, int> mp;
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> ans;
-       
-        // Store row minima
-        for(int r = 0; r<n; r++){
+
+        // storing the minElements first
+        for(int r=0; r<n; r++){
             int minEle = *min_element(matrix[r].begin(), matrix[r].end());
             mp[minEle] = 1;
         }
 
-        // Check column maxima
-        for(int c = 0; c<m; c++){
-            int maxEle = 0;
-            for(int r =0; r<n; r++){
-                maxEle = max(maxEle, matrix[r][c]);  // corrected indexing
+        //checking for the largest element in the column
+        vector<int> ans;
+        for(int c=0;  c<m; c++){
+            int maxEle =0;
+            for(int r=0; r<n; r++){
+                maxEle = max(maxEle, matrix[r][c]);
             }
+
             if(mp.find(maxEle) != mp.end()){
                 ans.push_back(maxEle);
             }
         }
+
         return ans;
+        
     }
 };
