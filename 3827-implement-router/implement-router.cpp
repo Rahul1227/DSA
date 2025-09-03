b496be2a44ebbe6be3ba1ee69aad9fb857d2dd57
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-09-03 15:40:09 +0530

    Time: 312 ms (49.35%) | Memory: 440.5 MB (39.87%) - LeetSync

--- 3827-implement-router/implement-router.cpp
+++ 3827-implement-router/implement-router.cpp
@@ -1,63 +1,74 @@
-struct Packet {
-    int s, d, t;
-};
-
-struct TripleHash {
-    size_t operator()(const tuple<int,int,int>& x) const {
-        auto [a, b, c] = x;
-        return ((size_t)a * 1315423911u) ^ ((size_t)b << 21) ^ ((size_t)c);
-    }
-};
-
 class Router {
-    queue<Packet> q;
-    unordered_set<tuple<int,int,int>, TripleHash> st;
-    unordered_map<int, vector<int>> mp;  // destination -> sorted timestamps
+    queue<vector<int>> q;
+    unordered_set<string> st;
+    unordered_map<int, vector<int>> mp;
     int maxSize;
 
 public:
-    Router(int memoryLimit) {
-        maxSize = memoryLimit;
-    }
-    
-    bool addPacket(int source, int destination, int timestamp) {
-        auto key = make_tuple(source, destination, timestamp);
+    Router(int memoryLimit) { maxSize = memoryLimit; }
 
-        if (st.find(key) != st.end()) return false;
+    bool addPacket(int source, int destination, int timestamp) {
+        string str = to_string(source) + "S" + to_string(destination) + "D" +
+                     to_string(timestamp) + "T";
+
+        if (st.find(str) != st.end())
+            return false;
 
         if (q.size() == maxSize) {
-            Packet p = q.front(); q.pop();
-            auto oldKey = make_tuple(p.s, p.d, p.t);
-            st.erase(oldKey);
-
-            auto &vec = mp[p.d];
-            auto it = lower_bound(vec.begin(), vec.end(), p.t);
-            if (it != vec.end() && *it == p.t) vec.erase(it);
+            vector<int> popedElement = q.front();
+            q.pop();
+            int s = popedElement[0];
+            int d = popedElement[1];
+            int t = popedElement[2];
+            string str1 =
+                to_string(s) + "S" + to_string(d) + "D" + to_string(t) + "T";
+            st.erase(str1);
+            auto& vec = mp[d];
+            auto it = lower_bound(vec.begin(), vec.end(), t);
+            if (it != vec.end()) {
+                vec.erase(it);
+            }
         }
-
-        st.insert(key);
-        mp[destination].push_back(timestamp); // always increasing
+        st.insert(str);
+        mp[destination].push_back(timestamp);
         q.push({source, destination, timestamp});
         return true;
     }
-    
+
     vector<int> forwardPacket() {
-        if (q.empty()) return {};
-        Packet p = q.front(); q.pop();
-        auto key = make_tuple(p.s, p.d, p.t);
-        st.erase(key);
+        if (q.size() == 0)
+            return {};
+        vector<int> popedElement = q.front();
+        q.pop();
+        int s = popedElement[0];
+        int d = popedElement[1];
+        int t = popedElement[2];
+        string str1 =
+            to_string(s) + "S" + to_string(d) + "D" + to_string(t) + "T";
+        st.erase(str1);
+        auto& vec = mp[d];
+        auto it = lower_bound(vec.begin(), vec.end(), t);
+        if (it != vec.end()) {
+            vec.erase(it);
+        }
 
-        auto &vec = mp[p.d];
-        auto it = lower_bound(vec.begin(), vec.end(), p.t);
-        if (it != vec.end() && *it == p.t) vec.erase(it);
-
-        return {p.s, p.d, p.t};
+        return {s, d, t};
     }
-    
+
     int getCount(int destination, int startTime, int endTime) {
-        auto &vec = mp[destination];
+        auto& vec = mp[destination];
+        // find first element >= startTime
         auto it1 = lower_bound(vec.begin(), vec.end(), startTime);
+        // find first element > endTime
         auto it2 = upper_bound(vec.begin(), vec.end(), endTime);
-        return distance(it1, it2);  // O(log n)
+        return distance(it1, it2); // O(log n)
     }
 };
+
+    /**
+     * Your Router object will be instantiated and called as such:
+     * Router* obj = new Router(memoryLimit);
+     * bool param_1 = obj->addPacket(source,destination,timestamp);
+     * vector<int> param_2 = obj->forwardPacket();
+     * int param_3 = obj->getCount(destination,startTime,endTime);
+     */
