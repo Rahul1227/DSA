4e2c28f42f4dfb13909761dfdcbcb60038e04012
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-09-24 15:54:48 +0530

    Time: 2441 ms (20.30%) | Memory: 34.4 MB (85.55%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // brute force approach
-        // int result = 0;
-        // for(int i=0; i<nums.size(); i++){
-        //     int currSum = 0;
-        //     for(int j = i; j<nums.size(); j++){
-        //         currSum += nums[j];
-        //         if(currSum == k){
-        //             result++;
-        //         }
-
-        //     }
-        // }
-        // return result;
-
-
-        // trying the optimal approach
+        // brute force 
+        // generating all the subarray and then checking
         int n = nums.size();
-        vector<int> prefix(n+1, 0);
-        unordered_map<int, int> lookUp;
-        lookUp[0] = 1;
-        int answer = 0;
-        for(int i =0; i< nums.size(); i++){
-            prefix[i+1] =  prefix[i] + nums[i];
-            int left = prefix[i+1] - k;
-            if(lookUp.find(left) != lookUp.end()){
-                answer += lookUp[left];
-            }
+        int result =0;
 
-            lookUp[prefix[i+1]]++;
+        for(int i =0; i<n; i++){
+            int sum =0;
+            for(int j =i; j<n; j++){
+                sum += nums[j];
+                if(sum == k) result++;
+
+            }
         }
-        return answer;
+        return result;
         
     }
 };
