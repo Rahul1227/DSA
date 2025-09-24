ab438ada7e6ec6f341dbd0d1edf6c07c0e9086f9
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-09-24 11:45:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (40.00%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        // we store all the elements in the hashMap and get the maxf of the elements
+        // and it is valid if len - maxf <= k(changes possible)
+        // updating the maxlen only when the condition is true.
         int n = s.size();
         int hashMap[26];
         int l =0;
