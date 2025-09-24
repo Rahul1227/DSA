9d8dfc869065e3b8a7badbe2851a273854e2d64a
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-09-24 16:13:58 +0530

    Time: 42 ms (66.66%) | Memory: 45.4 MB (55.22%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,16 +3,35 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         // brute force 
         // generating all the subarray and then checking
-        int n = nums.size();
+        // int n = nums.size();
+        // int result =0;
+
+        // for(int i =0; i<n; i++){
+        //     int sum =0;
+        //     for(int j =i; j<n; j++){
+        //         sum += nums[j];
+        //         if(sum == k) result++;
+
+        //     }
+        // }
+        // return result;
+
+
+
+        // optimal way
+        unordered_map<int,int> hashMap;
         int result =0;
-
-        for(int i =0; i<n; i++){
-            int sum =0;
-            for(int j =i; j<n; j++){
-                sum += nums[j];
-                if(sum == k) result++;
-
+        int sum =0;
+        hashMap[0] = 1;
+        for(auto num: nums){
+            sum += num;
+            int rem = sum - k;
+            if(hashMap.find(rem) != hashMap.end()){
+                result += hashMap[rem];
             }
+            hashMap[sum]++;
+
+
         }
         return result;
         
