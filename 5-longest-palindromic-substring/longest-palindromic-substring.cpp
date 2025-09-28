b18c63af2153bd1149d5bbe89e01b6dd419fa2bd
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-09-28 19:19:12 +0530

    Time: 1603 ms (5.03%) | Memory: 292.2 MB (8.72%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,41 +1,42 @@
 class Solution {
 private:
-    bool solve(int i, int j,string &s, vector<vector<int>> &dp){
-        if(i>=j) return true;
+    bool isPalindrome(int i, int j, string &s, vector<vector<int>> &dp){
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(s[i] == s[j]){
-            dp[i][j] = solve(i+1, j-1, s, dp);
-        }else{
-            dp[i][j] = false;
+        while(i<j){
+            if(s[i] == s[j]){
+                i++; 
+                j--;
+            }else{
+                dp[i][j] = false;
+                break;
+            }
         }
+        if(i >= j) dp[i][j] = true;
         return dp[i][j];
-
     }
-
-
-
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        int maxlen = 0;
-        int startPoint = 0;
+        // if(n == 1) return true;
+        vector<vector<int>> dp(n+1, vector<int>(n,-1));
+        int maxlen =0;
+        int startPoint = -1;
 
         for(int i =0; i<n; i++){
-            for(int j =i; j<n; j++){
-                if(solve(i,j,s,dp)){
-                    if(j-i+1 > maxlen){
-                        maxlen = j-i+1;
+            for(int j = i; j<n; j++){
+                if(isPalindrome(i,j,s,dp)){
+                    int currLen = j-i+1;
+                    if(currLen > maxlen){
+                        maxlen = currLen;
                         startPoint = i;
                     }
                 }
             }
         }
-
+        
         return s.substr(startPoint, maxlen);
-
         
     }
 };
