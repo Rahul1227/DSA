a51a94e20ff19aea481194b0e4b28606addab2fb
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-10-03 19:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (97.21%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
     int reverse(int x) {
-        // int sign = (x < 0)? -1 : 1;
-        vector<int> digits;
+        long long ans = 0;
         while(x){
             int rem = x % 10;
             x = x/10;
-            digits.push_back(rem);
-        }
-        
-        long long ans =0;
-        for(int i =0; i<digits.size(); i++){
-            ans = ans * 10 + digits[i];
+            ans = ans * 10 + rem;
             
         }
-        // ans  = ans * sign;
-        
 
         if(ans < INT_MIN || ans > INT_MAX){
-                return 0;
-        }else{
-            return ans;
+            return 0;
         }
 
+        return ans;
+
+
+
+
+
+        
+        // // int sign = (x < 0)? -1 : 1;
+        // vector<int> digits;
+        // while(x){
+        //     int rem = x % 10;
+        //     x = x/10;
+        //     digits.push_back(rem);
+        // }
+        
+        // long long ans =0;
+        // for(int i =0; i<digits.size(); i++){
+        //     ans = ans * 10 + digits[i];
+            
+        // }
+        // // ans  = ans * sign;
+        
+
+        // if(ans < INT_MIN || ans > INT_MAX){
+        //         return 0;
+        // }else{
+        //     return ans;
+        // }
+
+       
       
         
         
