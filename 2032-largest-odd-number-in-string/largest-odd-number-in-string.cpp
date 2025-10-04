ad48ba7302459abe5d0b2510ac8798e7299d2b76
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-10-04 22:23:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (90.23%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -6,16 +6,12 @@ public:
         while(i>=0){
             int lastdigit = num[i] -'0';
             if(lastdigit % 2 != 0){
-                break;
+                return num.substr(0, i+ 1);
             }
             i--;
         }
 
-        if(i < 0){
-            return "";
-        }else{
-            return num.substr(0, i+1);
-        }
+        return "";
         
     }
 };
