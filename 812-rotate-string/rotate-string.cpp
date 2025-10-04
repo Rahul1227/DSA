6a6c61bd1eadce4f550af9b82575f0c4631febc5
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-10-04 23:35:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (36.42%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+       // cancatenating the string with itself will contain all the combination possible.
+
+        int len1 = s.size();
+        int len2 = goal.size();
+        if(len1 != len2) return false;
+        if(len1 == 0) return true;
+        s = s + s;
+        
+        return s.find(goal) != -1;
+        
+    }
+};
