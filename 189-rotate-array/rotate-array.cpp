f7733a04a462d66c78769a950dca88a1bd7eceaf
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-10-19 15:16:14 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (60.60%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> temp;
+        // efficient approach
         int n = nums.size();
-        // if(n==1) return;
         k = k % n;
-        // if(k==n) return;
-        for(int i =0; i< n-k; i++){
-            temp.push_back(nums[i]);
-        }
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() +k, nums.end());
 
-        //placing the second part in the front
-        int ind = 0;
-        for(int i = n-k; i<n; i++){
-            nums[ind++] = nums[i];
-        }
 
-        // now second part
-        for(int i =0; i<temp.size(); i++){
-            nums[ind++] = temp[i];
-        }
+
+
+
+        // vector<int> temp;
+        // int n = nums.size();
+       
+        // k = k % n;
+        
+        // for(int i =0; i< n-k; i++){
+        //     temp.push_back(nums[i]);
+        // }
+
+        // //placing the second part in the front
+        // int ind = 0;
+        // for(int i = n-k; i<n; i++){
+        //     nums[ind++] = nums[i];
+        // }
+
+        // // now second part
+        // for(int i =0; i<temp.size(); i++){
+        //     nums[ind++] = temp[i];
+        // }
         
     }
 };
