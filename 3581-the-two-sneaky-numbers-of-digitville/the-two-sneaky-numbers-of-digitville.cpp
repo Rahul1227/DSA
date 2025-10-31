38a511445038f60efd2618206fcf2c9800770eca
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-10-31 22:23:50 +0530

    Time: 23 ms (9.16%) | Memory: 25.4 MB (75.61%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> hashMap(n,0);
-        for(auto num : nums){
-            hashMap[num]++;
+        int n = nums.size() - 2;
+        int xorResult = 0;
+        // with the elements
+        for(auto num: nums){
+            xorResult = xorResult ^ num;
         }
-        vector<int> ans;
-        for(int i =0; i< hashMap.size(); i++){
-            if(hashMap[i] == 2){
-                ans.push_back(i);
+
+        // with the indices
+        for(int i =0; i<n; i++){
+            xorResult = xorResult ^ i;
+        }
+
+        // now the xorResult contains the xored value of the ans;
+
+        int setBit = xorResult & -xorResult;
+        cout<<setBit;
+
+        int group0 = 0; // for the 0
+        int group1 = 0; // for the 1
+
+        for(auto num : nums){
+            int bit = (num & setBit);
+            cout<<bit;
+            if(bit){
+                group1 = group1 ^ num;
+            }else{
+                group0 = group0 ^ num;
             }
         }
-        return ans;
+
+        // for indices also
+        for(int i =0; i<n; i++){
+            int bit = i & setBit;
+            if(bit){
+                group1 = group1 ^ i;
+            }else{
+                group0 = group0 ^ i;
+            }
+        }
+
+        return {group0, group1};
         
     }
 };
