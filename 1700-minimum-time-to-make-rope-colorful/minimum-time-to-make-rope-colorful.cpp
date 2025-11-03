5fced546dc5236dadecfe79e6054a99f4f7a1ee4
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-11-03 21:22:02 +0530

    Time: 0 ms (100.00%) | Memory: 99.9 MB (35.11%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int n = colors.size();
-        int i =0;
-        int j =1;
-        int result = 0;
-        while(j<n){
-            while(j<n && colors[i] == colors[j]){
-                j++;
-            }
-            if(j-i > 1){
-                vector<int> temp;
-                for(int k =i; k<j; k++){
-                    temp.push_back(neededTime[k]);
+        // more optimized solution
 
-                }
-                // sort(temp.begin(), temp.end());
-                int sum = accumulate(temp.begin(), temp.end(),0);
-                int maxVal = *max_element(temp.begin(), temp.end());
-                sum -= maxVal;
-                result += sum;
-                
+        int result = 0;
+        for(int i =1; i< colors.size(); i++){
+            if(colors[i] == colors[i-1]){
+                result += min(neededTime[i], neededTime[i-1]);
+                neededTime[i] = max(neededTime[i], neededTime[i-1]);
             }
-            i =j;
-            j++;
         }
+
         return result;
+
+
+
+        // int n = colors.size();
+        // int i =0;
+        // int j =1;
+        // int result = 0;
+        // while(j<n){
+        //     while(j<n && colors[i] == colors[j]){
+        //         j++;
+        //     }
+        //     if(j-i > 1){
+        //         vector<int> temp;
+        //         for(int k =i; k<j; k++){
+        //             temp.push_back(neededTime[k]);
+
+        //         }
+        //         // sort(temp.begin(), temp.end());
+        //         int sum = accumulate(temp.begin(), temp.end(),0);
+        //         int maxVal = *max_element(temp.begin(), temp.end());
+        //         sum -= maxVal;
+        //         result += sum;
+                
+        //     }
+        //     i =j;
+        //     j++;
+        // }
+        // return result;
         
     }
 };
