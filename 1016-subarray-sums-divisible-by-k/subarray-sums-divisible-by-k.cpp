76a29da738f6906f20c5633fd06bb53e60b9c4d7
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-11-30 10:38:56 +0530

    Time: 34 ms (7.31%) | Memory: 35.9 MB (14.76%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        // brute force approach
-        // int result = 0;
-        // for(int i =0; i<nums.size(); i++){
-        //     int currSum =0;
-        //     for(int j=i; j<nums.size(); j++){
-        //         currSum += nums[j];
-        //         if(currSum % k == 0){
-        //             result ++;
+        map<int,int> mp;
+        mp[0] = 1;
 
-        //         }
-
-        //     }
-        // }
-        // return result;
-
-        // optimal approach
-
-        int currSum = 0;  // for the prefixSum calculation
-        unordered_map<int, int> lookUp; // map to see the remainders
-        lookUp[0] = 1;
-        int answer = 0; // variable to store the answer
-        for( int i =0; i< nums.size(); i++){
+        int currSum = 0;
+        int ans = 0;
+        for(int i =0; i< nums.size(); i++){
             currSum += nums[i];
-            int remainder = currSum % k;
-            if(remainder < 0){
-                remainder = k + remainder;
+            int rem = currSum % k;
+            if(rem < 0){
+                rem = rem + k;
             }
-            if(lookUp.find(remainder) != lookUp.end()){
-                answer += lookUp[remainder];
-                lookUp[remainder]++;
+            if(mp.find(rem) != mp.end()){
+                ans += mp[rem];
+                mp[rem]++;
             }else{
-                lookUp[remainder] = 1;
+                mp[rem] = 1;
             }
-
         }
-        return answer;
+
+        return ans;
+        
         
     }
 };
