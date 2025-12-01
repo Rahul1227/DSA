27cf2c5cdfb57e3a72dfa1e3df8fee58448d01a3
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-01 18:48:22 +0530

    Time: 3 ms (79.56%) | Memory: 13.9 MB (99.27%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,29 +1,57 @@
 class Solution {
-private:
-    int solve(int ind, int prevInd, vector<vector<int>> &dp, vector<int> &nums){
-        if(ind == nums.size()){
-            return 0;
-        }
+// private:
+//     int solve(int ind, int prevInd, vector<vector<int>> &dp, vector<int> &nums){
+//         if(ind == nums.size()){
+//             return 0;
+//         }
 
-        if(dp[ind][prevInd+1] != -1){
-            return dp[ind][prevInd+1];
-        }
+//         if(dp[ind][prevInd+1] != -1){
+//             return dp[ind][prevInd+1];
+//         }
 
-        // notTake
-        int notTake = solve(ind+1, prevInd, dp, nums);
+//         // notTake
+//         int notTake = solve(ind+1, prevInd, dp, nums);
 
-        int take = 0;
-        if(prevInd == -1 || nums[ind] > nums[prevInd]){
-            take = 1 + solve(ind+1, ind, dp, nums);
-        }
+//         int take = 0;
+//         if(prevInd == -1 || nums[ind] > nums[prevInd]){
+//             take = 1 + solve(ind+1, ind, dp, nums);
+//         }
 
-        return dp[ind][prevInd+1] = max(notTake, take);
-    }
+//         return dp[ind][prevInd+1] = max(notTake, take);
+//     }
 public:
     int lengthOfLIS(vector<int>& nums) {
+        // trying nlogn solution
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1,-1));
-        return solve(0,-1, dp, nums);
+        vector<int> lis;
+
+        for(int i =0; i<n; i++){
+            int lisSize = lis.size();
+            if(lisSize == 0 || lis[lisSize -1] < nums[i] ){
+                lis.push_back(nums[i]);
+            }else{
+                auto it = lower_bound(lis.begin(), lis.end(), nums[i]);
+                int ind = it - lis.begin();
+                lis[ind] = nums[i];
+            }
+        }
+
+        return lis.size();
+
+
+
+
+
+
+
+
+
+
+
+
+        // int n = nums.size();
+        // vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        // return solve(0,-1, dp, nums);
         
     }
 };
