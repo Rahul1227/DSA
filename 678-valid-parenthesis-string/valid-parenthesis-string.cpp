e304517d6b709b399ef0e5b70095883e443ec4f6
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-09 10:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (8.46%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,42 +1,42 @@
 class Solution {
 private:
-    // bool solve(string &s, int ind, int count){
-    //     if (ind == s.size()) return count == 0;
-
-    //     if (s[ind] == '(') {
-    //         return solve(s, ind+1, count+1);
-    //     }
-    //     else if (s[ind] == ')') {
-    //         if (count == 0) return false;
-    //         return solve(s, ind+1, count-1);
-    //     }
-    //     else { // '*'
-    //         bool open = solve(s, ind+1, count+1);           // treat '*' as '('
-    //         bool close = (count > 0) && solve(s, ind+1, count-1); // treat '*' as ')'
-    //         bool empty = solve(s, ind+1, count);            // treat '*' as empty
-    //         return open || close || empty;
-    //     }
-    // }
-public:
-    bool checkValidString(string s) {
-        int min = 0;
-        int max = 0;
-        for(auto c : s ){
-            if(c == '('){
-                min++;
-                max++;
-            }else if (c == ')'){
-                if(min > 0) min--;
-                max--;
-
-            }else{
-                if(min > 0) min --;
-                max++;
-            }
-             if(max < 0) return false;
+    bool solve(int ind, int count, vector<vector<int>> &dp, string &s){
+        //base case
+        if(count < 0){
+            return false;
         }
 
-       
-        return min == 0;
+
+        if(ind == s.size()){
+            return count == 0;
+        }
+
+        if(dp[ind][count] != -1){
+            return dp[ind][count];
+        }
+
+        bool isValid = false;
+
+        if(s[ind] == '('){
+            isValid =  solve(ind+1, count+1, dp, s);
+        }else if(s[ind] == ')'){
+            isValid = solve(ind+1, count -1, dp, s);
+        }else{
+            bool considerOpen =  solve(ind+1, count+1, dp, s);
+            bool considerClose =  solve(ind+1, count-1, dp, s);
+            bool considerNothing =  solve(ind+1, count, dp, s);
+
+            isValid = considerOpen || considerClose || considerNothing;
+        }
+
+        return dp[ind][count] = isValid;
+
+    }
+public:
+    bool checkValidString(string s) {
+        int  n = s.size();
+        vector<vector<int>> dp(n,  vector<int>(n+1, -1));
+        return solve(0, 0, dp, s);
+        
     }
 };
