954ee9d8b27370644101983fd1f78ff0684d529c
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-10 21:44:39 +0530

    Time: 203 ms (33.75%) | Memory: 13.9 MB (57.84%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,28 +1,34 @@
 class Solution {
 private:
-    int solve(int l, int r, vector<int> &arr, vector<vector<int>> &dp){
-        if(l>r) return 0;
+    int solve(int i, int j, vector<vector<int>>  &dp, vector<int> &nums){
+        //base case
+        if(i > j) return 0;
 
-        if(dp[l][r] != -1) return dp[l][r];
-        int result = INT_MIN;
+        // checking if the answer is already computed
+        if(dp[i][j] != -1) return dp[i][j];
 
-        for(int ind = l; ind <= r;  ind++){
-            int cost = (arr[ind] * arr[l-1] * arr[r+1]) 
-                         + solve(l, ind-1,arr,dp) 
-                         + solve(ind+1, r, arr, dp);
+        // for making the subproblems independent, we will be starting from the bottom
+        int maxCoins = INT_MIN;
+        for(int ind = i; ind <= j; ind++){
+            int currCoin = nums[i-1] * nums[ind] * nums[j+1]
+                            + solve(i, ind-1, dp, nums)
+                            + solve(ind+1, j, dp , nums);
 
-            result = max(result, cost);
+            maxCoins = max(maxCoins,currCoin);
         }
-
-        return dp[l][r] = result;
+        return dp[i][j] = maxCoins;
     }
 public:
     int maxCoins(vector<int>& nums) {
-        nums.push_back(1);
-        nums.insert(nums.begin(),1);
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return solve(1, n-2, nums, dp);
+        vector<int> newNums(n+2,1);
+        for(int i = 1; i<=n; i++){
+            newNums[i] = nums[i-1];
+        }
+
+
+        vector<vector<int>> dp(n+2, vector<int>(n+2, -1));
+        return solve(1,n, dp, newNums);
         
     }
 };
