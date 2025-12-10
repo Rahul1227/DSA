4141b95af19391b419cad9ef7355ea3a679f2b10
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-10 19:09:26 +0530

    Time: 39 ms (30.66%) | Memory: 13.8 MB (76.56%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,31 +1,32 @@
 class Solution {
 private:
-    int solve(int l,int r, vector<int>&cuts, vector<vector<int>> &dp){
-        if(r-l < 2) return 0; // 1 or less length, the stick cannot be broken
+    int solve(int i, int j, vector<vector<int>> &dp, vector<int> &cuts){
+        if(i > j) return 0;
 
-        if(dp[l][r] != -1) return dp[l][r];
+        if(dp[i][j] != -1) return dp[i][j];
 
-        int result = INT_MAX;
-
-        // first and last elements are not the cuts but we added them for our convinence
-        for(int ind = l+1; ind <= r-1; ind++){
-            int points = cuts[r] - cuts[l] + solve(l, ind,  cuts, dp) + solve(ind, r, cuts, dp);
-
-            result = min(result, points);
-
+        int minCost = INT_MAX;
+        for(int ind = i; ind <=j; ind++){
+            int currCost  = cuts[j+1] - cuts[i-1] 
+                            + solve(i, ind-1, dp, cuts)
+                            + solve(ind+1, j, dp, cuts);
+            
+            minCost = min(minCost, currCost);
         }
 
-        return dp[l][r] = result;
+        return dp[i][j] = minCost;
     }
 public:
     int minCost(int n, vector<int>& cuts) {
-        // sorting to that the values can be solved independently
+        cuts.push_back(0);
+        cuts.push_back(n);
         sort(cuts.begin(), cuts.end());
-        cuts.insert(cuts.begin(),0); // inserting 0 at the beginingg for the length calculation
-        cuts.push_back(n); // same here, inserting n at the last
-        int m = cuts.size();
-        vector<vector<int>> dp(m+1, vector<int>(m+1, -1)); // creating 2d dp of m+1 size
-        return solve(0,m-1, cuts, dp);
+
+        int len = cuts.size();
+        vector<vector<int>> dp(len, vector<int>(len, -1));
+
+        return solve(1, len-2, dp, cuts);
+        
         
     }
 };
