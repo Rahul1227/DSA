5723453494da7b7665867c9c2c7651a3bc21be7a
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-11 19:56:25 +0530

    Time: 28 ms (36.62%) | Memory: 89.5 MB (11.27%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,63 @@
+class Solution {
+private:
+    vector<int> nextSmallerElement(vector<int> &heights){
+        int n = heights.size();
+        vector<int> nse(n,n);
+
+        stack<int> st;
+        st.push(0);
+
+        for(int i =1; i<n; i++){
+            
+            while(!st.empty() && heights[i] < heights[st.top()]){
+                nse[st.top()] = i;
+                st.pop();
+                
+            }
+            st.push(i);
+        }
+
+        return nse;
+    }
+
+    vector<int> prevSmallerElement(vector<int> &heights){
+        int n = heights.size();
+        vector<int> pse(n, -1);
+
+        stack<int> st;
+        st.push(n-1);
+
+        for(int i = n-2;  i>=0; i--){
+            while(!st.empty() && heights[i] < heights[st.top()]){
+                pse[st.top()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        return pse;
+    }
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int> nse = nextSmallerElement(heights);
+        vector<int> pse = prevSmallerElement(heights);
+        // for(auto it: nse){
+        //     cout<<it<<" ";
+        // }
+
+        // for(auto it: pse){
+        //     cout<<it<<" ";
+        // }
+
+        int maxArea = INT_MIN;
+
+        for(int i =0; i< heights.size(); i++){
+            int currArea = (nse[i] - pse[i] - 1) * heights[i];
+            maxArea = max(currArea, maxArea);
+        }
+
+
+        return maxArea;
+        
+    }
+};
