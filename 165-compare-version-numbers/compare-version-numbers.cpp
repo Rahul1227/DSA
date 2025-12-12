6ffe3e2f710eef1e83ac7a83950a50d997ddfe96
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-12 21:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (7.64%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,71 +1,33 @@
 class Solution {
+private:   
+    vector<int> getIntergerFromToken(string &version){
+        vector<int> ans;
+        stringstream v1Token(version);
+        string num;
+
+        while(getline(v1Token, num, '.')){
+            ans.push_back(stoi(num));
+        }
+
+        return ans;
+
+    }
 public:
     int compareVersion(string version1, string version2) {
-        vector<int> arr1;
-        vector<int> arr2;
-        int num1=0;
-        int num2=0;
+        vector<int> v1 = getIntergerFromToken(version1);
+        vector<int> v2 = getIntergerFromToken(version2);
+
+        int len =  max((int)v1.size(), (int)v2.size());
+
+        for(int i =0;  i<len; i++){
+            int num1 = (i<v1.size()) ? v1[i]: 0;
+            int num2 = (i<v2.size()) ? v2[i]:0;
+            if(num1 <num2) return -1;
+            if(num1 > num2) return 1;
+        }
+
         
-        for(auto c : version1){
-            if(c == '.'){
-                arr1.push_back(num1);
-                num1 = 0;
-
-            }else{
-                num1 = num1 * 10 + (c - '0');
-
-            }
-        }
-        arr1.push_back(num1);
-        
-        for(auto c : version2){
-            if(c == '.'){
-                arr2.push_back(num2);
-                num2 = 0;
-
-            }else{
-                num2 = num2 * 10 + (c - '0');
-
-            }
-        }
-        arr2.push_back(num2);
-
-        int result = 0;
-
-        int len1 = arr1.size();
-        int len2 = arr2.size();
-
-        int len = min(len1, len2);
-
-        for(int i =0; i<len; i++){
-            if(arr1[i] < arr2[i]){
-                result =-1;
-                return result;
-
-            } 
-            if (arr1[i] > arr2[i]){
-                 result = 1;
-
-                return result;
-
-            }
-        }
-
-        if( len1 > len2){
-            int sum = accumulate(arr1.begin()+len2, arr1.end(),0);
-            if(sum > 0)  result = 1;
-            return result;
-        }
-
-        if( len2 > len1){
-            int sum = accumulate(arr2.begin()+ len1, arr2.end(),0);
-            if(sum > 0) result = -1;
-            return result;
-        }
-
-
-        return result;
-
+        return 0;
         
     }
 };
