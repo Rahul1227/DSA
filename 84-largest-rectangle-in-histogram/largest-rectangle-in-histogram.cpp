61250ce599c4f3568c474e4c9e0424606aff7837
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-12 10:56:21 +0530

    Time: 18 ms (75.14%) | Memory: 81.3 MB (69.97%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -39,25 +39,69 @@ private:
     }
 public:
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> nse = nextSmallerElement(heights);
-        vector<int> pse = prevSmallerElement(heights);
-        // for(auto it: nse){
-        //     cout<<it<<" ";
-        // }
-
-        // for(auto it: pse){
-        //     cout<<it<<" ";
-        // }
-
+        // trying to solve in 1 pass;
         int maxArea = INT_MIN;
+        int n = heights.size();
+        stack<int> st;
+        st.push(0);
 
-        for(int i =0; i< heights.size(); i++){
-            int currArea = (nse[i] - pse[i] - 1) * heights[i];
-            maxArea = max(currArea, maxArea);
+        for(int i =1 ; i<n; i++){
+            while(!st.empty() && heights[i] < heights[st.top()]){
+                int currHeight = heights[st.top()];
+                st.pop();
+                int pse = !st.empty() ? st.top() : -1;
+                int nse = i;
+                int currArea = (nse - pse - 1) * currHeight;
+                maxArea = max(currArea, maxArea);
+            }
+            st.push(i);
         }
 
-
+        while(!st.empty()){
+                int currHeight = heights[st.top()];
+                st.pop();
+                int pse = !st.empty() ? st.top() : -1;
+                int nse = n;
+                int currArea = (nse - pse - 1) * currHeight;
+                maxArea = max(currArea, maxArea);
+        }
         return maxArea;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // vector<int> nse = nextSmallerElement(heights);
+        // vector<int> pse = prevSmallerElement(heights);
+        // // for(auto it: nse){
+        // //     cout<<it<<" ";
+        // // }
+
+        // // for(auto it: pse){
+        // //     cout<<it<<" ";
+        // // }
+
+        // int maxArea = INT_MIN;
+
+        // for(int i =0; i< heights.size(); i++){
+        //     int currArea = (nse[i] - pse[i] - 1) * heights[i];
+        //     maxArea = max(currArea, maxArea);
+        // }
+
+
+        // return maxArea;
         
     }
 };
