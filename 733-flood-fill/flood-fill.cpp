f0624f94b6c79571ddfa765d641ed70321e0c460
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-15 20:17:04 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (14.99%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,72 +1,37 @@
 class Solution {
-// private:
-//     void dfs(int i, int j, vector<vector<int>> &image, int color, int originalColor){
-//         int n = image.size();
-//         int m = image[0].size();
-//         // if(i>n)
-
-//         image[i][j] = color;
-//         int dirX[] = {0,0,1,-1};
-//         int dirY[] = {1,-1,0,0};
-
-//         for(int k =0; k < 4; k++){
-//             int newX = i + dirX[k];
-//             int newY = j + dirY[k];
-//             if(newX >= 0 && newX < n && newY >=0 && newY < m && image[newX][newY] == originalColor){
-//                 dfs(newX, newY, image, color, originalColor);
-//             }
-
-//         }
-
-//     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        // //-------------- trying the dfs solution-------------
-        
-
-        // if(image[sr][sc] == color) return image;
-
-        // int originalColor = image[sr][sc];
-        // dfs(sr,sc,image, color, originalColor);
-        // return image;
-
-
-
-
-
-
-
-
         int n = image.size();
         int m = image[0].size();
-        
-        queue<pair<int,int>> q;
-        int dirX[] = {0,0,1,-1};
-        int dirY[] = {1,-1,0,0};
-        q.push({sr,sc});
-        int originalColor = image[sr][sc];
-        if(originalColor == color) return image;
-        image[sr][sc] = color;
-        while(!q.empty()){
-            auto[r,c] = q.front();
-            q.pop();
-            for(int i =0; i<4; i++){
-                int newX = r + dirX[i];
-                int newY = c + dirY[i];
 
-                // checking the validity;
-                if(newX >=0 && newX < n && newY >=0 && newY < m && image[newX][newY] == originalColor){
-                    image[newX][newY] = color;
-                    q.push({newX, newY});
+        int originalColor = image[sr][sc];
+        vector<vector<int>> visited(n, vector<int>(m,0));
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        visited[sr][sc] = 1;
+        image[sr][sc] = color;
+        int dirX[] = {-1,1,0,0};
+        int dirY[] ={0,0,1,-1};
+        while(!q.empty()){
+            int len = q.size();
+            for(int i=0; i<len; i++){
+
+                auto [row,col] = q.front();
+                q.pop();
+
+                for(int ind =0; ind<4; ind++){
+                    int newR = row + dirX[ind];
+                    int newC = col + dirY[ind];
+
+                    if(newR >=0 && newR < n && newC >=0 && newC <m && !visited[newR][newC] && image[newR][newC] == originalColor){
+                        q.push({newR, newC});
+                        image[newR][newC] = color;
+                        visited[newR][newC] = 1;
+                    }
                 }
+
             }
         }
-
         return image;
-
-
-       
-        
-        
     }
 };
