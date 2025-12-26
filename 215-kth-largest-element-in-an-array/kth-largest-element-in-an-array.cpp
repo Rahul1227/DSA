f10918382a4aa297767b06fdfc7d5283eb04a7bf
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-26 12:00:57 +0530

    Time: 37 ms (38.25%) | Memory: 59.3 MB (85.54%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,15 +1,46 @@
 class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        for (int i = 0; i < nums.size(); i++) {
+private:
+    int quickSort(int left, int right, vector<int>& nums) {
+        int partitionValue = nums[left];
+        int i = left;
+        int j = right;
+        while (i <= j) {
+            if (nums[i] < partitionValue && nums[j] > partitionValue ) {
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }
 
-            minHeap.push(nums[i]);
+            if (nums[i] >=  partitionValue) {
+                i++;
+            }
 
-            if ((int)minHeap.size() > k) {
-                minHeap.pop();
+            if (nums[j] <= partitionValue) {
+                j--;
             }
         }
-        return minHeap.top();
+
+        swap(nums[left], nums[j]);
+        return j;
+    }
+
+    int solve(int left, int right, vector<int>& nums, int k) {
+        if (left == right)
+            return nums[left];
+
+        int partitionInd = quickSort(left, right, nums);
+        if (partitionInd == k - 1) {
+            return nums[partitionInd];
+        } else if (partitionInd > k - 1) {
+            return solve(left, partitionInd - 1, nums, k);
+        } else {
+            return solve(partitionInd + 1, right, nums, k);
+        }
+    }
+
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        return solve(0, n - 1, nums, k);
     }
 };
