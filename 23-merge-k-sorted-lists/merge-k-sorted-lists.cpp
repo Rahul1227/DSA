c7e46f18115136364ec87a48199638be38f27a91
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-26 19:17:06 +0530

    Time: 1 ms (74.15%) | Memory: 18.7 MB (31.68%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -9,42 +9,36 @@
  * };
  */
 class Solution {
-private:
-    ListNode* solve(ListNode* l1, ListNode* l2){
-        ListNode * head = new ListNode(-1);
-        ListNode * temp = head;
-        while(l1 && l2){
-            if(l1->val <= l2->val){
-                temp -> next = l1;
-                temp = l1;
-                l1 = l1->next;
-            }else{
-                temp -> next = l2;
-                temp = l2;
-                l2 = l2->next;
-            }
-        }
-        if(l1) temp -> next = l1;
-        if(l2) temp ->next = l2;
-        return head->next;
-    }
-
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        // trying the merge sort technique
+        ListNode * head = new ListNode (-1);
+        ListNode * temp = head;
         int n = lists.size();
-        if(n == 0){
-            return nullptr;
+        priority_queue<pair<int, ListNode *>, vector<pair<int, ListNode *>>, greater<pair<int, ListNode *>> > minHeap;
+
+        // pushing the head of all the linkedlist into the heap
+        for(int i=0; i<n; i++){
+            if(lists[i]){
+                minHeap.push({lists[i]->val, lists[i]});
+            }
+            
+    }
+
+        // now working on the minHeap
+        while(!minHeap.empty()){
+            auto [val, node] = minHeap.top();
+            minHeap.pop();
+            temp -> next = node;
+            temp = node;
+            if(node->next){
+                node = node ->next;
+                minHeap.push({node->val, node});
+            }
         }
 
-        if(n==1){
-            return lists[0];
-        }
-        ListNode * temp = lists[0];
-        for (int i = 1; i < n; i++) {
-            temp = solve(temp, lists[i]);
-           
-        }
-        return temp;
+        return head->next;
+
+        
+        
     }
 };
