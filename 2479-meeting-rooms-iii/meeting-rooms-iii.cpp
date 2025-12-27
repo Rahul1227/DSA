dfc6f27895c8b72e3d5c6b8dcbcb465aacb48c6a
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-27 17:07:48 +0530

    Time: 67 ms (75.39%) | Memory: 98.2 MB (70.22%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,55 +1,51 @@
 class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        vector<int> usedCount(n,0);
+        vector<int> count(n, 0);
+        priority_queue<int, vector<int>, greater<int>> availableRooms;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>,
+                       greater<pair<long long, int>>>
+            allocatedRooms;
 
-        priority_queue<int, vector<int>, greater<int>> avaliableRooms;
-        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> usedRooms;
+        for (int i = 0; i < n; i++) {
+            availableRooms.push(i);
+        }
 
         sort(meetings.begin(), meetings.end());
 
-        // initially all rooms are free
-        for(int i = 0; i < n; i++){
-            avaliableRooms.push(i);
-        }
+        for(int i =0; i<meetings.size(); i++){
+            int startTime = meetings[i][0];
+            int endTime = meetings[i][1];
 
-        for(int i = 0; i < meetings.size(); i++){
-            long long currStart = meetings[i][0];
-            long long currEnd = meetings[i][1];
-
-            // free rooms that have finished before currStart
-            while(!usedRooms.empty() && usedRooms.top().first <= currStart){
-                auto p = usedRooms.top();
-                usedRooms.pop();
-                avaliableRooms.push(p.second);
+            while(!allocatedRooms.empty() && allocatedRooms.top().first <= startTime){
+                auto [time, room] = allocatedRooms.top();
+                allocatedRooms.pop();
+                availableRooms.push(room);
             }
 
-            if(!avaliableRooms.empty()){
-                int room = avaliableRooms.top();
-                avaliableRooms.pop();
-                usedRooms.push({currEnd, room});
-                usedCount[room]++;
-            } else {
-                auto p = usedRooms.top();
-                usedRooms.pop();
-                long long endT = p.first;
-                int room = p.second;
-
-                long long duration = currEnd - currStart;
-                long long newEnd = endT + duration;
-
-                usedRooms.push({newEnd, room});
-                usedCount[room]++;
+            if(!availableRooms.empty()){
+                int room = availableRooms.top();
+                availableRooms.pop();
+                count[room]++;
+                allocatedRooms.push({endTime, room});
+            }else{
+                int duration = endTime - startTime;
+                auto [endingOn, room] = allocatedRooms.top();
+                allocatedRooms.pop();
+                count[room]++;
+                allocatedRooms.push({endingOn + duration, room});
             }
         }
 
-        int ans = 0;
-        for(int i = 1; i < n; i++){
-            if(usedCount[i] > usedCount[ans]){
-                ans = i;
+        int ans = INT_MIN;
+        int ind = -1;
+        for(int i =0; i<n; i++){
+            if(count[i] > ans){
+                ans = count[i];
+                ind =  i;
             }
         }
-
-        return ans;
+        return ind;
+        
     }
 };
