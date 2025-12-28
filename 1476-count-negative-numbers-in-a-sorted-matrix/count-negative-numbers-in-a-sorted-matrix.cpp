b2f927c9fa199c4555d1b59ae8a04d669906731f
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-28 18:04:26 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (67.95%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -5,24 +5,36 @@ public:
         int m = grid[0].size();
         int totalNegative = 0;
 
-        for(int i =0; i<n; i++){
-            int left = 0;
-            int right = m-1;
-            int currNegativeInd = -1;
-            while(left <= right){
-                int mid = left + ((right -left)/2);
-                if(grid[i][mid] < 0){
-                    currNegativeInd = mid;
-                    right = mid -1;
-                }else{
-                    left = mid + 1;
-                }
-            }
-
-            if(currNegativeInd != -1){
-                totalNegative += m - currNegativeInd;
+        // more optimized way
+        int i = n-1;
+        int j = 0;
+        while(i>=0 && j<m){
+            if(grid[i][j] < 0){
+                totalNegative += m-j;
+                i--;
+            }else{
+                j++;
             }
         }
+
+        // for(int i =0; i<n; i++){
+        //     int left = 0;
+        //     int right = m-1;
+        //     int currNegativeInd = -1;
+        //     while(left <= right){
+        //         int mid = left + ((right -left)/2);
+        //         if(grid[i][mid] < 0){
+        //             currNegativeInd = mid;
+        //             right = mid -1;
+        //         }else{
+        //             left = mid + 1;
+        //         }
+        //     }
+
+        //     if(currNegativeInd != -1){
+        //         totalNegative += m - currNegativeInd;
+        //     }
+        // }
 
         return totalNegative;        
     }
