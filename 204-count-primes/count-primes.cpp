6fa582b8a9294a2ff45d0d63e10f0cebceb673ed
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-29 23:42:15 +0530

    Time: 147 ms (89.25%) | Memory: 132.6 MB (7.68%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        // Sieve of Erathonese
-        // precompute the prime
-        vector<int> primes(n, 1);
-       
+        vector<int> primes(n,1);
 
-        // logic for the precompute
-        for(int i = 2; i*i<n; i++){
+        // nloglogn
+        for(int i =2; i * i < n; i++){
             if(primes[i] == 1){
-                for(int j = i*i; j<n; j = j + i){
+                for(int j = i * i; j<n; j = j + i){
                     primes[j] = 0;
+
                 }
             }
         }
-        int result =0;
 
-        for(int i = 2; i< n; i++){
-            if(primes[i] == 1){
-                result++;
-            }
+        int count = 0;
 
+        for(int i = 2; i<n; i++){
+            if(primes[i] == 1) count++;
         }
 
-        return result;
+        return count;
         
     }
 };
