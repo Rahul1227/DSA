e7fa8811b6defd0bd8a37444229645ebc1c25f25
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2025-12-29 12:07:49 +0530

    Time: 7 ms (6.61%) | Memory: 12.7 MB (8.17%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,31 +1,30 @@
 class Solution {
-public:
-    void calculatePermutation(int n, vector<int> &nums, vector<int> &currArray, vector<vector<int>> &result, unordered_map<int, bool> &used) {
-        if(currArray.size() == n) {
-            result.push_back(currArray);
-            return;
+private:
+    void solve(vector<int> curr, vector<int>& nums, vector<vector<int>>& ans,
+               vector<int> used) {
+        if (curr.size() == nums.size()) {
+            ans.push_back(curr);
         }
 
-        for(int i = 0; i < n; i++) {
-            if(!used[nums[i]]) {
-                currArray.push_back(nums[i]);
-                used[nums[i]] = true;
-
-                calculatePermutation(n, nums, currArray, result, used);
-
-                currArray.pop_back();
-                used[nums[i]] = false;
+        for (int i = 0; i < nums.size(); i++) {
+            if (!used[i]) {
+                curr.push_back(nums[i]);
+                used[i] = 1;
+                solve(curr, nums, ans, used);
+                //back tracking
+                curr.pop_back();
+                used[i] = 0;
             }
         }
     }
 
+public:
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> answer;
-        vector<int> currArray;
-        unordered_map<int, bool> used;
-
-        calculatePermutation(n, nums, currArray, answer, used);
-        return answer;
+        vector<vector<int>> ans;
+        vector<int> used(n, 0);
+        vector<int> curr;
+        solve(curr, nums, ans, used);
+        return ans;
     }
 };
