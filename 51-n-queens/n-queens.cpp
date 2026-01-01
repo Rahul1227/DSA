26c6450a27ad05b0102dccba3586f21039f7554f
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-01 12:23:43 +0530

    Time: 7 ms (18.11%) | Memory: 12 MB (20.82%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,55 +1,67 @@
 class Solution {
+    unordered_set<int> colCheck;
+    unordered_set<int> rightDia;
+    unordered_set<int> leftDia;
+
 private:
-    bool isValid(int row, int col,  vector<string> &board){
+    bool isValid(int row, int col, vector<string>& board) {
         // checking in the upper rows;
         int n = board.size();
-        for(int i = row -1; i >= 0; i--){
-            if(board[i][col] == 'Q') return false;
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q')
+                return false;
         }
 
         // checking in the rightDiagonal;
-        int i = row -1;
+        int i = row - 1;
         int j = col + 1;
-        while(i >= 0  && j<n){
-            if(board[i][j] == 'Q') return false;
+        while (i >= 0 && j < n) {
+            if (board[i][j] == 'Q')
+                return false;
             i--;
             j++;
         }
 
         // checking the leftDiagonal
-        int x = row-1;
-        int y = col -1;
-        while(x >=0 && y>=0){
-            if(board[x][y] == 'Q') return false;
+        int x = row - 1;
+        int y = col - 1;
+        while (x >= 0 && y >= 0) {
+            if (board[x][y] == 'Q')
+                return false;
             x--;
             y--;
         }
 
         return true;
     }
-    void solve(int row, vector<string> &board, vector<vector<string>> & ans){
+    void solve(int row, vector<string>& board, vector<vector<string>>& ans) {
         int n = board.size();
-        if(row >= n){
+        if (row >= n) {
             ans.push_back(board);
             return;
         }
 
-
-        for(int col = 0; col < n; col++){
-            if(isValid(row, col, board)){
+        for (int col = 0; col < n; col++) {
+            if (!colCheck.count(col) && !rightDia.count(row + col) &&
+                !leftDia.count(row - col)) {
+                colCheck.insert(col);
+                rightDia.insert(row+col);
+                leftDia.insert(row-col);
                 board[row][col] = 'Q';
-                solve(row+1, board, ans);
+                solve(row + 1, board, ans);
                 board[row][col] = '.';
+                colCheck.erase(col);
+                rightDia.erase(row+col);
+                leftDia.erase(row-col);
             }
         }
-
     }
+
 public:
     vector<vector<string>> solveNQueens(int n) {
         vector<string> curr(n, string(n, '.'));
         vector<vector<string>> ans;
         solve(0, curr, ans);
         return ans;
-        
     }
 };
