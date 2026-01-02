181d14da46d8489a964328186e514b20ee732f7c
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-02 19:17:13 +0530

    Time: 39 ms (17.07%) | Memory: 54.3 MB (63.49%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -4,27 +4,25 @@ public:
     int numSubseq(vector<int>& nums, int target) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-
-        vector<int> pow2(n+1, 1);
-        for(int i = 1; i <= n; i++){
-            pow2[i] = (pow2[i-1] * 2LL) % MOD;
+        vector<int> power(n+1,1);
+        for(int i =1; i<=n; i++){
+            power[i] = (power[i-1]* 2LL) % MOD;
         }
 
-        long long count = 0;
-
-        for(int i = 0; i < n; i++){
+        int count = 0;
+        for(int i =0; i<n; i++){
             if(nums[i] + nums[i] > target) break;
-
-            int maxVal = target - nums[i];
-            int ansInd = upper_bound(nums.begin()+i, nums.end(), maxVal)
-                         - nums.begin() - 1;
-
-            if(ansInd >= i){
-                count = (count + pow2[ansInd - i]) % MOD;
+            int toSearch = target - nums[i];
+            auto it = upper_bound(nums.begin() + i, nums.end(), toSearch);
+            int ind = (it - nums.begin()) - 1;
+            if(ind >= i){
+                count = (count + power[ind-i]) % MOD;
             }
+            
         }
+
         return count;
+
+        
     }
 };
-
-
