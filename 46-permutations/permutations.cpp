1d63308c0a02ae0fe32526203344ec4f90b5518b
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-04 10:23:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (49.18%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,33 @@
 class Solution {
 private:
-    void solve(vector<int> curr, vector<int>& nums, vector<vector<int>>& ans,
-               vector<int> used) {
-        if (curr.size() == nums.size()) {
+    void solve(vector<int> &curr, vector<int> &used, vector<vector<int>> &ans, vector<int> &nums){
+        //base case
+        if(curr.size() == nums.size()){
             ans.push_back(curr);
+            return;
         }
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (!used[i]) {
-                curr.push_back(nums[i]);
+
+        // exploring all the ways
+        for(int i =0;  i< nums.size(); i++){
+            if(!used[i]){
                 used[i] = 1;
-                solve(curr, nums, ans, used);
-                //back tracking
-                curr.pop_back();
+                curr.push_back(nums[i]);
+                solve(curr, used, ans, nums);
                 used[i] = 0;
+                curr.pop_back();
             }
         }
-    }
 
+    }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> ans;
-        vector<int> used(n, 0);
         vector<int> curr;
-        solve(curr, nums, ans, used);
+        vector<int> used(n, 0);
+        solve(curr,used, ans, nums);
         return ans;
+        
     }
 };
