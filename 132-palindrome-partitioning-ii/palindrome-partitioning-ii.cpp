7542f56774576086a2ec92c2d54115030fcacf46
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-04 15:22:57 +0530

    Time: 1359 ms (33.34%) | Memory: 32.3 MB (17.21%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,11 +1,12 @@
 class Solution {
 private:
-
-    bool isPalindrome(int i, int j, string &s){
-        while(i<j){
-            if(s[i] != s[j]) return false;
-            i++;
-            j--;
+    bool isPalindrome(string &s){
+        int left =0;
+        int right = s.size() - 1;
+        while(left < right){
+            if(s[left] != s[right]) return false;
+            left++;
+            right--;
         }
         return true;
     }
@@ -13,30 +14,32 @@ private:
 
 
 
-    int solve(int ind, vector<int> &dp, string &s){
-        //base condition
-        if(ind == s.size()) return 0;
+    int solve(int ind, string &s, vector<int> &dp){
+        if(ind == s.size()){
+            return 0;
+        }
 
         if(dp[ind] != -1) return dp[ind];
 
-        int minPartition = INT_MAX;
+        string st ="";
+        int ans = INT_MAX;
+        for(int i =ind; i<s.size(); i++){
+            st += s[i];
+            if(isPalindrome(st)){
+                int curr = 1 + solve(i+1, s,dp);
+                ans = min(ans, curr);
 
-        for(int j = ind; j<s.size(); j++){
-            int currPartition = INT_MAX;
-            if(isPalindrome(ind,j,s)){
-                currPartition = 1 + solve(j+1, dp, s);
             }
-
-            minPartition = min(minPartition, currPartition);
+           
         }
-
-        return dp[ind] = minPartition;
+        return dp[ind] = ans;
     }
 public:
     int minCut(string s) {
         int n = s.size();
         vector<int> dp(n, -1);
-        return solve(0, dp, s) - 1;
+        return solve(0, s, dp)-1;
+
         
     }
 };
