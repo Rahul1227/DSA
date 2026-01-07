9201d95f1f3cd16dfb3f2a2b42837bfc31535068
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-07 10:56:41 +0530

    Time: 8 ms (28.64%) | Memory: 81.1 MB (21.60%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -11,21 +11,22 @@
  */
 class Solution {
 private:
-    int getSum(TreeNode * node, unordered_map<TreeNode*,int> &precomputed){
+    int getSum(TreeNode * node, vector<int> &precomputed){
         if(!node) return 0;
 
         int leftSum = getSum(node->left, precomputed);
         int rightSum = getSum(node->right, precomputed);
 
-        return precomputed[node] = leftSum +  rightSum + node->val;
+        precomputed.push_back(leftSum +  rightSum + node->val);
+        return precomputed.back();
     }
 public:
     const int MOD = 1e9+7;
     int maxProduct(TreeNode* root) {
-        unordered_map<TreeNode*, int> precomputed;
+        vector<int> precomputed;
         int totalSum = getSum(root, precomputed);
         long long maxSum = 1;
-        for(auto [node, val] : precomputed){
+        for(auto val : precomputed){
             long long sum1 = val;
             long long sum2 = totalSum - val;
             long long currSum = sum1 * sum2;
