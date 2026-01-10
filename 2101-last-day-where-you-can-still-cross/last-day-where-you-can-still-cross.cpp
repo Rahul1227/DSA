d557bd12437bebec8972f911077a4af55c4fbf71
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-10 23:41:58 +0530

    Time: 34 ms (74.64%) | Memory: 112.7 MB (69.34%) - LeetSync

--- 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.cpp
@@ -1,109 +1,100 @@
-/*
-Approach
-1. Start with all cells as water (day = n)
-2. Go backwards through the days (n → 1)
-3. For each day, "remove water" (turn cell back to land)
-4. Connect this cell with all adjacent land cells using DSU
-5. If cell is in top row → connect to TOP virtual node
-6. If cell is in bottom row → connect to BOTTOM virtual node
-7. Check if TOP and BOTTOM are connected
-8. First time they connect → that's our answer!
-*/
-
-
-class DisjointSet {
+class DisjointSet{
 private:
     vector<int> parent;
     vector<int> size;
-
 public:
-    DisjointSet(int n) {
+    DisjointSet(int n){
         parent.resize(n);
         size.resize(n, 1);
-        for (int i = 0; i < n; i++) {
+        for(int i =0; i<n; i++){
             parent[i] = i;
         }
     }
 
-    int findUltimateParent(int u) {
-        if (parent[u] == u) {
+    int findUltimateParent(int u){
+        if(parent[u] == u){
             return u;
         }
 
         return parent[u] = findUltimateParent(parent[u]);
     }
 
-    void unionBySize(int u, int v) {
-        int ultimateParentU = findUltimateParent(u);
-        int ultimateParentV = findUltimateParent(v);
 
-        if (ultimateParentU == ultimateParentV)
-            return;
 
-        if (size[ultimateParentU] <= size[ultimateParentV]) {
-            parent[ultimateParentU] = ultimateParentV;
-            size[ultimateParentV] += size[ultimateParentU];
+    void unionBySize(int u, int v){
+        int parentU = findUltimateParent(u);
+        int parentV = findUltimateParent(v);
 
-        } else {
-            parent[ultimateParentV] = ultimateParentU;
-            size[ultimateParentU] += size[ultimateParentV];
+        if(parentU == parentV) return;
+
+        if(size[parentU] >= size[parentV]){
+            parent[parentV] = parentU;
+            size[parentU] += size[parentV];
+        }else{
+            parent[parentU] = parentV;
+            size[parentV] += size[parentU];
         }
     }
 
-    bool isConnected(int u, int v) {
-        int ultimateParentU = findUltimateParent(u);
-        int ultimateParentV = findUltimateParent(v);
+    bool isConnected(int u, int v){
+        int parentU = findUltimateParent(u);
+        int parentV = findUltimateParent(v);
 
-        return ultimateParentU == ultimateParentV;
+        return parentU == parentV;
     }
+
 };
 
 class Solution {
 public:
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+        // 1 ==> water
+        // 0 ==> land
+        vector<vector<int>> grid(row+1, vector<int>(col+1,1));
+
+        int total = row * col + 3;
         int TOP = row * col + 1;
         int BOTTOM = row * col + 2;
-        DisjointSet ds(row * col + 3);
 
-        // 1==> water , 0 ==> land
-        vector<vector<int>> grid(row + 1, vector<int>(col + 1, 1));
+        DisjointSet ds(total);
 
         int n = cells.size();
-        for (int days = n - 1; days >= 0; days--) {
-            int r = cells[days][0];
-            int c = cells[days][1];
+        for(int day = n-1; day >=0; day--){
+            int currRow = cells[day][0];
+            int currCol = cells[day][1];
+            grid[currRow][currCol] = 0;
+            int currNode = (currRow -1)* col + currCol;
+            int dirX[] = {-1,1,0,0};
+            int dirY[] = {0,0,1,-1};
+            // searching in all the 4 directioons
+            for(int i =0; i<4; i++){
+                int newRow = currRow + dirX[i];
+                int newCol = currCol + dirY[i];
+                int newNode = (newRow-1) * col + newCol;
+                if(
+                    newRow > 0 && newRow <= row &&
+                    newCol > 0 && newCol <= col &&
+                    grid[newRow][newCol] == 0){
+                        ds.unionBySize(currNode, newNode);
 
-            grid[r][c] = 0;
-            int currCeil = (r - 1) * col + c;
-
-            int dirX[] = {-1, 1, 0, 0};
-            int dirY[] = {0, 0, 1, -1};
-            for (int i = 0; i < 4; i++) {
-                int newR = r + dirX[i];
-                int newC = c + dirY[i];
-
-                if (newR >= 1 && newR <= row && newC >= 1 && newC <= col &&
-                    grid[newR][newC] == 0) {
-                    int newCeil = (newR - 1) * col + newC;
-                    ds.unionBySize(currCeil, newCeil);
                 }
             }
 
-            // connect to the top
-            if (r == 1) {
-                ds.unionBySize(currCeil, TOP);
+            if(currRow == 1){
+                ds.unionBySize(currNode, TOP);
             }
 
-            // connect to the bottom
-            if (r == row) {
-                ds.unionBySize(currCeil, BOTTOM);
+            if(currRow == row){
+                ds.unionBySize(currNode, BOTTOM);
             }
 
-            // check if top and bottom are connect
-            if (ds.isConnected(TOP, BOTTOM)) {
-                return days;
+            if(ds.isConnected(TOP,BOTTOM)){
+                return day;
             }
+
         }
+
         return 0;
+        
     }
 };
