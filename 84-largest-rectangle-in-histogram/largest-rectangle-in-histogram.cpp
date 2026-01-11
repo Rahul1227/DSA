050161ab94885033ac8ee2c3cfbe894e348a5c61
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-11 11:13:56 +0530

    Time: 20 ms (65.61%) | Memory: 81.5 MB (53.49%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,107 +1,35 @@
 class Solution {
-private:
-    vector<int> nextSmallerElement(vector<int> &heights){
-        int n = heights.size();
-        vector<int> nse(n,n);
-
-        stack<int> st;
-        st.push(0);
-
-        for(int i =1; i<n; i++){
-            
-            while(!st.empty() && heights[i] < heights[st.top()]){
-                nse[st.top()] = i;
-                st.pop();
-                
-            }
-            st.push(i);
-        }
-
-        return nse;
-    }
-
-    vector<int> prevSmallerElement(vector<int> &heights){
-        int n = heights.size();
-        vector<int> pse(n, -1);
-
-        stack<int> st;
-        st.push(n-1);
-
-        for(int i = n-2;  i>=0; i--){
-            while(!st.empty() && heights[i] < heights[st.top()]){
-                pse[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        }
-
-        return pse;
-    }
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        // trying to solve in 1 pass;
+    int largestRectangleArea(vector<int>& height) {
+        // if no pse, pse = -1 (out of range)
+        // if no nse, nse = n (out of range)
+        int n = height.size();
         int maxArea = INT_MIN;
-        int n = heights.size();
         stack<int> st;
-        st.push(0);
-
-        for(int i =1 ; i<n; i++){
-            while(!st.empty() && heights[i] < heights[st.top()]){
-                int currHeight = heights[st.top()];
+        for(int i =0; i<n; i++){
+            while(!st.empty() && height[i] < height[st.top()]){
+                int currHeight = height[st.top()];
                 st.pop();
-                int pse = !st.empty() ? st.top() : -1;
+                // for nse and pse, we are considering the indices
+                // indices are pushed into the stack
                 int nse = i;
+                int pse = (!st.empty() ? st.top() : -1);
                 int currArea = (nse - pse - 1) * currHeight;
-                maxArea = max(currArea, maxArea);
+                maxArea = max(maxArea, currArea);
             }
             st.push(i);
         }
 
         while(!st.empty()){
-                int currHeight = heights[st.top()];
-                st.pop();
-                int pse = !st.empty() ? st.top() : -1;
-                int nse = n;
-                int currArea = (nse - pse - 1) * currHeight;
-                maxArea = max(currArea, maxArea);
+            int currHeight = height[st.top()];
+            st.pop();
+            int nse = n;
+            int pse = (!st.empty()? st.top():-1);
+            int currArea = (nse-pse-1) * currHeight;
+            maxArea = max(maxArea, currArea);
         }
+
         return maxArea;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // vector<int> nse = nextSmallerElement(heights);
-        // vector<int> pse = prevSmallerElement(heights);
-        // // for(auto it: nse){
-        // //     cout<<it<<" ";
-        // // }
-
-        // // for(auto it: pse){
-        // //     cout<<it<<" ";
-        // // }
-
-        // int maxArea = INT_MIN;
-
-        // for(int i =0; i< heights.size(); i++){
-        //     int currArea = (nse[i] - pse[i] - 1) * heights[i];
-        //     maxArea = max(currArea, maxArea);
-        // }
-
-
-        // return maxArea;
         
     }
 };
