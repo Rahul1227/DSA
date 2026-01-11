192345e20050511140b2a26fd2c3cbc63a1b382c
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-11 11:22:36 +0530

    Time: 5 ms (66.95%) | Memory: 18.2 MB (60.07%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,57 +1,60 @@
 class Solution {
-private: 
-    int largestRectangleArea(vector<int>& heights) {
-        // trying to solve in 1 pass;
-        heights.push_back(0); // for removing the outer while loop
-        int maxArea = 0;
-        int n = heights.size();
+private:
+    int largestRectangleArea(vector<int>& height) {
+        // if no pse, pse = -1 (out of range)
+        // if no nse, nse = n (out of range)
+        int n = height.size();
+        int maxArea = INT_MIN;
         stack<int> st;
-        st.push(0);
-
-        for(int i =1 ; i<n; i++){
-            while(!st.empty() && heights[i] < heights[st.top()]){
-                int currHeight = heights[st.top()];
+        for(int i =0; i<n; i++){
+            while(!st.empty() && height[i] < height[st.top()]){
+                int currHeight = height[st.top()];
                 st.pop();
-                int pse = !st.empty() ? st.top() : -1;
+                // for nse and pse, we are considering the indices
+                // indices are pushed into the stack
                 int nse = i;
+                int pse = (!st.empty() ? st.top() : -1);
                 int currArea = (nse - pse - 1) * currHeight;
-                maxArea = max(currArea, maxArea);
+                maxArea = max(maxArea, currArea);
             }
             st.push(i);
         }
 
-        // while(!st.empty()){
-        //         int currHeight = heights[st.top()];
-        //         st.pop();
-        //         int pse = !st.empty() ? st.top() : -1;
-        //         int nse = n;
-        //         int currArea = (nse - pse - 1) * currHeight;
-        //         maxArea = max(currArea, maxArea);
-        // }
-        return maxArea;
-    }       
-public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<int> currHisto(m,0);
-        int globalMaxArea = INT_MIN;
-        for(int i =0; i<n; i++){
-            //  forming the histogram
-            for(int j =0; j<m; j++){
-                if(matrix[i][j] == '0'){
-                    currHisto[j] = 0;
-                }else{
-                    currHisto[j] += matrix[i][j] - '0';
-                }
-
-            }
-
-            int currArea = largestRectangleArea(currHisto);
-            globalMaxArea = max(globalMaxArea, currArea);
+        while(!st.empty()){
+            int currHeight = height[st.top()];
+            st.pop();
+            int nse = n;
+            int pse = (!st.empty()? st.top():-1);
+            int currArea = (nse-pse-1) * currHeight;
+            maxArea = max(maxArea, currArea);
         }
 
-        return globalMaxArea;
+        return maxArea;
         
+    }
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<int> heights(col,0);
+        int maxArea = INT_MIN;
+        for(int i =0; i<row; i++){
+            for(int j =0; j<col; j++){
+                if(matrix[i][j] == '1'){
+                    heights[j] += matrix[i][j] - '0';
+                }else{
+                    heights[j] = 0;
+                }
+            }
+
+            int currArea = largestRectangleArea(heights);
+            maxArea = max(currArea, maxArea);
+        }
+
+        return maxArea;
+
+
+        
+
     }
 };
