d399fd19419b4eb0e770a847c62bfe651ece8977
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-15 13:11:11 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (18.26%) - LeetSync

--- 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
@@ -1,26 +1,29 @@
 class Solution {
 private:
-    int getMax(vector<int>& bars) {
-        sort(bars.begin(), bars.end());
-        int best = 1, cur = 1;
-
-        for (int i = 1; i < bars.size(); i++) {
-            if (bars[i] == bars[i - 1] + 1) {
-                cur++;
-            } else {
-                best = max(best, cur);
-                cur = 1;
+    int getMaxSide(vector<int> & sides){
+        int n = sides.size();
+        int maxSide = 1;
+        sort(sides.begin(), sides.end());
+        int curr = 1;
+        for(int i =1; i<n; i++){
+            if(sides[i] == (sides[i-1] +1)){
+                curr++;
+            }else{
+                maxSide = max(maxSide, curr);
+                curr= 1;
             }
         }
-        best = max(best, cur);
-        return best + 1;
-    }
+        maxSide = max(maxSide, curr) + 1;
 
+        return maxSide;
+    }
 public:
     int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
-        int h = getMax(hBars);
-        int v = getMax(vBars);
-        int side = min(h, v);
-        return side * side;
+        int height = getMaxSide(vBars);
+        int width = getMaxSide(hBars);
+        int minSide = min(height, width);
+        int area = minSide * minSide;
+        return area;
+        
     }
 };
