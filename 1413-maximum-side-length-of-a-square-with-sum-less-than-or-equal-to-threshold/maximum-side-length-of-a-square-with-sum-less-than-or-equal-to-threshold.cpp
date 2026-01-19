0dca9b668c688faad4c6aea8b2dfc6179850552e
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-19 22:48:56 +0530

    Time: 18 ms (41.36%) | Memory: 32.8 MB (60.91%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,39 +1,54 @@
 class Solution {
 private:
-    bool isValid(int x1, int y1, int x2, int y2, vector<vector<int>> &prefixSum, int threshold){
-        int sum = prefixSum[x2+1][y2+1] - prefixSum[x1][y2+1] 
-                            - prefixSum[x2+1][y1] + prefixSum[x1][y1];
+    bool isValid(int x1, int y1, int x2, int y2, vector<vector<int>>& prefixSum,
+                 int threshold) {
+        int sum = prefixSum[x2 + 1][y2 + 1] - prefixSum[x1][y2 + 1] -
+                  prefixSum[x2 + 1][y1] + prefixSum[x1][y1];
         return sum <= threshold;
     }
+
 public:
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
         int m = mat.size();
         int n = mat[0].size();
 
-        //prefixSUm
-        vector<vector<int>> prefixSum(m+1, vector<int>(n+1,0));
+        // prefixSUm
+        vector<vector<int>> prefixSum(m + 1, vector<int>(n + 1, 0));
 
-        for(int i =0; i<m; i++){
-            for(int j=0; j<n; j++){
-                prefixSum[i+1][j+1] = prefixSum[i][j+1] + prefixSum[i+1][j] 
-                                        - prefixSum[i][j] + mat[i][j];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                prefixSum[i + 1][j + 1] = prefixSum[i][j + 1] +
+                                          prefixSum[i + 1][j] -
+                                          prefixSum[i][j] + mat[i][j];
             }
         }
+        int ans = 0;
+        int low = 1;
+        int high = min(m, n);
+        while (low <= high) {
+            int side = low + ((high - low) / 2);
+            bool found = false;
 
-        for(int side = min(m,n); side >0; side--){
-            for(int i=0; i + side -1<m; i++){
-                for(int j=0; j + side - 1<n; j++){
-                    int k = i+side-1;
-                    int l = j+side-1;
+            for (int i = 0; i + side - 1 < m && !found; i++) {
+                for (int j = 0; j + side - 1 < n; j++) {
+                    int k = i + side - 1;
+                    int l = j + side - 1;
 
-                    if(isValid(i,j,k,l,prefixSum, threshold)){
-                        return side;
+                    if (isValid(i, j, k, l, prefixSum, threshold)) {
+                        found = true;
+                        break;
                     }
                 }
             }
+
+            if (found) {
+                ans = side;
+                low = side + 1;
+            } else {
+                high = side - 1;
+            }
         }
 
-        return 0;
-        
+        return ans;
     }
 };
