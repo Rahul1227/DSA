9230a2a43dd1a569a2c72dbad7f18ad6f09bb055
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-01-27 13:32:16 +0530

    Time: 1275 ms (5.04%) | Memory: 309.7 MB (50.72%) - LeetSync

---
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> adj(n);
+
+        for(int i =0; i<edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, 2*w});
+        }
+
+        vector<int> distance(n, INT_MAX);
+
+        priority_queue<pair<int,int>> pq;
+        pq.push({0,0});
+        distance[0] = 0;
+
+        while(!pq.empty()){
+            auto [u, dist] = pq.top();
+            pq.pop();
+
+            for(auto &[v, storedDistance] : adj[u]){
+                int newDistance = dist + storedDistance;
+
+                if(newDistance < distance[v]){
+                    distance[v] = newDistance;
+                    pq.push({v, newDistance});
+                }
+            }
+        }
+
+       return (distance[n-1] != INT_MAX ? distance[n-1] : -1);
+
+        
+    }
+};
