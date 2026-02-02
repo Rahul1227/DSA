0cda7f2a54f685f009522ab98f8a8cd19313c79b
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-02-02 23:20:40 +0530

    Time: 951 ms (5.05%) | Memory: 11.6 MB (92.09%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,34 +1,24 @@
 class Solution {
 private:
-    int solve (int n, int target, vector<int> &nums, vector<vector<int>> &dp){
-        if(n == 0){
-            if(target == 0){
+    int solve(int ind, int prefixSum, vector<int> &nums, int target){
+        int n = nums.size();
+        if(ind == n){
+            if(prefixSum == target){
                 return 1;
-            } else{
+            }else{
                 return 0;
             }
         }
-        int targetSize = dp[0].size();
-        // if(target + offset < 0 || target + offset >= dp[0].size()) return 0;
-        if(dp[n][(target +  targetSize) % targetSize] != -1) return dp[n][(target +  targetSize) % targetSize];
 
-        int plus = solve(n-1, target - nums[n-1], nums,dp);
-        int minus = solve(n-1, target + nums[n-1], nums,dp);
+        int plus = solve(ind+1, prefixSum + nums[ind], nums, target);
 
-        return dp[n][(target +  targetSize) % targetSize] = plus + minus;
+        int minus = solve(ind+1, prefixSum - nums[ind], nums, target);
 
-
-
+        return plus + minus;
     }
-
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        int offset = sum;
-        int targetSize = sum + abs(target) + 1;
-        int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(targetSize, -1));
-        return solve (n, target, nums, dp);
+        return solve(0, 0, nums, target);
         
     }
 };
