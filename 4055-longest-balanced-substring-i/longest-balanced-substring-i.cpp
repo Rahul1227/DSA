4f81810b4788ba51a2cfc7d96d3c6ad7e0163e0d
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-02-12 10:52:11 +0530

    Time: 144 ms (74.80%) | Memory: 11.4 MB (96.34%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -1,30 +1,26 @@
 class Solution {
-private:
-    bool isPossible(int i, int j, string &s, unordered_map<char, int> &mp){
-        mp[s[j]]++;
-        int toMatchWith = mp[s[j]];
-        for(auto &[key, val] :mp){
-            if(val != toMatchWith) return false;
-
-        }
-
-        return true;
-    }
 public:
     int longestBalanced(string s) {
         int n = s.size();
-        int maxLen = 1;
-        for(int i =0; i<n; i++){
-            unordered_map<char, int> mp;
-            for(int j =i; j<n; j++){
-                if(isPossible(i,j,s, mp)){
-                    int currLen = j-i+1;
-                    maxLen = max(maxLen, currLen);
-
+        int res = 0;
+        vector<int> cnt(26);
+        for (int i = 0; i < n; i++) {
+            fill(cnt.begin(), cnt.end(), 0);
+            for (int j = i; j < n; j++) {
+                bool flag = true;
+                int c = s[j] - 'a';
+                cnt[c]++;
+                for (auto x : cnt) {
+                    if (x > 0 && x != cnt[c]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    res = max(res, j - i + 1);
                 }
             }
         }
-
-        return maxLen;
+        return res;
     }
 };
