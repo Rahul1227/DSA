9384dfef1f796fad80af7b8b785fcae547e17d53
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-02-14 09:51:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (75.24%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,63 +1,74 @@
-class Solution {
+class DisjointSet{
 private:
-    int findUltimateParent(int u, vector<int> &par){
-        if(u == par[u]){
+    vector<int> size;
+    vector<int> parent;
+
+public:
+    DisjointSet(int n){
+        size.resize(n,1);
+        parent.resize(n);
+        for(int i =0; i<n; i++){
+            parent[i] = i;
+
+        }
+    }
+
+    int findUltimateParent(int u){
+        if(parent[u] == u){
             return u;
         }
 
 
-        return par[u] = findUltimateParent(par[u], par);
+        return parent[u] = findUltimateParent(parent[u]);
     }
 
-    void unionBySize(int u, int v, vector<int> &par, vector<int> &size){
-        int parentU = findUltimateParent(u, par);
-        int parentV = findUltimateParent(v, par);
 
-        if(parentU ==  parentV) return;
+    void unionBySize(int u, int v){
+        int parentU = findUltimateParent(u);
+        int parentV = findUltimateParent(v);
 
-        if(size[parentU] <= size[parentV]){
-            par[parentU] = parentV;
-            size[parentV] += size[parentU];
-        }else{
-            par[parentV] = parentU;
+        if(parentU == parentV) return;
+
+        if(size[parentU] > size[parentV]){
+            parent[parentV] = parentU;
             size[parentU] += size[parentV];
+        }else{
+            parent[parentU] = parentV;
+            size[parentV] += size[parentU];
         }
     }
 
-    bool isConnected(int u, int v, vector<int> &par){
-        int parentU = findUltimateParent(u, par);
-        int parentV = findUltimateParent(v, par);
-
-        return parentU == parentV;
-    }
-public:
-    int makeConnected(int n, vector<vector<int>>& connections) {
-
-        int len = connections.size();
-
-        if(len < n-1) return -1;
-
-        vector<int> par(n);
-        vector<int> size(n,1);
-        for(int i=0; i<n; i++){
-            par[i] = i;
-        }
-
-        for(int i=0; i<len; i++){
-            int u = connections[i][0];
-            int v = connections[i][1];
-            unionBySize(u,v,par,size);
-
-        }
-
+    int getResult(){
         int count = 0;
-        for(int i=0; i<n; i++){
-            if(i == par[i]){
+        for(int i =0; i< parent.size(); i++){
+            if(parent[i] == i){
                 count++;
             }
         }
 
         return count-1;
+    }
+
+    
+};
+
+
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int len = connections.size();
+        if(len < n-1) return -1;
+
+        DisjointSet ds(n);
+
+        for(int i =0; i<len; i++){
+            int u = connections[i][0];
+            int v = connections[i][1];
+            ds.unionBySize(u,v);
+        }
+
+        return ds.getResult();
 
         
     }
