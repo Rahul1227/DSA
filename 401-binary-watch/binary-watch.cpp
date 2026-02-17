87bfd482771fb7ff9f091b00829cdde28e92650d
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-02-17 21:49:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (41.32%) - LeetSync

--- 401-binary-watch/binary-watch.cpp
+++ 401-binary-watch/binary-watch.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     vector<string> readBinaryWatch(int turnedOn) {
-        vector<string> ans;
-        for (int i = 0; i < 1024; ++i) {
-            int h = i >> 6, m = i & 63;  // Extract the high 4 bits and low 6
-                                         // bits using bitwise operations
-            if (h < 12 && m < 60 && __builtin_popcount(i) == turnedOn) {
-                ans.push_back(to_string(h) + ":" + (m < 10 ? "0" : "") +
-                              to_string(m));
+        vector<string> result;
+        for(int i =0; i<1024; i++){
+            int hour = i >> 6;
+            int mins = i & 63; // 63 = 11111
+            if(hour < 12 && mins < 60 && __builtin_popcount(i) == turnedOn){
+                string ans = to_string(hour);
+                ans += ":";
+                if(mins < 10){
+                    ans += '0';
+                }
+                ans += to_string(mins);
+                result.push_back(ans);
+                
             }
+
         }
-        return ans;
+
+        return result;
     }
 };
