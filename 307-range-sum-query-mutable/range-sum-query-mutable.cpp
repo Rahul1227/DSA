2b13ec9d20ff971fe8b20256180b1f49bd515932
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-03-26 12:01:20 +0530

    Time: 55 ms (68.72%) | Memory: 182.4 MB (36.34%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -52,7 +52,7 @@ private:
 public:
     NumArray(vector<int>& nums) {
         n =nums.size();
-        segmentTree.resize(4*n);
+        segmentTree.resize(4*n,0);
         // ind, left, right, nums
         buildTree(0,0, n-1, nums);
         
