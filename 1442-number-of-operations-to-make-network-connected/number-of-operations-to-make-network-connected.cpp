99a862642a634fbc1313c78417a88af1350204fc
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-04-03 19:19:10 +0530

    Time: 17 ms (56.22%) | Memory: 46.7 MB (51.10%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,74 +1,72 @@
 class DisjointSet{
-private:
-    vector<int> size;
-    vector<int> parent;
+    private:
+        vector<int> size;
+        vector<int> parent;
+    public:
+        DisjointSet(int n){
+            size.resize(n,1);
+            parent.resize(n);
+            for(int i =0; i<n; i++){
+                parent[i] = i;
 
-public:
-    DisjointSet(int n){
-        size.resize(n,1);
-        parent.resize(n);
-        for(int i =0; i<n; i++){
-            parent[i] = i;
-
-        }
-    }
-
-    int findUltimateParent(int u){
-        if(parent[u] == u){
-            return u;
-        }
-
-
-        return parent[u] = findUltimateParent(parent[u]);
-    }
-
-
-    void unionBySize(int u, int v){
-        int parentU = findUltimateParent(u);
-        int parentV = findUltimateParent(v);
-
-        if(parentU == parentV) return;
-
-        if(size[parentU] > size[parentV]){
-            parent[parentV] = parentU;
-            size[parentU] += size[parentV];
-        }else{
-            parent[parentU] = parentV;
-            size[parentV] += size[parentU];
-        }
-    }
-
-    int getResult(){
-        int count = 0;
-        for(int i =0; i< parent.size(); i++){
-            if(parent[i] == i){
-                count++;
             }
         }
 
-        return count-1;
-    }
+        int findUltimateParent(int u){
+            if(parent[u] == u){
+                return u;
+            }
 
-    
+            return parent[u] = findUltimateParent(parent[u]);
+        }
+
+        void unionBySize(int u, int v){
+            int parentU = findUltimateParent(u);
+            int parentV = findUltimateParent(v);
+
+            if(parentU == parentV) return;
+
+            if(size[parentU] >= size[parentV]){
+                parent[parentV] = parentU;
+                size[parentU] += size[parentV];
+            }else{
+                parent[parentU] = parentV;
+                size[parentV] += size[parentU];
+            }
+        }
+
+        int getDistinctParent(){
+            // for(auto par: parent){
+            //     cout<<par<<" ";
+            // }
+            int ans = 0;
+            for(int i =0; i<parent.size(); i++){
+                if(parent[i] == i){
+                    ans++;
+                }
+            }
+
+            return ans-1;
+        }
+
 };
-
-
 
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        int len = connections.size();
-        if(len < n-1) return -1;
-
         DisjointSet ds(n);
+        size_t conSize = connections.size();
+        if(conSize < n-1) return -1;
 
-        for(int i =0; i<len; i++){
-            int u = connections[i][0];
-            int v = connections[i][1];
+        for(auto connection : connections){
+            int u = connection[0];
+            int v = connection[1];
             ds.unionBySize(u,v);
         }
 
-        return ds.getResult();
+        int ans = ds.getDistinctParent();
+        return ans;
+
 
         
     }
