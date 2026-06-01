6ce9bc0dfb49cff37389613ae07a5dd9c2801151
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-06-01 22:06:09 +0545

    Time: 226 ms (61.68%) | Memory: 10.8 MB (48.63%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,50 +1,50 @@
 class Solution {
 private:
-    bool check(int i, int j, int ind, string& word, vector<vector<char>>& board,
-               vector<vector<int>>& visited) {
-        // base condition
+    bool solve(int i, int j, vector<vector<int>> &visited, int ind,
+     vector<vector<char>> &board, string &word){
+        // if(board[i][j] != word[ind]) return false;
         int m = board.size();
         int n = board[0].size();
-
-        if (ind == word.size()) {
+        
+        if(ind == word.size()){
             return true;
         }
+        // visited[i][j] = 1;
+        int dirX[] ={-1,1,0,0};
+        int dirY[] ={0,0,1,-1};
 
-        int dirX[] = {-1, 1, 0, 0};
-        int dirY[] = {0, 0, 1, -1};
+        for(int z =0; z<4; z++){
+            int newI = i + dirX[z];
+            int newJ = j + dirY[z];
 
-        for (int x = 0; x < 4; x++) {
-            int newI = i + dirX[x];
-            int newJ = j + dirY[x];
-
-            // validity check
-            if (newI >= 0 && newI < m && newJ >= 0 && newJ < n &&
-                !visited[newI][newJ] && board[newI][newJ] == word[ind]) {
+            if(newI>=0 && newI <m && newJ >=0 && newJ < n && 
+            !visited[newI][newJ] && board[newI][newJ] == word[ind]){
                 visited[newI][newJ] = 1;
-                if(check(newI, newJ, ind + 1, word, board, visited)) return true;
+                if(solve(newI, newJ, visited, ind+1, board, word)) return true;
                 visited[newI][newJ] = 0;
             }
         }
         return false;
-    }
 
+
+
+     }
 public:
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
-
-        // finding the starting letter of the word and then starting the search
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word[0]) {
-                    vector<vector<int>> visited(m, vector<int>(n, 0));
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+        for(int i =0; i<m; i++){
+            for(int j =0; j<n; j++){
+                if(board[i][j] == word[0]){
                     visited[i][j] = 1;
-                    if (check(i, j, 1, word, board, visited))
-                        return true;
+                    if(solve(i,j,visited,1,board, word)) return true;
+                    visited[i][j] = 0;
                 }
+               
             }
-        }
 
+        }
         return false;
     }
 };
