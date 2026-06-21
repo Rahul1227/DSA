827399084c34be4c84e4c9009b98de84e9e92d3f
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-06-21 23:32:43 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (70.82%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,42 +1,20 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-         int n = nums.size();
-        vector<pair<int,int>> ranges;
-        for(int i =0; i<n; i++){
-            if(nums[i] == 0) continue;
-            int start = i;
-            int end = min(n, i+nums[i]);
-            ranges.push_back({start, end});
-
-        }
-
-        sort(ranges.begin(), ranges.end());
-
-        int currEnd = 0;
-        int target = n-1;
+        int currEnd =0;
+        int maxEnd = 0;
         int jumps = 0;
-        int i =0;
-        while(currEnd < target){
-            if(i >= ranges.size()) return false;
 
-            if(ranges[i].first > currEnd) return false;
+        for(int i =0; i<nums.size(); i++){
+            if(i > maxEnd) return -1;
 
-            int maxRange = currEnd;
-            while( i< ranges.size() && ranges[i].first <= currEnd){
-                maxRange = max(maxRange, ranges[i].second);
-                i++;
+            if(i > currEnd){
+                jumps++;
+                currEnd = maxEnd;
             }
 
-            if(maxRange == currEnd){
-                return false;
-            }
-
-            currEnd = maxRange;
-            jumps++;
+            maxEnd = max(maxEnd, i + nums[i]);
         }
-
         return jumps;
-        
     }
 };
