f12fa5a665f060312717085bd997095c2f4d8def
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-06-23 16:06:43 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (89.52%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,54 +1,29 @@
 class Solution {
 private:
-    bool isValid(string s){
-        stack<char> st;
-        for(auto c : s){
-            if(c == '('){
-                st.push(c);
-            }else{
-                if(!st.empty()){
-                    st.pop();
-                }else{
-                    return false;
-                }
-            }
+    void solve(int OC, int CC, int n, string &curr, vector<string>& ans) {
+        if (OC > n || CC > n)
+            return;
+        if (OC == n && CC == n) {
+            ans.push_back(curr);
+            return;
         }
 
-        return st.empty();
-    }
+        curr.push_back('(');
+        solve(OC + 1, CC, n, curr, ans);
+        curr.pop_back();
 
-    void solve(int openCount, int closeCount, string &curr, vector<string> &ans){
-        // base case
-        if(openCount == 0 && closeCount ==0 ){
-            // if(isValid(curr)){
-                ans.push_back(curr);
-                return;
-            // }else{
-            //     return;
-            // }
-        }
-
-        // openTake;
-        if(openCount){
-            curr.push_back('(');
-            solve(openCount-1, closeCount, curr, ans);
-            curr.pop_back();
-        }
-
-        if(closeCount > openCount){
+        if (OC > CC) {
             curr.push_back(')');
-            solve(openCount, closeCount-1, curr, ans);
+            solve(OC, CC + 1, n, curr, ans);
             curr.pop_back();
         }
     }
+
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        string curr;
-        int openCount = n;
-        int closeCount = n;
-        solve(openCount, closeCount, curr, ans);
+        string curr = "";
+        solve(0, 0, n, curr, ans);
         return ans;
-        
     }
 };
