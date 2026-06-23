398d0b732f7b93c8db73f3c9f91343c5a86e1585
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-06-23 12:31:27 +0530

    Time: 3 ms (5.57%) | Memory: 9.2 MB (60.39%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,49 +1,39 @@
 class Solution {
 private:
-    void solve(int ind, string &curr, vector<string> &ans, vector<string> &given){
-        if(ind == given.size()){
+    void solve(int ind, string &curr, vector<string> &ans, unordered_map<int, string> &mp, string &digits){
+        if(ind == digits.size()){
+            cout<<"curr"<<curr<<endl;
             ans.push_back(curr);
             return;
         }
-        int len = given[ind].size();
-        for(int i =0; i< len; i++){
-            curr.push_back(given[ind][i]);
-            solve(ind+1,curr,ans, given);
+
+        string val = mp[digits[ind]-'0'];
+        for(auto c: val){
+            curr.push_back(c);
+            solve(ind+1, curr, ans, mp, digits);
             curr.pop_back();
         }
     }
 public:
     vector<string> letterCombinations(string digits) {
-        // doing the mapping
         unordered_map<int, string> mp;
         char c = 'a';
-        for(int i = 2; i<=9; i++){
+        for(int i =2; i<=9; i++){
             int count = 3;
             while(count--){
                 mp[i].push_back(c);
                 c++;
             }
-            if(i == 7 || i == 9){
+
+            if(i ==7 || i == 9){
                 mp[i].push_back(c);
                 c++;
             }
-            
-        }
 
+        }
         vector<string> ans;
-        vector<string> given;
-        for(auto c : digits){
-            given.push_back(mp[c-'0']);
-        }
         string curr ="";
-        
-
-        solve(0,curr,ans, given);
+        solve(0,curr,ans, mp, digits);
         return ans;
-
-
-        
-
-        
     }
 };
