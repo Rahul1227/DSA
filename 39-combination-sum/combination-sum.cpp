63d0351213ac52fcefc4cb7137ae3bc4dada1112
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-06-26 09:38:01 +0530

    Time: 3 ms (52.33%) | Memory: 18 MB (22.34%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,31 +1,29 @@
 class Solution {
 private:
-    void solve(int ind, vector<int> &curr, vector<vector<int>> &ans, vector<int> &nums, int target){
-        if(target == 0){
-            ans.push_back(curr);
+    void solve(int ind, vector<int> &curr, vector<vector<int>> &ans, vector<int> &candidates, int target){
+        //base case
+        if(ind == candidates.size()){
+            if(target==0){
+                ans.push_back(curr);
+            }
             return;
         }
 
-        if(ind ==nums.size()){
-            return;
-        }
+        // not take;
+        solve(ind+1,curr,ans,candidates,target);
 
-
-        // notTake
-        solve(ind+1,curr, ans, nums, target);
 
         // take
-        if(target >= nums[ind]){
-            target -= nums[ind];
-            curr.push_back(nums[ind]);
-            solve(ind, curr, ans, nums,target);
+        if(target >=  candidates[ind]){
+            curr.push_back(candidates[ind]);
+            solve(ind, curr, ans, candidates, target - candidates[ind]);
             curr.pop_back();
         }
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
         vector<int> curr;
+        vector<vector<int>> ans;
         solve(0, curr, ans, candidates, target);
         return ans;
         
