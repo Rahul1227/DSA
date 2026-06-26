f174cd856210fc8e7781ee77cf709126f6b1ecbf
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-06-26 11:09:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (31.97%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,32 +1,30 @@
 class Solution {
 private:
-    void solve(int ind, vector<int> &curr, vector<vector<int>> &ans, int k, int n){
-        //base case
-        if(k == 0){
-            if(n==0){
+    void solve(int ind, vector<int> &curr, int currSum, vector<vector<int>> &ans, int k, int n){
+        if(ind > 9){
+            if(k ==0 and currSum == n ){
                 ans.push_back(curr);
-                return;
-            }else{
-                return;
             }
+            return;
         }
 
-        for(int i = ind; i<=9; i++){
-            if(n >= i){
-                curr.push_back(i);
-                solve(i+1,curr, ans,k-1,n-i);
-                curr.pop_back();
-            }
+        // take
+        if(currSum + ind <= n){
+            curr.push_back(ind);
+            solve(ind+1,curr, currSum + ind, ans, k-1, n);
+            curr.pop_back();
         }
 
 
-
+        // notTake
+        solve(ind +1, curr, currSum, ans, k, n);
     }
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
+        int currSum =0;
         vector<vector<int>> ans;
         vector<int> curr;
-        solve(1, curr, ans,  k,  n);
+        solve(1,curr, currSum, ans, k, n);
         return ans;
         
     }
