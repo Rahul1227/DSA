7bc75a45d7278571c80223f0376bd634620fa8c2
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-06-26 12:10:51 +0530

    Time: 3 ms (32.66%) | Memory: 10.5 MB (66.93%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,23 +1,26 @@
 class Solution {
 private:
-    void solve(int ind, vector<int> &curr, vector<vector<int>>&ans, vector<int> &nums){
-        // push at each level
-        ans.push_back(curr);
-
-        for(int i = ind; i< nums.size(); i++){
-            // trying to skip the duplicate
-            if(i != ind && nums[i] == nums[i-1]) continue;
-
-            curr.push_back(nums[i]);
-            solve(i+1, curr, ans, nums);
-            curr.pop_back();
+    void solve(int ind, vector<int> &curr, vector<vector<int>> &ans, vector<int> &nums){
+        // base condition
+        if(ind == nums.size()){
+            ans.push_back(curr);
+            return;
         }
+
+        // take
+        curr.push_back(nums[ind]);
+        solve(ind+1, curr, ans, nums);
+        curr.pop_back();
+
+        // not take
+        int newInd = upper_bound(nums.begin() + ind, nums.end(), nums[ind]) - nums.begin();
+        solve(newInd, curr, ans , nums);
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
         vector<int> curr;
+        vector<vector<int>> ans;
         solve(0, curr, ans, nums);
         return ans;
         
