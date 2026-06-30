896e55e43fc3da377a5b6c3c8d5bcc7804b2545f
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-06-30 13:23:12 +0530

    Time: 7 ms (61.29%) | Memory: 11.5 MB (64.83%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,14 +1,14 @@
 class Solution {
 private:
-    bool isValid(unordered_map<char,int> &needed, unordered_map<char,int> &present){
-        for(auto [key, val] : needed){
-            if(!present.count(key)) return false;
-            else{
-                if(val > present[key]) return false;
-            }
-        }
-        return true;
-    }
+    // bool isValid(unordered_map<char,int> &needed, unordered_map<char,int> &present){
+    //     for(auto [key, val] : needed){
+    //         if(!present.count(key)) return false;
+    //         else{
+    //             if(val > present[key]) return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 public:
     string minWindow(string s, string t) {
         int startingInd = -1;
@@ -16,23 +16,27 @@ public:
         for(auto c : t){
             needed[c]++;
         }
-        unordered_map<char,int> present;
+        int required = needed.size();
         int i =0;
         int j =0;
         int n = s.size();
         int minLen =INT_MAX;
-        // int currLen =0;
+        int count = 0;
         while(j < n){
-            present[s[j]]++;
+            needed[s[j]]--;
+            if(needed[s[j]]== 0){
+                count++;
+            }
 
-            while(isValid(needed, present)){
+            while(count == required){
                 int currLen = j-i+1;
                 if(currLen < minLen){
                     startingInd = i;
                     minLen = currLen;
                     // ans = s.substr(i, minLen);
                 }
-                present[s[i]]--;
+                needed[s[i]]++;
+                if(needed[s[i]] > 0) count--;
                 i++;
             }
             j++;
