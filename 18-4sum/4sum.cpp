f04a1f772ff08b38c05c70ceedc5ee2ab5cbee85
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-01 21:38:14 +0530

    Time: 22 ms (60.28%) | Memory: 17.5 MB (55.08%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>> st;
-        int n = nums.size();
         sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        int n = nums.size();
+        for(int i =0; i<n-3; i++){
+            if(i> 0 && nums[i] == nums[i-1]) continue;
+            for(int j= i+1; j<n-2; j++){
+                if(j > i+1 && nums[j-1] == nums[j]) continue;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                int k = j + 1;
-                int l = n - 1;
+                int k = j+1;
+                int l = n-1;
+                
+                while(k < l){
+                    long long currSum = 1LL *nums[i] + nums[j] + nums[k] + nums[l];
 
-                while(k < l) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
-
-                    if(sum == target) {
-                        st.insert({nums[i], nums[j], nums[k], nums[l]});
+                    if(currSum < target){
+                        k++;
+                    }else if(currSum > target){
+                        l--;
+                    }else{
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
                         k++;
                         l--;
-                    }
-                    else if(sum > target) {
-                        l--;
-                    }
-                    else {
-                        k++;
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                        while(k < l && nums[l] == nums[l+1]) l--;
                     }
                 }
             }
+            
         }
-
-        return vector<vector<int>>(st.begin(), st.end());
+        return ans;
+        
     }
 };
