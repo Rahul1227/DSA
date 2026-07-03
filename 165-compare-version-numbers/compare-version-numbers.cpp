f0b2db40cb4c59de78ff40952680bc10049d30ba
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-03 23:13:56 +0530

    Time: 2 ms (6.86%) | Memory: 8.7 MB (17.23%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,33 +1,27 @@
 class Solution {
-private:   
-    vector<int> getIntergerFromToken(string &version){
-        vector<int> ans;
-        stringstream v1Token(version);
-        string num;
-
-        while(getline(v1Token, num, '.')){
-            ans.push_back(stoi(num));
-        }
-
-        return ans;
-
-    }
 public:
     int compareVersion(string version1, string version2) {
-        vector<int> v1 = getIntergerFromToken(version1);
-        vector<int> v2 = getIntergerFromToken(version2);
+        stringstream ss1(version1);
+        stringstream ss2(version2);
+        
 
-        int len =  max((int)v1.size(), (int)v2.size());
+        while(ss1 || ss2){
+            string word1;
+            string word2;
+            
+            if(ss1){
+                getline(ss1,word1, '.');
+            }
 
-        for(int i =0;  i<len; i++){
-            int num1 = (i<v1.size()) ? v1[i]: 0;
-            int num2 = (i<v2.size()) ? v2[i]:0;
-            if(num1 <num2) return -1;
+            if(ss2){
+                getline(ss2,word2, '.');
+            }
+            int num1 = word1.size() > 0 ? stoi(word1) :0;
+            int num2 = word2.size() > 0 ? stoi(word2) : 0;
+            if(num1 < num2) return -1;
             if(num1 > num2) return 1;
         }
 
-        
         return 0;
-        
     }
 };
