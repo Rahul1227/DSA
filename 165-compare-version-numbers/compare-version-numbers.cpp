acba7e0962ae427b15c20619eb04483b620a97fb
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-03 23:24:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (22.07%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -9,13 +9,13 @@ public:
             string word1;
             string word2;
             
-            if(ss1){
+            // if(ss1){
                 getline(ss1,word1, '.');
-            }
+            // }
 
-            if(ss2){
+            // if(ss2){
                 getline(ss2,word2, '.');
-            }
+            // }
             int num1 = word1.size() > 0 ? stoi(word1) :0;
             int num2 = word2.size() > 0 ? stoi(word2) : 0;
             if(num1 < num2) return -1;
