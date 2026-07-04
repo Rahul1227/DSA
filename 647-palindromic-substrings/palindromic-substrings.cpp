dd6c10a43b36dd2a6f810538d80d5bd94458d053
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-04 23:21:16 +0530

    Time: 30 ms (31.45%) | Memory: 11.2 MB (39.53%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,24 +1,32 @@
 class Solution {
-private:
-    bool isPalin(int i, int j, string &s){
-        while(i < j){
-            if(s[i] != s[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
 public:
     int countSubstrings(string s) {
+        int count = 0;
         int n = s.size();
-        int result = 0;
-        for(int i =0; i<n; i++){
-            for(int j =i; j<n; j++){
-                if(isPalin(i,j,s)){
-                    result++;
+        vector<vector<bool>> dp(n+1, vector<bool>(n+1, false));
+
+        // trying every possible length
+        for(int L = 1; L<=n; L++){
+            // trying every starting point
+            for(int i =0; i+L-1 < n; i++){
+                int j = i+L-1;
+                // case 1 -> 1 len
+                if(i == j){
+                    dp[i][j] = true;
+                    // case 2 -> 2 len
+                }else if(i+1 == j){
+                    dp[i][j] = (s[i] == s[j]);
+                }else{
+                    dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
                 }
-            }
+
+                if(dp[i][j] == true){
+                    count++;
+                }
+            } 
         }
-        return result;
+
+        return count;
+        
     }
 };
