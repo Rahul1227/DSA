81ba80d3409c38e1336d692d117e35d768368158
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-04 23:29:41 +0530

    Time: 284 ms (16.32%) | Memory: 292.8 MB (5.28%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,31 +1,34 @@
 class Solution {
-private:
-    int expandAroundCenter(int left, int right, string &s){
-
-        while(left >= 0 && right < s.size() && s[left] == s[right]){
-            left--;
-            right++;
-        }
-        return right - left - 1;
-
-        
-    }
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
+        int startingPoint = 0;
         int maxLen = 1;
-        int start =0;
-        for(int i =0; i < n; i++){
-            int len1 = expandAroundCenter(i,i,s);
-            int len2 = expandAroundCenter(i,i+1,s);
-            int currLen = max(len1, len2);
-            if(currLen > maxLen){
-                maxLen = currLen;
-                start = i - (currLen-1)/2;
+        int n = s.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        for(int l =1; l<=n; l++){
+            for(int i =0; i+l-1 <n; i++){
+                int j = i+l-1;
+
+                if(i ==j){
+                    dp[i][j] = l;
+                }else if(i+1 == j){
+                    if(s[i] == s[j]){
+                        dp[i][j] = l;
+                    }
+                }else{
+                    if(s[i] == s[j] && dp[i+1][j-1] != -1){
+                        dp[i][j] = l;
+                    }
+                }
+
+                if(maxLen < dp[i][j]){
+                    maxLen = dp[i][j];
+                    startingPoint = i;
+                }
             }
         }
 
-        return s.substr(start, maxLen);
+        return s.substr(startingPoint, maxLen);
         
     }
 };
