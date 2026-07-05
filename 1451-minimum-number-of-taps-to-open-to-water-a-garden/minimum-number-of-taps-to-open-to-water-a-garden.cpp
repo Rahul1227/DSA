fb681a9a4ae8d002da950e7581b99f7c58be1043
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-05 13:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (44.48%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,46 +1,28 @@
 class Solution {
-private:
-    static bool compare(const pair<int,int> &a, const pair<int,int> &b){
-        if(a.first == b.first){
-            return a.second > b.second;
-        }
-
-        return a.first < b.first;
-    }
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<pair<int,int>> interval;
+        vector<int> intervals(n+1);
+        for(int i =0; i<ranges.size(); i++){
+            int startPoint = max(0, i - ranges[i]);
+            int endPoint = min(n, i+ranges[i]);
+            intervals[startPoint] = max(intervals[startPoint], endPoint);
+        }
+        int count =0;
+        int maxReach = 0;
+        int currReach =0;
+        
         for(int i =0; i<=n; i++){
-            int start = max(0, i-ranges[i]);
-            int end = min(n, i + ranges[i]);
-            interval.push_back({start, end});
-        }
+           if(i > maxReach) return -1;
 
-        if(interval.size() == 0) return -1;
-        sort(interval.begin(), interval.end());
-
-        int taps =  0;
-        int currEnd = 0;
-        int i =0;
-        while(currEnd < n){
-            if(i >= interval.size()) return -1;
-
-            if(interval[i].first > currEnd +1) return -1;
-
-            int maxReach = currEnd;
-            while(i<interval.size() && interval[i].first <= currEnd){
-                maxReach = max(maxReach, interval[i].second);
-                i++;
+            if(i > currReach){
+                currReach = maxReach;
+                count++;
             }
+            maxReach = max(maxReach, intervals[i]);
 
-            if(maxReach == currEnd) return -1;
-
-            currEnd = maxReach;
-            taps++;
         }
 
-        return taps;
-
+        return count;
         
     }
 };
