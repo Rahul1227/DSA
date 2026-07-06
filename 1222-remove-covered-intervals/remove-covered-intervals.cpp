e5235e9b6d9bb1e18ea0ea81629f075bb94248dc
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-06 18:17:57 +0530

    Time: 59 ms (9.14%) | Memory: 15.1 MB (82.12%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -15,17 +15,11 @@ public:
         int j = 1;
         int i =0;
         while(j < n){
-            // int a = intervals[i][0];
-            // int b = intervals[i][1];
-            // int c = intervals[j][0];
-            // int d = intervals[j][1];
             while(j < n && intervals[i][0] <=intervals[j][0] && intervals[i][1] >= intervals[j][1]){
                 ans--;
                 cout<<j<<endl;
                 cout<<ans<<endl;
                 j++;
-                // c = intervals[j][0];
-                // d = intervals[j][1];
             }
             i =j;
             j++;
