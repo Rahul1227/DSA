5cff7c5a16775443cbe92b3e6a173598f6f9b254
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-08 12:05:20 +0530

    Time: 92 ms (29.09%) | Memory: 76 MB (21.73%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,31 +1,24 @@
 class Solution {
-private:
-    int solve(int ind1, int ind2, vector<vector<int>> &dp, string &txt1, string &txt2){
-        //base case
-        if(ind1 < 0 || ind2 < 0) return 0;
+public:
+    int longestPalindromeSubseq(string s1) {
+        string s2 = s1;
+        reverse(s1.begin(), s1.end());
+        int n = s1.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
 
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-
-        //both character matching
-        if(txt1[ind1] == txt2[ind2]){
-            return 1 + solve(ind1-1, ind2-1, dp, txt1, txt2);
+        for(int i =1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    int skip1 = dp[i-1][j];
+                    int skip2 = dp[i][j-1];
+                    dp[i][j] = max(skip1, skip2);
+                }
+            }
         }
 
-        int moveFirst = solve(ind1 -1, ind2, dp, txt1, txt2);
-        int moveSecond = solve(ind1, ind2-1, dp, txt1, txt2);
-
-
-        return dp[ind1][ind2] = max(moveFirst, moveSecond);
-    }
-public:
-    int longestPalindromeSubseq(string s) {
-        string s2 = s;
-        reverse(s.begin(), s.end());
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        return solve(n-1,n-1,dp,s,s2);
-
-
+        return dp[n][n];
         
     }
 };
