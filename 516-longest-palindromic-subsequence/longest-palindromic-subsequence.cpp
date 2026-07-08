03e5df4417fedf285bd3eb0ab8c3d33ac0854f02
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-08 12:07:36 +0530

    Time: 40 ms (94.55%) | Memory: 9.5 MB (87.78%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -4,21 +4,24 @@ public:
         string s2 = s1;
         reverse(s1.begin(), s1.end());
         int n = s1.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        // vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        vector<int> curr(n+1, 0);
+        vector<int> prev(n+1, 0);
 
         for(int i =1; i<=n; i++){
             for(int j=1; j<=n; j++){
                 if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 }else{
-                    int skip1 = dp[i-1][j];
-                    int skip2 = dp[i][j-1];
-                    dp[i][j] = max(skip1, skip2);
+                    int skip1 = prev[j];
+                    int skip2 = curr[j-1];
+                    curr[j] = max(skip1, skip2);
                 }
             }
+            prev =  curr;
         }
 
-        return dp[n][n];
+        return prev[n];
         
     }
 };
