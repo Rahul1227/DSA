f6862c69156d5d3c9d6d927664366cab5534daa1
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-08 17:27:34 +0530

    Time: 19 ms (88.85%) | Memory: 9.5 MB (99.62%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -4,19 +4,22 @@ public:
         string s2 = s1;
         reverse(s2.begin(), s2.end());
         int n = s1.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        // vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        vector<int> prev(n+1, 0);
+        vector<int> curr(n+1, 0);
 
         for(int i =1; i<=n; i++){
             for(int j =1; j<=n; j++){
                 if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 }else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    curr[j] = max(prev[j], curr[j-1]);
                 }
             }
+            prev = curr;
         }
 
-        return n - dp[n][n];
+        return n - prev[n];
         
     }
 };
