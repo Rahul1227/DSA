279e0d7ffee0543d2767e393dbf66d136d95a81b
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-09 14:40:57 +0530

    Time: 12 ms (19.86%) | Memory: 13.2 MB (41.69%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -6,7 +6,7 @@ private:
             return i;
         }
 
-        if(i ==0){
+        if(i == 0){
             return j;
         }
 
@@ -33,8 +33,31 @@ public:
     int minDistance(string word1, string word2) {
         int n1 = word1.size();
         int n2 = word2.size();
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        return solve(n1, n2, word1, word2, dp);
+        // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        // return solve(n1, n2, word1, word2, dp);
+
+        for(int i=1; i<=n1; i++){
+            dp[i][0] = i;
+        }
+
+        for(int j=1; j<=n2; j++){
+            dp[0][j] = j;
+
+        }
+
+
+        for(int i =1; i<=n1; i++){
+            for(int j=1; j<=n2; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1 +  min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                }
+            }
+        }
+
+        return dp[n1][n2];
         
     }
 };
