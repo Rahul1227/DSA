33642b70b590d00a54b61f63191899cf6f49575c
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-10 22:53:52 +0530

    Time: 3 ms (76.22%) | Memory: 10.5 MB (22.41%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,35 +1,63 @@
 class Solution {
 private:
-    int getCount(int i, string &a, string &b){
-        int count = 1;
-        int j = 0;
-        int n1= a.size();
+    vector<int> getLPS(string s){
+        int n = s.size();
+        vector<int> lps(n, 0);
+        int len =0;
+        int i =1;
+        while(i < n){
+            if(s[i] == s[len]){
+                len++;
+                lps[i] = len;
+                i++;
+            }else if(len != 0){
+                len = lps[len-1];
+            }else{
+                i++;
+            }
+        }
+        return lps;
+    }
+
+
+    bool solve(string &a, string &b){
+        vector<int> lps = getLPS(b);
+        int i =0;
+        int j =0;
+        int n1 = a.size();
         int n2 = b.size();
 
-        while( j < n2){
+        while(i<n1){
             if(a[i] == b[j]){
                 i++;
                 j++;
-                if(j < n2 && i == n1){
-                    count++;
-                    i = 0;
-                }
+                if(j == n2) return true;
+            }else if(j != 0){
+                j = lps[j-1];
             }else{
-                return -1;
-            }
-        }
-        return count;
-    }
-public:
-    int repeatedStringMatch(string a, string b) {
-        for(int i =0; i<a.size(); i++){
-            if(a[i] == b[0]){
-                int count = getCount(i, a, b);
-                if(count != -1) return count;
+                i++;
             }
         }
 
+        return false;
+
+        
+
+    }
+public:
+    int repeatedStringMatch(string a, string b) {
+        string str = a;
+        int ans = 1;
+        while(str.size() < b.size()){
+            str += a;
+            ans++;
+        }
+        if(solve(str,b)) return ans;
+        str += a;
+        ans++;
+        if(solve(str,b)) return ans;
         return -1;
+
         
     }
 };
