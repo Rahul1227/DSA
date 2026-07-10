a579ed6d5fe56a84c9fb14a4422d621ae5eff868
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-10 11:49:29 +0530

    Time: 29 ms (60.04%) | Memory: 27 MB (53.98%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,38 @@
+// using pii = pair<int,int>;
+class Solution {
+private:
+    int solve(int i, int j, string &s, string &t, vector<vector<int>> &dp){
+     
+        // base case
+        if( j ==0){
+            return 1;
+        }
+        
+        if(i ==0){
+            if(j == 0){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int skip = solve(i-1, j, s,t, dp);
+        int take = 0;
+        if(s[i-1] == t[j-1]){
+            take = solve(i-1,j-1,s,t,dp);
+        }
+
+        return dp[i][j] = skip + take;
+    }
+public:
+    int numDistinct(string s, string t) {
+        int n1 = s.size();
+        int n2 = t.size();
+
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        return solve(n1, n2, s, t, dp);
+        
+    }
+};
