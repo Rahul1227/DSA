05802d5804529eae45fc995b8dc52b4e12d851e0
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-10 10:56:14 +0530

    Time: 3 ms (48.51%) | Memory: 9.3 MB (39.70%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,43 +1,72 @@
 class Solution {
 private:
-    bool solve(int i, int j, string &s, string &p, vector<vector<int>> &dp){
+    bool getAnswer(int j, string& p) {
+        while (j > 0) {
+            if (p[j - 1] != '*')
+                return false;
+            j = j - 2;
+        }
+        return true;
+    }
+
+    bool solve(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
         // base case
-        if(i ==0){
-            while(j>0){
-                if(p[j-1] != '*') return false;
-                j = j-2;
-            }
-            return true;
+        if (i == 0) {
+            return getAnswer(j, p);
         }
 
-        if(j==0){
-            return i ==0;
+        if (j == 0) {
+            return i == 0;
         }
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         bool skipI = false;
         bool skipJ = false;
 
-        if(s[i-1] == p[j-1] || p[j-1] == '.'){
-            return dp[i][j] = solve(i-1, j-1, s, p, dp);
-        }else if(p[j-1] == '*'){
-            if(p[j-2] == s[i-1] || p[j-2] == '.'){
-                skipI = solve(i-1,j,s,p,dp);
+        if (s[i - 1] == p[j - 1] || p[j - 1] == '.') {
+            return dp[i][j] = solve(i - 1, j - 1, s, p, dp);
+        } else if (p[j - 1] == '*') {
+            if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
+                skipI = solve(i - 1, j, s, p, dp);
             }
-            skipJ = solve(i, j-2, s, p, dp);
+            skipJ = solve(i, j - 2, s, p, dp);
             return dp[i][j] = skipI || skipJ;
-            
-        }else{
+
+        } else {
             return dp[i][j] = false;
         }
     }
+
 public:
     bool isMatch(string s, string p) {
         int n1 = s.size();
         int n2 = p.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1, -1));
-        return solve(n1, n2, s, p, dp);
-        
+        // vector<vector<int>> dp(n1+1,vector<int>(n2+1, -1));
+        // return solve(n1, n2, s, p, dp);
+        vector<vector<bool>> dp(n1 + 1, vector<bool>(n2 + 1, false));
+        dp[0][0] = true;
+        for (int j = 1; j <= n2; j++) {
+            dp[0][j] = getAnswer(j, p);
+        }
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                bool skipI = false;
+                bool skipJ = false;
+
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '.') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (p[j - 1] == '*') {
+                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
+                        skipI = dp[i-1][j];
+                    }
+                    skipJ = dp[i][j-2];
+                    dp[i][j] = skipI || skipJ;
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 };
