b958c764596641f6d9681925767612a0206efbe2
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-11 11:00:53 +0530

    Time: 136 ms (5.07%) | Memory: 144 MB (11.97%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,26 +1,35 @@
 class Solution {
 private:
-    bool isComplete(unordered_map<int,int> &nodes, vector<vector<int>> &adj){
-        for(auto [node, count] : nodes){
-            if(adj[node].size() < nodes.size() -1) return false;
-            for(auto v: adj[node]){
-                if(!nodes.count(v)) return false;
-            }
-        }
-        return true;
+    // bool isComplete(unordered_map<int,int> &nodes, vector<vector<int>> &adj){
+    //     int edges = 0;
+    //     for(auto [node, count] : nodes){
+    //         // if(adj[node].size() < nodes.size() -1) return false;
+    //         // for(auto v: adj[node]){
+    //         //     if(!nodes.count(v)) return false;
+    //         // }
+    //         edges += adj[node].size();
+    //     }
+    //     int nodeCount = nodes.size();
 
-    }
+    //     nodeCount = (nodeCount * (nodeCount-1)) / 2;
+    //     // cout<<nodeCount<<" "<<edgeCount<<endl;
+    //     return edges ==  nodeCount;
+
+    // }
     int bfs(int ind, vector<vector<int>> &adj, vector<int> &visited){
         queue<int> q;
         visited[ind] = 1;
         q.push(ind);
-        unordered_map<int, int> nodes;
+        int nodeCount = 0;
+        int edgeCount = 0;
 
         while(!q.empty()){
             int u = q.front();
             q.pop();
-            nodes[u]=1;
+            nodeCount++;
+            // nodes[u]=1;
             for(auto v : adj[u]){
+                edgeCount++;
                 if(!visited[v]){
                     visited[v] = 1;
                     q.push(v);
@@ -28,10 +37,10 @@ private:
             }
         }
 
-        if(isComplete(nodes, adj)){
-            return 1;
-        }
-        return 0;
+        nodeCount = nodeCount * (nodeCount -1);
+        cout<<nodeCount<<" "<<edgeCount<<endl;
+
+        return nodeCount == edgeCount;
 
         
 
