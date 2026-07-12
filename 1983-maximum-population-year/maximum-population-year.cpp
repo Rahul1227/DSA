60ad81582f6ad9c2f07fdbc2ef7fa3e29e60d885
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-12 12:49:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (8.38%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
         int maxPop = 0;
-        int year = 0;
         int currPop = 0;
-        vector<vector<int>> events;
-        for(auto log : logs){
+        int year;
+        vector<int> diff(2051);
+        for(auto log: logs){
             int birth = log[0];
             int death = log[1];
-            events.push_back({birth, 1});
-            events.push_back({death, -1});
+            diff[birth]++;
+            diff[death]--;
         }
 
-        sort(events.begin(), events.end());
-
-        for(int i =0; i<events.size(); i++){
-            currPop += events[i][1];
+        for(int i=0; i<2051; i++){
+            currPop += diff[i];
             if(currPop > maxPop){
                 maxPop = currPop;
-                year = events[i][0];
+                year = i;
             }
-
         }
-
         return year;
         
     }
