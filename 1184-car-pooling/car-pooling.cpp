8f7d173f95b792e726ba5a2e3a4a71378424922e
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-12 16:09:22 +0530

    Time: 2 ms (36.80%) | Memory: 14.7 MB (22.90%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        map<int,int> events;
+        // better solution
+        vector<int> diff(1001, 0);
         for(auto trip : trips){
             int num = trip[0];
             int from = trip[1];
             int to = trip[2];
-            events[from] += num;
-            events[to] -=num;
+            diff[from] += num;
+            diff[to] -=num;
         }
 
-        int currPassenger = 0;
-        for(auto [key, val] : events){
-            currPassenger += val;
-            if(currPassenger > capacity){
+        int currPass = 0;
+        for(auto num: diff){
+            currPass +=  num;
+            if(currPass > capacity){
                 return false;
             }
         }
-
         return true;
+        
+
+
+
+        // O(nlogn)
+        // map<int,int> events;
+        // for(auto trip : trips){
+        //     int num = trip[0];
+        //     int from = trip[1];
+        //     int to = trip[2];
+        //     events[from] += num;
+        //     events[to] -=num;
+        // }
+
+        // int currPassenger = 0;
+        // for(auto [key, val] : events){
+        //     currPassenger += val;
+        //     if(currPassenger > capacity){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
         
     }
 };
