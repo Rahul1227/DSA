1bacb3bf19e2a4a524883030dce2c251ecb0328e
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-14 10:42:40 +0530

    Time: 4 ms (22.22%) | Memory: 16.7 MB (11.88%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -10,40 +10,37 @@
  * };
  */
 class Solution {
+private:
+    void solve(TreeNode* root, int col, int row, map<int, map<int,vector<int>>> &mp){
+        if(!root) return;
+
+        mp[col][row].push_back(root->val);
+        solve(root->left, col-1, row+1, mp);
+        solve(root->right, col+1, row+1, mp);
+
+
+    }
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int, map<int, vector<int>>> mp;
+        solve(root, 0,0, mp);
+
         vector<vector<int>> ans;
-        // line ==> level ==> multiset
-        map<int, map<int,vector<int>>> mp;
-        //node, line, level
-        queue<tuple<TreeNode*, int , int>> q;
-        q.push({root,0,0});
-        while(!q.empty()){
-            auto [node, line, level] = q.front();
-            q.pop();
-            mp[line][level].push_back(node->val);
-            if(node->left){
-                q.push({node->left,line-1, level +1});
-            }
-            if(node->right){
-                q.push({node->right, line+1, level +1});
-            }
-        }
 
-
-        for(auto [key, val] : mp){
+        for(auto& [key, val] : mp){
             vector<int> temp;
-            for(auto [key2, val2] :val){
+            for(auto &[key2, val2] : val){
                 sort(val2.begin(), val2.end());
-                for(auto num : val2){
+                for(auto num: val2){
                     temp.push_back(num);
                 }
+                
             }
-
             ans.push_back(temp);
         }
 
         return ans;
+        
         
     }
 };
