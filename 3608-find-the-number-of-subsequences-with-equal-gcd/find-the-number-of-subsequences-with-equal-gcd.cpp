0cabed12bef9a2ee768b2b2afb1b2da24202bb31
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-15 22:07:28 +0530

    Time: 2201 ms (5.21%) | Memory: 94 MB (53.04%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,13 +1,13 @@
 const int MOD = 1e9+7;
 class Solution {
 public:
-    long long dp[205][205][205];
+    long long dp[201][201][201];
 
-    int getGCD (int a, int b){
-        if(b == 0) return a;
+    // int getGCD (int a, int b){
+    //     if(b == 0) return a;
 
-        return getGCD(b, a%b);
-    }
+    //     return getGCD(b, a%b);
+    // }
 
     long long solve(int ind, int gcd1, int gcd2, vector<int> &nums){
         //base case
@@ -17,10 +17,10 @@ public:
 
         if(dp[ind][gcd1][gcd2] != -1) return dp[ind][gcd1][gcd2];
 
-        int updatedGCD1 = getGCD(gcd1, nums[ind]);
-        int updatedGCD2 = getGCD(gcd2, nums[ind]);
-        long long take1 = solve(ind+1,updatedGCD1, gcd2, nums);
-        long long take2 = solve(ind+1, gcd1, updatedGCD2, nums);
+        // int updatedGCD1 = getGCD(gcd1, nums[ind]);
+        // int updatedGCD2 = getGCD(gcd2, nums[ind]);
+        long long take1 = solve(ind+1,__gcd(gcd1, nums[ind]), gcd2, nums);
+        long long take2 = solve(ind+1, gcd1, __gcd(gcd2, nums[ind]), nums);
         long long skip = solve(ind+1, gcd1, gcd2, nums);
 
         return dp[ind][gcd1][gcd2] = (1LL*take1 + (take2 + skip) % MOD) % MOD;
