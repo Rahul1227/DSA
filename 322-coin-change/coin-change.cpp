0e2b07de96d301b751bb9780222fd6546559f309
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-17 13:22:53 +0530

    Time: 51 ms (24.33%) | Memory: 53.1 MB (10.50%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -23,10 +23,29 @@ private:
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
-        int result = solve(n, amount, dp, coins);
+        // vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        // int result = solve(n, amount, dp, coins);
 
-        return (result >= INT_MAX/2 ? -1 : result);
+        // return (result >= INT_MAX/2 ? -1 : result);'
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, INT_MAX/2));
+        // dp[0][0] = 0;
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 0;
+        }
+        for(int i =1; i<=n; i++){
+            for(int currSum = 1; currSum<=amount; currSum++){
+                int take = INT_MAX/2;
+                if(currSum >= coins[i-1]){
+                    take = 1 + dp[i][currSum - coins[i-1]];
+                }
+                int noTake = dp[i-1][currSum];
+
+                dp[i][currSum] = min(take, noTake);
+            }
+        }
+
+        int result = dp[n][amount];
+        return result >= INT_MAX/2 ? -1 : result;
         
     }
 };
