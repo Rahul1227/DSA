7ac654dbd9c4cd0fbd630268d788bda9721558a8
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-19 21:56:54 +0530

    Time: 4 ms (44.81%) | Memory: 18.3 MB (37.23%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,40 +1,41 @@
+using pii = pair<int,int>;
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        int PossibleIteration = k+1;
         vector<int> dist(n, INT_MAX);
-        vector<vector<pair<int,int>>> adj(n);
-        for(int i=0; i< flights.size(); i++){
-            int u = flights[i][0];
-            int v = flights[i][1];
-            int w = flights[i][2];
+        dist[src] = 0;
+        queue<pii> q;
+        q.push({src, 0});
+        k++;
+        vector<vector<pii>> adj(n);
+
+        for(auto edge : flights){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
             adj[u].push_back({v,w});
         }
 
-        queue<pair<int,int>> q;
-        
-        q.push({src,0});
-        dist[src] = 0;
-
-        while(!q.empty() && PossibleIteration){
+        while(!q.empty() && k>0){
             int len = q.size();
-            for(int times =0; times<len; times++){
-                auto [u, distance] = q.front();
+            for(int i=0; i<len; i++){
+                auto [u, weight] = q.front();
                 q.pop();
 
-                for(auto [v, w] : adj[u]){
-                    int newDist = distance + w;
+                for(auto [v,w] :  adj[u]){
+                    int newDist = weight + w;
                     if(newDist < dist[v]){
                         dist[v] = newDist;
                         q.push({v, newDist});
                     }
                 }
+
             }
-            PossibleIteration--;
-            
+            k--;
         }
 
-        return dist[dst] != INT_MAX ? dist[dst] : -1;
+        if(dist[dst] >= INT_MAX) return -1;
+        return dist[dst];
         
     }
 };
