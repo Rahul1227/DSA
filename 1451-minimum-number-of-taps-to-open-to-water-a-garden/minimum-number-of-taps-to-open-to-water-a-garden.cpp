faf6ea95f050be356157a83890525f3615ec3d7a
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-22 12:35:45 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (82.11%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
+        // int n = ranges.size();
         vector<int> intervals(n+1);
-        for(int i =0; i<ranges.size(); i++){
-            int startPoint = max(0, i - ranges[i]);
-            int endPoint = min(n, i+ranges[i]);
-            intervals[startPoint] = max(intervals[startPoint], endPoint);
-        }
-        int count =0;
-        int maxReach = 0;
-        int currReach =0;
-        
         for(int i =0; i<=n; i++){
-           if(i > maxReach) return -1;
+            int start = max(0, i - ranges[i]);
+            int end = min(n, i + ranges[i]);
+            intervals[start] = max(intervals[start], end);
+        }
 
-            if(i > currReach){
-                currReach = maxReach;
+        int currEnd = 0;
+        int maxEnd = 0;
+        int count =0;
+
+        for(int i=0; i<=n; i++){
+            if(i > maxEnd) return -1;
+
+            if(i > currEnd){
                 count++;
-            }
-            maxReach = max(maxReach, intervals[i]);
+                currEnd = maxEnd;
 
+            }
+
+            maxEnd = max(maxEnd, intervals[i]);
         }
 
         return count;
