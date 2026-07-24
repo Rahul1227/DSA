ec35cafa035bae3445f963e0ec6e92e8002e1731
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-24 20:05:17 +0530

    Time: 4 ms (67.42%) | Memory: 11.6 MB (63.64%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char,int> mp;
         vector<int> ans;
-        int n1 = s.size();
-        int n2 = p.size();
-        if(n2 > n1) return ans;
-        for(auto c: p){
+        int n = s.size();
+        int m = p.size();
+        if(m > n) return ans;
+        unordered_map<char, int> mp;
+        for(auto c : p){
             mp[c]++;
         }
-        int count = n2;
-        int i = 0;
+        int count = m;
+        int i =0;
         int j =0;
-        while(j < n1){
+
+        while(j < n){
             mp[s[j]]--;
             if(mp[s[j]] >=0) count--;
 
-            if(j-i+1 > n2){
+            if(j-i+1 > m){
                 mp[s[i]]++;
                 if(mp[s[i]] > 0) count++;
                 i++;
@@ -24,11 +25,12 @@ public:
 
             if(count == 0){
                 ans.push_back(i);
+
             }
             j++;
+
         }
 
         return ans;
-
     }
 };
