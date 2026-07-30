4c7e4a0b487b16d75f7c4c8cf9bb21a567804aba
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-30 22:20:24 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (24.25%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.cpp
+++ 1242-matrix-block-sum/matrix-block-sum.cpp
@@ -1,44 +1,39 @@
 class Solution {
-public:
+private:
     int fill(int row, int col, int k, vector<vector<int>> &prefixSum){
-        // cout<<"inside thhe fll"<<endl;
-        int n = prefixSum.size();
-        int m = prefixSum[0].size() - 1;
-        int leftCol = max(0,  col - k);
+        int n = prefixSum.size()-1;
+        int m = prefixSum[0].size()-1;
+
+        int leftCol = max(0, col-k);
         int rightCol = min(m-1, col+k);
 
-        int upRow = max(0, row -k);
-        int downRow = min(n-1, row + k);
+        int leftRow = max(0, row-k);
+        int rightRow = min(n-1, row+k);
 
-        int totalSum =0;
-        for(int i = upRow; i<= downRow; i++){
-            totalSum += prefixSum[i][rightCol+1] - prefixSum[i][leftCol];
-        }
-        // cout<<totalSum<<endl;
-        return totalSum;
-        
+        int val = prefixSum[rightRow+1][rightCol+1] - prefixSum[leftRow][rightCol+1] - prefixSum[rightRow+1][leftCol] + prefixSum[leftRow][leftCol];
+        return val;
     }
+public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-         // Your code here
         int n = mat.size();
         int m = mat[0].size();
 
-        vector<vector<int>> prefixSum(n, vector<int>(m+1, 0));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                prefixSum[i][j+1] = prefixSum[i][j] + mat[i][j];
+        vector<vector<int>> prefix(n+1, vector<int>(m+1, 0));
+
+        for(int i=1; i<=n; i++){
+            for(int j =1; j<=m; j++){
+                prefix[i][j] = mat[i-1][j-1] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
             }
         }
 
         vector<vector<int>> ans(n, vector<int>(m));
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                ans [i][j] = fill(i,j,k,prefixSum);
-                // cout<<ans[i][j]<<endl;
+                ans[i][j] = fill(i,j,k, prefix);
             }
         }
         return ans;
-    }
         
-    
+        
+    }
 };
