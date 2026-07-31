241283c5c789fc54c733f5b4b7e838e12f00f6f6
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-07-31 21:09:50 +0530

    Time: 37 ms (13.28%) | Memory: 89.6 MB (8.19%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,31 +1,42 @@
 class Solution {
-public:
-    int largestRectangleArea(vector<int>& height) {
-        // if no pse, pse = -1 (out of range)
-        // if no nse, nse = n (out of range)
-        int n = height.size();
-        int maxArea = INT_MIN;
+private:
+    vector<int> getNSE(vector<int> &arr){
+        int n = arr.size();
+        vector<int> nse(n, n);
         stack<int> st;
-        for(int i =0; i<n; i++){
-            while(!st.empty() && height[i] < height[st.top()]){
-                int currHeight = height[st.top()];
+        for(int i =0; i< n; i++){
+            while(!st.empty() && arr[i] < arr[st.top()]){
+                nse[st.top()] = i;
                 st.pop();
-                // for nse and pse, we are considering the indices
-                // indices are pushed into the stack
-                int nse = i;
-                int pse = (!st.empty() ? st.top() : -1);
-                int currArea = (nse - pse - 1) * currHeight;
-                maxArea = max(maxArea, currArea);
             }
             st.push(i);
         }
+        return nse;
+    }
 
-        while(!st.empty()){
-            int currHeight = height[st.top()];
-            st.pop();
-            int nse = n;
-            int pse = (!st.empty()? st.top():-1);
-            int currArea = (nse-pse-1) * currHeight;
+    vector<int> getPSE(vector<int> &arr){
+        int n = arr.size();
+        vector<int> pse(n,-1);
+        stack<int> st;
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && arr[i] < arr[st.top()]){
+                pse[st.top()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        return pse;
+
+    }
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int> nse = getNSE(heights);
+        vector<int> pse = getPSE(heights);
+        int maxArea = 0;
+
+        for(int i =0; i< heights.size(); i++){
+            int weight = nse[i] - pse[i] -1;
+            int currArea = weight * heights[i];
             maxArea = max(maxArea, currArea);
         }
 
