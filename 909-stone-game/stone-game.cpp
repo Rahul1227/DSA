9b40849814f6d70c4f355d500bb35521a008acd3
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-02 20:50:17 +0530

    Time: 14 ms (21.59%) | Memory: 19.5 MB (31.69%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,24 +1,23 @@
 class Solution {
 private:
-    int solve(int i, int j, vector<vector<int>> &dp, vector<int> &nums){
-        if(i>j) return 0;
+    int solve(int i, int j, vector<vector<int>>&dp, vector<int> &piles){
+        if(i > j) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
-        
-        int takeI = nums[i] + min(solve(i+2, j,dp,nums), solve(i+1, j-1,dp, nums));
-        int takeJ = nums[j] + min(solve(i+1, j-1, dp, nums), solve(i, j-2, dp, nums));
 
-        return dp[i][j]=max(takeI, takeJ);
+        int takeFirst = piles[i] + min(solve(i+2,j,dp,piles), solve(i+1,j-1, dp,  piles));
+
+        int takeLast = piles[j] + min(solve(i, j-2,  dp, piles), solve(i+1, j-1, dp, piles));
+
+        return dp[i][j] = max(takeFirst, takeLast);
     }
-
-
 public:
-    int stoneGame(vector<int>& piles) {
+    bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        int AliceScore =  solve(0, n-1, dp, piles);
-        int totalPossibleScore = accumulate(piles.begin(), piles.end(), 0);
-        int BobScore = totalPossibleScore - AliceScore;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        int AliceScore = solve(0, n-1, dp, piles);
+        int totalScore = accumulate(piles.begin(), piles.end(), 0);
+        int BobScore = totalScore - AliceScore;
         return AliceScore > BobScore;
         
     }
