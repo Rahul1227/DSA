32400a0947717e58d34c7e64ac7bd2c0969ef47f
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-02 20:23:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (32.41%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,25 +1,25 @@
 class Solution {
 private:
-    int solve(int i, int j, vector<vector<int>> &dp, vector<int> &nums){
-        if(i>j) return 0;
+    long long solve(int i, int j, vector<vector<long long>> &dp, vector<int> &nums){
+        if(i > j){
+            return 0;
+        }
 
         if(dp[i][j] != -1) return dp[i][j];
 
         int takeI = nums[i] + min(solve(i+2, j, dp, nums), solve(i+1, j-1, dp, nums));
-        int takeJ = nums[j] + min(solve(i+1, j-1, dp, nums), solve(i, j-2, dp, nums));
 
-        return max(takeI, takeJ);
+        int takeJ = nums[j] + min(solve(i, j-2, dp, nums), solve(i+1, j-1, dp, nums));
+
+        return dp[i][j] = max(takeI, takeJ);
     }
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        int player1Score = solve(0,n-1, dp, nums);
-        int totalPossibleScore = accumulate(nums.begin(), nums.end(),0);
-        int player2Score = totalPossibleScore - player1Score;
+        vector<vector<long long>> dp(n, vector<long long>(n, -1));
+        long long totalSum = accumulate(nums.begin(), nums.end(), 0LL);
+        long long result = solve(0, n-1, dp, nums);
+        return 2* result >= totalSum;
 
-        return player1Score >= player2Score;
-
-        
     }
 };
