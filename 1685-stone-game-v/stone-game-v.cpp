c30d85efc31499d0e80dd2ed3c43da24dcb66d2e
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-03 10:55:26 +0530

    Time: 682 ms (32.45%) | Memory: 29 MB (6.73%) - LeetSync

---
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    int solve(int i, int j, vector<int> &stones, vector<int> &prefixSum, vector<vector<int>> &dp){
+        if(i >= j){
+            return 0;
+        }
+        
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int totalSum = prefixSum[j+1] - prefixSum[i];
+        int result = 0;
+        for(int k = i; k<=j; k++){
+            int leftSum = prefixSum[k+1] - prefixSum[i];
+            int rightSum = totalSum - leftSum;
+            if(leftSum < rightSum){
+                result = max(result, leftSum + solve(i, k,stones, prefixSum,dp));
+            }else if(rightSum < leftSum){
+                result = max(result, rightSum + solve(k+1, j, stones, prefixSum, dp));
+            }else{
+                 result = max({result, leftSum + solve(i, k,stones, prefixSum,dp), rightSum + solve(k+1, j, stones, prefixSum, dp)});
+            }
+        }
+
+        return dp[i][j] = result;
+
+           
+    }
+public:
+    int stoneGameV(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        vector<int> prefixSum(n+1, 0);
+
+        for(int i=0; i<n; i++){
+            prefixSum[i+1] = prefixSum[i] + stoneValue[i];
+        }
+
+        return solve(0,n-1, stoneValue, prefixSum, dp);
+        
+    }
+};
