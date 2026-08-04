7d290541bd964234081804b46077c4737935ff51
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-04 23:46:06 +0530

    Time: 545 ms (22.54%) | Memory: 57 MB (6.84%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-    int numTeams(vector<int>& rating) {
-        int n = rating.size();
-        int count = 0;
+    // ind, prevInd, len, flag
+    int dp[1005][1005][5][2];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if ((rating[i] < rating[j] && rating[j] < rating[k]) ||
-                        (rating[i] > rating[j] && rating[j] > rating[k])) {
-                        count++;
-                    }
+    int solve(int ind, int prevInd, int len, int inc, vector<int> &rating){
+        if(len == 0){
+            return 1;
+        }
+
+        if(ind == rating.size()){
+            return 0;
+        }
+
+        if(dp[ind][prevInd+1][len][inc] != -1) return dp[ind][prevInd+1][len][inc];
+
+        int take = 0;
+        if(len > 0){
+            if(inc){
+                if(prevInd == -1 || rating[ind] > rating[prevInd]){
+                    take = solve(ind+1, ind, len-1,inc,rating);
+                }
+            }else{
+                if(prevInd == -1 || rating[ind] < rating[prevInd]){
+                    take = solve(ind+1, ind, len-1, inc, rating);
                 }
             }
         }
 
-        return count;
+        int noTake = solve(ind+1, prevInd, len, inc, rating);
+
+
+
+        return dp[ind][prevInd+1][len][inc] = take + noTake;
+    }
+
+    int numTeams(vector<int>& rating) {
+        memset(dp, -1, sizeof(dp));
+        int sum1 = solve(0,-1,3,1,rating);
+        int sum2 = solve(0,-1,3,0,rating);
+        return sum1 + sum2;
+
+        
+        
     }
 };
