25ad84eba921b28d3a3813b2c3ab4612d09978b0
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-04 13:16:54 +0530

    Time: 60 ms (13.63%) | Memory: 66.2 MB (47.91%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<vector<int>> adj(n);
-        vector<int> indegree(n, 0);
-        for(int i =0; i<n; i++){
-            if(!graph[i].size()) continue;
-            int v = i;
-            for(auto u : graph[i]){
-                adj[u].push_back(v);
-                indegree[v]++;
+        int V = graph.size();
+        vector<int> outDegree(V, 0);
+        vector<vector<int>> adj(V);
+        for(int i=0; i<V; i++){
+            outDegree[i] = graph[i].size();
+            for(int j =0; j< graph[i].size(); j++){
+                adj[graph[i][j]].push_back(i);
+
             }
         }
 
+        vector<int> ans;
         queue<int> q;
-        for(int i =0; i<n; i++){
-            if(indegree[i] == 0){
+
+        for(int i =0; i<V; i++){
+            if(outDegree[i] == 0){
                 q.push(i);
             }
         }
-        vector<int> ans(n);
-        while(!q.empty()){
-            int u = q.front();
-            q.pop();
-            ans[u] = 1;
 
-            for(auto v : adj[u]){
-                indegree[v]--;
-                if(indegree[v] == 0){
-                    q.push(v);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto v : adj[node]){
+                if(outDegree[v] > 0){
+                    outDegree[v]--;
+                    if(outDegree[v] == 0){
+                        q.push(v);
+                    }
                 }
             }
         }
-        vector<int> finalAns;
-        for(int i =0; i<n; i++){
-            if(ans[i] ==1){
-                finalAns.emplace_back(i);
-            }
-        }
-        return finalAns;
 
+        sort(ans.begin(), ans.end());
+        return ans;
         
     }
 };
