e91af7df030acb54c21ca295490e4ba3a903c091
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-04 12:54:32 +0530

    Time: 55 ms (28.36%) | Memory: 61.7 MB (21.78%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.cpp
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -1,46 +1,51 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if(n == 1) return {0};
+        if(n == 1){
+            return {0};
+        }
         vector<vector<int>> adj(n);
-        vector<int> indegree(n);
-        for(int i =0; i<edges.size(); i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            indegree[u]++;
-            indegree[v]++;
+        vector<int> ans;
+        vector<int> degree(n, 0);
+
+        for(auto edge: edges){
+            int u = edge[0];
+            int v = edge[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
+            degree[u]++;
+            degree[v]++;
         }
 
         queue<int> q;
         for(int i =0; i<n; i++){
-            if(indegree[i] == 1){
+            if(degree[i] == 1){
                 q.push(i);
             }
         }
 
-        while(n > 2){
+        while(!q.empty()){
+            ans.clear();
             int len = q.size();
-            n -= len;
             for(int i =0; i<len; i++){
-                auto u = q.front();
+                int node = q.front();
                 q.pop();
-                for(auto v : adj[u]){
-                    indegree[v]--;
-                    if(indegree[v] == 1){
-                        q.push(v);
+                // if(degree[node] == 0) continue;
+                degree[node]--;
+                ans.push_back(node);
+
+                for(auto v: adj[node]){
+                    if(degree[v] > 0){
+                        degree[v]--;
+                        if(degree[v] == 1){
+                            q.push(v);
+                        }
                     }
                 }
             }
         }
-        vector<int> ans;
-        while(!q.empty()){
-            ans.push_back(q.front());
-            q.pop();
-        }
-
         return ans;
+
         
     }
 };
