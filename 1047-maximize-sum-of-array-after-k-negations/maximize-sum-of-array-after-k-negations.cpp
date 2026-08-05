48a901c9366956302e7a1a3a8164358140fee605
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-05 23:43:21 +0530

    Time: 7 ms (7.38%) | Memory: 12.9 MB (27.74%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int i =0;
-        int n = nums.size();
-        while(i<n && k){
-            if(nums[i] < 0){
-                nums[i] = - nums[i];
-                i++;
-                k--;
-            }else{
-                if(k%2 != 0){
-                    if(i> 0 && nums[i-1] < nums[i]){
-                        nums[i-1] = -nums[i-1];
-                    }else{
-                        nums[i] = -nums[i];
-                    }
-                    
-                }
-                k = 0;
-            }
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto num : nums){
+            pq.push(num);
         }
 
-        if(k > 0){
-            if(k % 2 != 0){
-                nums[n-1] = -nums[n-1];
-            }
+        while(k--){
+            int ele = pq.top();
+            pq.pop();
+            ele = -ele;
+            pq.push(ele);
+            // k--;
         }
 
-        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        int totalSum =0;
+
+        while(!pq.empty()){
+            totalSum += pq.top();
+            pq.pop();
+        }
         return totalSum;
         
     }
