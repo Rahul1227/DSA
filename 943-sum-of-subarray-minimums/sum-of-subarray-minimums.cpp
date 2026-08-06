622af747ebd3b9db6abb4fe6148dbd1d5fc90357
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-06 13:13:51 +0530

    Time: 44 ms (91.58%) | Memory: 178 MB (92.93%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,44 @@
+static const int MOD = 1e9 + 7;
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        // 2 pass solution is easy
+        // get pse
+        // get nse
+        // left count = i- pse(i)
+        // right count = i+ nse[i];
+        // sum += leftCount * rightCount * ar[i];
+
+        stack<int> st;
+        int n = arr.size();
+        long long totalSum = 0;
+        for(int i =0; i<n; i++){
+            while(!st.empty() && arr[i] < arr[st.top()]){
+                int currInd = st.top();
+                st.pop();
+                int pse = !st.empty() ? st.top() : -1;
+                int nse = i;
+                int leftCount = currInd - pse;
+                int rightCount = nse - currInd;
+                long long currSum = (1LL * arr[currInd] * leftCount * rightCount) % MOD;
+                totalSum = (1LL * totalSum + currSum) % MOD;
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            int currInd = st.top();
+            st.pop();
+            int pse = !st.empty() ? st.top() : -1;
+            int nse = n;
+            int leftCount = currInd - pse;
+            int rightCount = nse - currInd;
+            long long currSum = (1LL * arr[currInd] * leftCount * rightCount) % MOD;
+            totalSum = (1LL * totalSum + currSum) % MOD;
+
+        }
+
+        return totalSum;
+        
+    }
+};
