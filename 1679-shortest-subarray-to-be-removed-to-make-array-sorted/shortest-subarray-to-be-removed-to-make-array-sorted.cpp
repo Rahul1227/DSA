ee766e4cb3e0b02df8b4c391d470aba6a50c1a19
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-08 18:48:46 +0530

    Time: 0 ms (100.00%) | Memory: 90.5 MB (19.58%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,70 +1,40 @@
 class Solution {
-private:
-    bool isPossible(int targetLen, vector<int> &arr, vector<bool> &prefix, vector<bool> &suffix){
-        int i =0;
-        int n = arr.size();
-        for(int j=targetLen-1; j<n; j++){
-            bool isValid = true;
-            isValid = isValid && (i-1 >=0 ? prefix[i-1] : true);
-            if(isValid){
-                if(j+1<n && i-1 >=0 && arr[i-1] > arr[j+1]){
-                    isValid = false;
-
-                }
-            }
-
-            isValid = isValid && (j+1 < n ? suffix[j+1] : true);
-
-            if(isValid) return true;
-
-            i++;
-
-
-        }
-
-        return false;
-    }
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
         int n = arr.size();
-        int l =0;
-        int r = n-1;
-        int ans = 0;
-        // precompute
-        vector<bool> isPrefixSorted(n, false);
-        isPrefixSorted[0] = true;
-        vector<bool> isSuffixSorted(n, false);
-        isSuffixSorted[n-1] = true;
 
-        for(int i=1; i<n; i++){
-            if(arr[i] >= arr[i-1]){
-                isPrefixSorted[i] = true;
-            }else{
-                break;
-            }
+        // Find the first index of the sorted suffix
+        int right = n - 1;
+
+        while (right > 0 && arr[right] >= arr[right - 1]) {
+            right--;
         }
 
-        for(int i=n-2; i>=0; i--){
-            if(arr[i+1] >= arr[i]){
-                isSuffixSorted[i] = true;
-            }else{
+        // Delete everything before right
+        int ans = right;
+
+        // Keep a sorted prefix [0...i]
+        int i = 0;
+        int j = right;
+
+        while (i < right) {
+
+            // Prefix [0...i] must remain sorted
+            if (i > 0 && arr[i] < arr[i - 1]) {
                 break;
             }
-        }
 
-
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(isPossible(mid,arr, isPrefixSorted, isSuffixSorted)){
-                ans = mid;
-                r = mid-1;
-            }else{
-                l = mid+1;
+            // Move suffix start until it can connect
+            while (j < n && arr[j] < arr[i]) {
+                j++;
             }
+
+            // Delete [i+1 ... j-1]
+            ans = min(ans, j - i - 1);
+
+            i++;
         }
 
         return ans;
-
-        
     }
 };
