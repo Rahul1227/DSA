f6e19b7620fc0d6b46c287e045edec84f7be8dc4
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-10 22:56:38 +0530

    Time: 19 ms (28.62%) | Memory: 38.7 MB (19.56%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -22,8 +22,24 @@ private:
         return ans;
     }
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return getCount(nums, goal) - getCount(nums, goal-1);
-        
+    int numSubarraysWithSum(vector<int>& arr, int goal) {
+        // return getCount(nums, goal) - getCount(nums, goal-1);
+        int ans = 0;
+        unordered_map<int,int> mp;
+        mp[0] =1;
+        int currSum =0;
+        for(int i=0; i<arr.size(); i++){
+            currSum += arr[i];
+            int req = currSum - goal;
+            if(mp.count(req)){
+                ans += mp[req];
+            }
+            
+            
+            
+            mp[currSum]++;
+        }
+        return ans;
     }
+    
 };
