6015092b6c5fd8d37d05736f4b24871bf11e4ed5
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-10 16:04:10 +0530

    Time: 10 ms (89.15%) | Memory: 12.5 MB (82.21%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
-    // ind, prevInd, len, flag
-    int dp[1005][1005][5][2];
+    int numTeams(vector<int>& rating) {
+        int n = rating.size();
+        int ans = 0;
+        for(int i=1; i<n-1; i++){
+            int currEle = rating[i];
 
-    int solve(int ind, int prevInd, int len, int inc, vector<int> &rating){
-        if(len == 0){
-            return 1;
-        }
+            int leftSmaller = 0;
+            int leftGreater = 0;
+            int rightSmaller = 0;
+            int rightGreater = 0;
 
-        if(ind == rating.size()){
-            return 0;
-        }
-
-        if(dp[ind][prevInd+1][len][inc] != -1) return dp[ind][prevInd+1][len][inc];
-
-        int take = 0;
-        if(len > 0){
-            if(inc){
-                if(prevInd == -1 || rating[ind] > rating[prevInd]){
-                    take = solve(ind+1, ind, len-1,inc,rating);
-                }
-            }else{
-                if(prevInd == -1 || rating[ind] < rating[prevInd]){
-                    take = solve(ind+1, ind, len-1, inc, rating);
+            // scanning left
+            for(int j=0; j<i; j++){
+                if(rating[j] < currEle){
+                    leftSmaller++;
+                }else if(rating[j] > currEle){
+                    leftGreater++;
                 }
             }
+
+            // scanning right
+            for(int j=i+1; j<n; j++){
+                if(rating[j] < currEle){
+                    rightSmaller++;
+                }else if(rating[j] > currEle){
+                    rightGreater++;
+                }
+            }
+
+            ans += leftSmaller * rightGreater;
+            ans += leftGreater * rightSmaller;
         }
 
-        int noTake = solve(ind+1, prevInd, len, inc, rating);
-
-
-
-        return dp[ind][prevInd+1][len][inc] = take + noTake;
-    }
-
-    int numTeams(vector<int>& rating) {
-        memset(dp, -1, sizeof(dp));
-        int sum1 = solve(0,-1,3,1,rating);
-        int sum2 = solve(0,-1,3,0,rating);
-        return sum1 + sum2;
-
-        
-        
+        return ans;
     }
 };
