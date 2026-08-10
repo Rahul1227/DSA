320be85c4a9271dcd2913c55365b880a2aa0f238
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-10 23:44:20 +0530

    Time: 271 ms (9.60%) | Memory: 160.2 MB (8.82%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
@@ -1,23 +1,72 @@
+class SegmentTree{
+public:
+    vector<long long> seg;
+
+    SegmentTree(int n){
+        seg.assign(4*n, 0);
+    }
+
+    void update(int i, int l, int r, int ind, int val){
+        if(l == r){
+            seg[i] += val;
+            return;
+        }
+
+        int mid = l + (r-l)/2;
+
+        if(ind <= mid){
+            update(2*i+1, l, mid, ind, val);
+        }else{
+            update(2*i+2, mid+1, r, ind,val);
+        }
+
+        seg[i] = 1LL * seg[2*i+1] + seg[2*i+2];
+    }
+
+    long long getVal(int i, int l, int r, int start, int end){
+        if(r < start || l > end){
+            return 0;
+        }else if(start <= l && r<= end){
+            return seg[i];
+        }else{
+            int mid = l + (r-l)/2;
+            return 1LL *getVal(2*i+1, l, mid, start, end) + getVal(2*i+2, mid+1, r, start, end);
+        }
+    }
+
+
+};
+
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
         int n = nums.size();
-        long long ans = 0;
-        unordered_map<int,int> mp;
-        mp[0] = 1;
-        int cumSum =0;
-        long long validSubarray = 0;
-        for(int i =0; i<n; i++){
-            if(nums[i] == target){
-                validSubarray += mp[cumSum];
-                cumSum++;
-            }else{
-                cumSum--;
-                validSubarray -= mp[cumSum];
-            }
-            ans += validSubarray;
-            mp[cumSum]++;
+        vector<int> prefixSum(n+1, 0);
+
+        // getting the prefixSum
+        for(int i=0; i<n; i++){
+            prefixSum[i+1] = prefixSum[i] + (nums[i] == target ? 1: -1);
         }
+
+        set<int> values(prefixSum.begin(), prefixSum.end());
+
+        unordered_map<int,int> mp;
+        int rank =0;
+        for(auto x: values){
+            mp[x] = rank++;
+
+        }
+
+        long long ans;
+        SegmentTree st(rank);
+
+        for(int i =0; i<=n; i++){
+            int currInd = mp[prefixSum[i]];
+            long long val = st.getVal(0,0,rank-1,0,currInd-1);
+            ans += val;
+            st.update(0,0,rank-1,currInd,1);
+        }
+
         return ans;
         
     }
