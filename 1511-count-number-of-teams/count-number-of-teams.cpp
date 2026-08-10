5ac831c66b2d3e3937d0bddfbabb15285f4ef52d
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-10 17:13:22 +0530

    Time: 23 ms (68.04%) | Memory: 32.3 MB (11.26%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -1,38 +1,74 @@
+class SegmentTree{
+public:
+    vector<int> seg;
+
+    void reStart(int n){
+        seg.assign(4*n,0);
+    }
+
+    void update(int i, int l, int r, int ind, int val){
+        if(l == r){
+            seg[i] = val;
+            return;
+        }
+
+        int mid = l + (r-l)/2;
+        if(ind <= mid){
+            update(2*i+1, l, mid, ind, val);
+        }else{
+            update(2*i+2, mid+1, r, ind, val);
+        }
+
+        seg[i] = seg[2*i+1] + seg[2*i+2];
+    }
+
+    int getVal(int i, int l, int r, int start, int end){
+        if(r < start || l  > end){
+            return 0;
+        }else if(start <=l && r<= end){
+            return seg[i];
+        }else{
+            int mid = l + (r-l)/2;
+            return getVal(2*i+1, l, mid, start, end) + getVal(2*i+2, mid+1,r, start, end);
+        }
+    }
+};
+
+
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
         int n = rating.size();
         int ans = 0;
-        for(int i=1; i<n-1; i++){
+        int maxEle = *max_element(rating.begin(), rating.end());
+        SegmentTree st;
+        st.reStart(maxEle+1);
+        vector<int> leftSmaller(n, 0);
+        vector<int> leftGreater(n, 0);
+
+
+        for(int i=0; i<n; i++){
             int currEle = rating[i];
+            int smaller = st.getVal(0,0,maxEle,0,currEle-1);
+            int greater = st.getVal(0,0,maxEle,currEle+1, maxEle);
+            leftSmaller[i] = smaller;
+            leftGreater[i] = greater;
+            st.update(0,0,maxEle,currEle, 1);
+        }
+        st.reStart(maxEle+1);
+        for(int i=n-1; i>=0;i--){
+            int currEle = rating[i];
+            
+            int smaller = st.getVal(0,0,maxEle,0,currEle-1);
+            int greater = st.getVal(0,0,maxEle,currEle+1, maxEle);
+            ans += smaller * leftGreater[i];
+            ans += greater * leftSmaller[i];
+            st.update(0,0,maxEle,currEle, 1);
 
-            int leftSmaller = 0;
-            int leftGreater = 0;
-            int rightSmaller = 0;
-            int rightGreater = 0;
-
-            // scanning left
-            for(int j=0; j<i; j++){
-                if(rating[j] < currEle){
-                    leftSmaller++;
-                }else if(rating[j] > currEle){
-                    leftGreater++;
-                }
-            }
-
-            // scanning right
-            for(int j=i+1; j<n; j++){
-                if(rating[j] < currEle){
-                    rightSmaller++;
-                }else if(rating[j] > currEle){
-                    rightGreater++;
-                }
-            }
-
-            ans += leftSmaller * rightGreater;
-            ans += leftGreater * rightSmaller;
+            
         }
 
         return ans;
+        
     }
 };
