ac63492b45d28727091f14d1f617ba660bec43f6
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-10 15:31:13 +0530

    Time: 36 ms (69.29%) | Memory: 18.3 MB (23.56%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,32 +1,24 @@
 class Solution {
-private:
-    bool solve(int Alice, int n, vector<vector<int>> &dp){
-        if(n <= 0){
-            return 1-Alice;
+private:    
+    bool solve(int n, vector<int> &dp){
+        if(n == 0){
+            return false;
         }
 
-        if(dp[Alice][n] != -1) return dp[Alice][n];
-        
-        bool result = Alice ? false : true;
-        for(int i=1; i<=sqrt(n); i++){
-            if(i*i <= n){
-                if(Alice){
-                    result = result || solve(1-Alice, n-i*i, dp);
-                }else{
-                    result = result && solve(1-Alice, n-i*i, dp);
-                }
-                
+        if(dp[n] != -1) return dp[n];
+
+        for(int i=1; i*i <=n; i++){
+            if(solve(n - i*i, dp) == false){
+                return dp[n] = true;
             }
         }
 
-        return dp[Alice][n] = result;
-
-
+        return dp[n] = false;
     }
 public:
     bool winnerSquareGame(int n) {
-        vector<vector<int>> dp(2, vector<int>(n+1, -1));
-        return solve(1, n, dp);
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
         
     }
 };
