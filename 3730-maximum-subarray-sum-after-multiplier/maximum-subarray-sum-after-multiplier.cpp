e9dffd4b66d51950c7e77d576f144c3d2cc84619
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-11 21:15:26 +0530

    Time: 79 ms (25.97%) | Memory: 215.2 MB (15.38%) - LeetSync

--- 3730-maximum-subarray-sum-after-multiplier/maximum-subarray-sum-after-multiplier.cpp
+++ 3730-maximum-subarray-sum-after-multiplier/maximum-subarray-sum-after-multiplier.cpp
@@ -1,41 +1,42 @@
-#define ll long long
-#define all(v) v.begin(),v.end()
-const ll inf = 1e18;
 class Solution {
+private:
+    long long T(int num, int k, bool mul){
+        if(mul){
+            return 1LL * num * k;
+        }else{
+            return num/k;
+        }
+    }
+
+    long long solve(vector<int> &nums, int k, bool mul){
+        int n = nums.size();
+        vector<long long> nT(n), cT(n), eT(n);
+        nT[0] = nums[0];
+        cT[0] = T(nums[0],k, mul);
+        eT[0] = LLONG_MIN/8;
+
+        for(int i=1; i<n; i++){
+            long long trans = T(nums[i],k, mul);
+            nT[i] = max(1LL *nT[i-1] + nums[i], (long long)nums[i]);
+            cT[i] = max({
+                        nT[i-1] +trans,
+                        cT[i-1] + trans,
+                        trans
+                    });
+            eT[i] = max(eT[i-1] + nums[i], cT[i-1] + nums[i]);
+
+        }
+
+        long long ans = LLONG_MIN;
+        for(int i=0; i<n; i++){
+            ans = max({ans, cT[i], eT[i]});
+        }
+        return ans;
+
+    }
 public:
-    ll div(ll x, int k) {
-        if (x >= 0) return x / k;
-        return -((-x) / k);
-    }
-
-    ll op(ll x, int k, ll flag) {
-        if (flag) return x * k;
-        return div(x, k);
-    }
-
-    ll f(vector<int>& v, int k, ll flag) {
-        int n = v.size();
-        vector<ll> dp1(n), dp2(n), dp3(n);
-
-        dp1[0] = v[0], dp2[0] = op(v[0] * 1LL, k, flag);
-        dp3[0] = -inf;
-
-        for (int i = 1; i < n; i++) {
-            ll val = op(v[i] * 1LL, k, flag);
-            dp1[i] = max(1LL * v[i], dp1[i - 1] + v[i]);
-            dp2[i] = max({val, dp1[i - 1] + val, dp2[i - 1] + val});
-            dp3[i] = max({dp2[i - 1] + v[i], dp3[i - 1] + v[i], dp2[i]});
-        }
-
-        ll ans = -inf;
-        for (int i = 0; i < n; i++) {
-            ans = max({ans, dp1[i], dp2[i], dp3[i]});
-        }
-        return ans;
-    }
-
-    long long maxSubarraySum(vector<int>& v, int k) {
-        ll ans = max(f(v, k, 0), f(v, k, 1));
-        return ans;
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        return max(solve(nums, k, true), solve(nums, k, false));
+        
     }
 };
