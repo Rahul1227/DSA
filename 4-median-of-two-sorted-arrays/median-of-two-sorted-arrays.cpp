a0f31adbe99ace55f5567c03579a92c61a969203
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-11 11:34:41 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (61.86%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,45 +1,53 @@
 class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> temp;
+private:
+    double solve(vector<int>& nums1, vector<int>& nums2){
         int n1 = nums1.size();
         int n2 = nums2.size();
-        int totalLen = n1 + n2;
+        int totalLen = n1+n2;
+        int parLen = (n1 + n2 + 1)/2;
+        int l = 0;
+        int r = n1;
 
-        int i =0;
-        int j =0;
+        while(l <= r){
+            int cut1 = l + (r-l)/2;
+            int cut2 = parLen - cut1;
 
-        while(i<n1 && j<n2){
-            if(nums1[i] < nums2[j]){
-                temp.push_back(nums1[i]);
-                i++;
-            }else{
-                temp.push_back(nums2[j]);
-                j++;
+            int l1 = (cut1 == 0 ? INT_MIN :nums1[cut1-1]);
+            int l2 = (cut2 == 0 ? INT_MIN: nums2[cut2-1]);
+
+            int r1 = (cut1 == n1 ? INT_MAX : nums1[cut1]);
+            int r2 = (cut2 == n2 ? INT_MAX : nums2[cut2]); 
+
+            if(l2 > r1){
+                l = cut1 + 1;
+            }else if(l1 > r2){
+                r = cut1 -1;
+            }else if(l1 <= r2 && l2 <= r1){
+                if(totalLen % 2 == 1){
+                    return max(l1, l2);
+                }else{
+                    double ans = (1.000 * max(l1, l2) + min(r1, r2)) / 2;
+                    return ans;
+                }
             }
         }
 
-        while(i<n1){
-            temp.push_back(nums1[i]);
-            i++;
-        }
+        return 0.00;
 
-        while(j<n2){
-            temp.push_back(nums2[j]);
-            j++;
-        }
 
-        int half = totalLen/2;
+    }
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
 
-        if(totalLen % 2 == 1){
-            return temp[half];
-
+        double result = 0.000;
+        if(n1 < n2){
+            result = solve(nums1, nums2);
         }else{
-            double ans = (1.00 * temp[half-1] + temp[half])/2;
-            return ans;
+            result = solve(nums2, nums1);
         }
 
-
-
+        return result;       
     }
 };
