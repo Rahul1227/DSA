10b881bcab932db4b659e7f98e11860bb0bdc9a0
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-13 13:53:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (57.48%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,35 +1,37 @@
 class Solution {
+private:
+    int getLen(int i, int j, string &s){
+        // int len = 1;
+        int n = s.size();
+        while(i>= 0 && j < n && s[i] == s[j]){
+            i--;
+            j++;
+        }
+        return j-i-1;
+    }
 public:
     string longestPalindrome(string s) {
+        int startInd = 0;
+        int maxLen = 1;
         int n = s.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        int maxLen = 0;
-        int startingPoint = -1;
-        
-        for(int l = 1; l<=n; l++){
-            for(int i=0; i+l-1<n; i++){
-                int j = i+l-1;
-                if(i == j){
-                    dp[i][j] = l;
-                }else if(i+1 == j){
-                    if(s[i] == s[j]){
-                        dp[i][j] = l;
-                    }
-                }else{
-                    if(s[i] == s[j] && dp[i+1][j-1] != 0){
-                        dp[i][j] = l;
-                    }
-                }
 
-                if(dp[i][j] > maxLen){
-                    maxLen = dp[i][j];
-                    startingPoint = i;
-                }
+        for(int i=0; i<n; i++){
+            int oddLen = getLen(i,i,s);
+
+            int evenLen = getLen(i,i+1, s);
+
+            int currLen = max(oddLen, evenLen);
+
+            if(currLen > maxLen){
+                maxLen = currLen;
+                startInd =i-((currLen-1)/2);
             }
+
         }
 
-        return s.substr(startingPoint, maxLen);
+        // cout<<startInd<<"   "<<maxLen<<endl;
 
+        return s.substr(startInd, maxLen);
         
     }
 };
