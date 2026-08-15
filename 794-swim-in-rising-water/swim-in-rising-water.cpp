afb3c0e598937b4b4397bfa741028bd0d00ba4e6
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-15 13:35:19 +0530

    Time: 3 ms (87.67%) | Memory: 14.2 MB (39.32%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,42 +1,71 @@
-using t = tuple<int,int,int>;
 class Solution {
-public:
-    int swimInWater(vector<vector<int>>& grid) {
+private:
+    bool isPossible(int r, int c,int mid, vector<vector<int>> &grid, vector<vector<int>> &visited){
+        if(grid[r][c] > mid){
+            return false;
+        }
         int row = grid.size();
         int col = grid[0].size();
 
-        int maxTime = 0;
+        if( r == row-1 && c == col-1){
+            return true;
+        }
 
-        vector<vector<int>> visited(row, vector<int>(col, 0));
-        // val, => i,j
-        priority_queue<t, vector<t>, greater<>> pq;
-        pq.push({grid[0][0], 0,0});
-        visited[0][0] =  1;
-        int dirX[] = {0,0,1,-1};
-        int dirY[] = {-1,1,0,0};
+        visited[r][c] = 1;
+        
 
-        while(!pq.empty()){
-            auto [val, r,c] = pq.top();
-            pq.pop();
-            maxTime = max(maxTime, val);
 
-            if(r == row-1 && c == col-1){
-                return maxTime;
+        static int dirX[] = {0,0,1,-1};
+        static int dirY[] = {-1,1,0,0};
+
+        
+
+        for(int z=0; z<4; z++){
+            int newR = r + dirX[z];
+            int newC = c + dirY[z];
+
+            if(newR >=0 && newR < row && newC >=0 && newC < col && !visited[newR][newC]){
+                if(isPossible(newR, newC,mid,grid, visited)) return true;
             }
 
-            for(int z=0; z<4; z++){
-                int newR = r + dirX[z];
-                int newC = c + dirY[z];
 
-                if(newR >=0 && newR < row && newC >=0 && newC < col && !visited[newR][newC]){
-                    pq.push({grid[newR][newC], newR, newC});
-                    visited[newR][newC] = 1;
-                }
+        }
+        // visited[r][c] =0;
+
+        return false;
+
+
+    }
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int l = grid[0][0];
+        int r = INT_MIN;
+
+        int row = grid.size();
+        int col = grid[0].size();
+
+        
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                // l = min(l, grid[i][j]);
+                r = max(r, grid[i][j]);
             }
         }
 
-        return 0;
-        
+        int ans = 0;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            vector<vector<int>> visited(row, vector<int>(col, 0));
+            if(isPossible(0,0,mid, grid, visited)){
+                ans = mid;
+                r = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
 
+        return ans;
+        
     }
 };
