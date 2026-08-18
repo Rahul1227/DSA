2d3e6f8777a7b676381464070dd04ebd21e5036c
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-18 12:14:58 +0530

    Time: 7 ms (21.42%) | Memory: 9.2 MB (9.65%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfThree(int n) {
         if (n <= 0) return 0;
 
-        double logVal = log2(n) / log2(3);
-         return abs(logVal - round(logVal)) < 1e-10;
+        double logVal = log(n) / log(3);
+        return abs(logVal - round(logVal)) < 1e-10;
     }
 };
