912d10d488e739203a11fe9fb76c38a3a944f7f4
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-19 12:43:37 +0530

    Time: 38 ms (65.65%) | Memory: 69.5 MB (74.18%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=1; i<nums.size(); i++){
             if(nums[i] <= nums[i-1]){
                 ans += (nums[i-1] - nums[i]) + 1;
-                nums[i] = nums[i-1] + 1;
+                nums[i] += (nums[i-1] - nums[i]) + 1;
             }
         }
 
