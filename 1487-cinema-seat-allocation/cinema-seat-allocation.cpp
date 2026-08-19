7b1ce84dd7290365e9750966de7dc12b870d0dcd
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-19 10:44:44 +0530

    Time: 546 ms (5.07%) | Memory: 98.7 MB (5.41%) - LeetSync

---
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        vector<vector<int>> seatBlocks;
+        seatBlocks.push_back({2,3,4,5});
+        seatBlocks.push_back({4,5,6,7});
+        seatBlocks.push_back({6,7,8,9});
+
+        unordered_map<int, unordered_set<int>> mp;
+
+        for(auto seats : reservedSeats){
+            int row = seats[0];
+            int col = seats[1];
+            mp[row].insert(col);
+        }
+
+        int ans = (n - mp.size()) * 2;
+        
+
+        for(auto &[row, col]: mp){
+            bool prev = false;
+            for(int i=0; i<3; i++){
+                bool isValid = true;
+                if(prev){
+                    prev = false;
+                    continue;
+                }
+                for(int j=0; j<4; j++){
+                    if(col.count(seatBlocks[i][j])){
+                        isValid = false;
+                        break;
+                    }
+                }
+                if(isValid){
+                    prev = true;
+                    ans++;
+                }
+                cout<<ans<<endl;
+
+            }
+
+        }
+
+        return ans;
+        
+    }
+};
