e0adc21b19fff563b940f4f977b67c0337852654
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-19 12:42:22 +0530

    Time: 35 ms (80.99%) | Memory: 69.6 MB (22.14%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
         int ans = 0;
-        priority_queue<int, vector<int>, greater<>> pq;
-        for(auto num : nums){
-            pq.push(num);
-        }
-
-        while(!pq.empty()){
-            int topEle = pq.top();
-            pq.pop();
-            if(pq.empty() || topEle != pq.top()) continue;
-            vector<int> currEq;
-            while(!pq.empty() && topEle == pq.top()){
-                currEq.push_back(topEle);
-                pq.pop();
-            }
-
-            // int counter = 1;
-            for(int i=0; i<currEq.size(); i++){
-                ans += i+1;
-                pq.push(currEq[i] + i+1);
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] <= nums[i-1]){
+                ans += (nums[i-1] - nums[i]) + 1;
+                nums[i] = nums[i-1] + 1;
             }
         }
 
