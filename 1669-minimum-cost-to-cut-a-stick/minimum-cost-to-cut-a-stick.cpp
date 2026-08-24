157ec4a170544504c41c04665b8daf209d1714d9
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-24 10:19:18 +0530

    Time: 39 ms (29.47%) | Memory: 14.1 MB (8.81%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,54 +1,38 @@
 class Solution {
 private:
-    int solve(int i, int j, vector<vector<int>> &dp, vector<int> &cuts){
-        if(i > j) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int minCost = INT_MAX;
-        for(int ind = i; ind <=j; ind++){
-            int currCost  = cuts[j+1] - cuts[i-1] 
-                            + solve(i, ind-1, dp, cuts)
-                            + solve(ind+1, j, dp, cuts);
-            
-            minCost = min(minCost, currCost);
+    int solve(int i, int j, vector<int> &cuts, vector<vector<int>> &dp) {
+        if(j-i<=1){
+            return 0;
         }
 
-        return dp[i][j] = minCost;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int result = INT_MAX;
+
+        for (int ind = i + 1; ind < j; ind++) {
+            result = min(
+                result,
+                cuts[j] - cuts[i]
+                + solve(i, ind, cuts, dp)
+                + solve(ind, j, cuts, dp)
+            );
+        }
+
+        return dp[i][j] = result;
     }
+
 public:
     int minCost(int n, vector<int>& cuts) {
         cuts.push_back(0);
         cuts.push_back(n);
+
         sort(cuts.begin(), cuts.end());
 
-        int len = cuts.size();
-        vector<vector<int>> dp(len, vector<int>(len));
+        int m = cuts.size();
 
-        // return solve(1, len-2, dp, cuts);
-        
-        // trying the tabulation
-        for(int i =0; i<len; i++){
-            for(int j =0; j<len; j++){
-                if(i > j) dp[i][j] =0;
-            }
-        }
+        vector<vector<int>> dp(m, vector<int>(m, -1));
 
-        for(int i = len-2; i>=1; i--){
-            for(int j = i; j<len-1; j++){
-                int minCost = INT_MAX;
-                for(int ind = i; ind <=j; ind++){
-                    int currCost  = cuts[j+1] - cuts[i-1] 
-                                    + dp[i][ind-1]
-                                    + dp[ind+1][j];
-                    
-                    minCost = min(minCost, currCost);
-                }
-
-                dp[i][j] = minCost;
-
-            }
-        }
-        return dp[1][len-2];
+        return solve(0, m - 1, cuts, dp);
     }
 };
