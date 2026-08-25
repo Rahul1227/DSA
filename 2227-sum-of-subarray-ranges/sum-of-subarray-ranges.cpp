9113e8ff695129b03ad59bf05984274b27e7756c
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-08-25 14:00:41 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (72.81%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,19 +1,85 @@
 class Solution {
-public:
-    long long subArrayRanges(vector<int>& nums) {
+private:
+    long long sumOfMax(vector<int> &arr) {
+        // code here
+        stack<int> st;
         long long ans = 0;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            int currMax = nums[i];
-            int currMin = nums[i];
-            for(int j=i+1; j<n; j++){
-                currMax = max(currMax, nums[j]);
-                currMin = min(currMin, nums[j]);
-                ans += 1LL * (currMax - currMin);
+        int n = arr.size();
+        
+        for(int i=0; i<=n; i++){
+            int currEle = i < n ? arr[i] : INT_MAX;
+            while(!st.empty() && currEle > arr[st.top()]){
+                int currInd = st.top();
+                st.pop();
+                int ngeInd = i;
+                int pgeInd = !st.empty() ? st.top() : -1;
+                int leftContri = currInd - pgeInd;
+                int rightContri = ngeInd - currInd;
+                ans += 1LL * leftContri * rightContri * arr[currInd];
             }
+            st.push(i);
+        }
+        
+        // while(!st.empty()){
+        //     int currInd = st.top();
+        //     st.pop();
+        //     int ngeInd = n;
+        //     int pgeInd = !st.empty() ? st.top() : -1;
+        //     int leftContri = currInd - pgeInd;
+        //     int rightContri = ngeInd - currInd;
+        //     ans += leftContri * rightContri * arr[currInd];
+        // }
+        
+        return ans;
+    
+        
+    }
+
+
+    long long sumOfMin(vector<int>& arr) {
+        // 2 pass solution is easy
+        // get pse
+        // get nse
+        // left count = i- pse(i)
+        // right count = i+ nse[i];
+        // sum += leftCount * rightCount * ar[i];
+
+        stack<int> st;
+        int n = arr.size();
+        long long totalSum = 0;
+        for(int i =0; i<=n; i++){
+            int currEle = (i==n) ? INT_MIN: arr[i];
+            while(!st.empty() && currEle < arr[st.top()]){
+                int currInd = st.top();
+                st.pop();
+                int pse = !st.empty() ? st.top() : -1;
+                int nse = i;
+                int leftCount = currInd - pse;
+                int rightCount = nse - currInd;
+                long long currSum = 1LL * arr[currInd] * leftCount * rightCount;
+                totalSum = 1LL * totalSum + currSum;
+            }
+            st.push(i);
         }
 
-        return ans;
+        // while(!st.empty()){
+        //     int currInd = st.top();
+        //     st.pop();
+        //     int pse = !st.empty() ? st.top() : -1;
+        //     int nse = n;
+        //     int leftCount = currInd - pse;
+        //     int rightCount = nse - currInd;
+        //     long long currSum = (1LL * arr[currInd] * leftCount * rightCount) % MOD;
+        //     totalSum = (1LL * totalSum + currSum) % MOD;
+
+        // }
+
+        return totalSum;
+        
+    }
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        return sumOfMax(nums) - sumOfMin(nums);
         
     }
 };
