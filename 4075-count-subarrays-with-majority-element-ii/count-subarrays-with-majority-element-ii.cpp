32981b308ebd524cf1f03c9b87607b566885cf0a
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-02 19:23:21 +0530

    Time: 291 ms (14.52%) | Memory: 160.1 MB (13.77%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
@@ -1,38 +1,38 @@
 class SegmentTree{
-public:
+private:
     vector<long long> seg;
-
+public:
     SegmentTree(int n){
-        seg.assign(4*n, 0);
-    }
-
-    void update(int i, int l, int r, int ind, int val){
-        if(l == r){
-            seg[i] += val;
-            return;
-        }
-
-        int mid = l + (r-l)/2;
-
-        if(ind <= mid){
-            update(2*i+1, l, mid, ind, val);
-        }else{
-            update(2*i+2, mid+1, r, ind,val);
-        }
-
-        seg[i] = 1LL * seg[2*i+1] + seg[2*i+2];
+        seg.resize(4*n, 0);
     }
 
     long long getVal(int i, int l, int r, int start, int end){
         if(r < start || l > end){
             return 0;
-        }else if(start <= l && r<= end){
+        }else if(start <= l && r <= end){
             return seg[i];
         }else{
             int mid = l + (r-l)/2;
-            return 1LL *getVal(2*i+1, l, mid, start, end) + getVal(2*i+2, mid+1, r, start, end);
+            return getVal(2*i+1, l, mid, start, end) + getVal(2*i+2,mid+1, r, start, end);
         }
     }
+
+    void update(int i, int l, int r, int targetInd){
+        if(l == r){
+            seg[i] = 1LL * seg[i]+ 1;
+            return;
+        }
+        int mid = l + (r-l)/2;
+        if(targetInd <= mid){
+            update(2*i+1, l, mid, targetInd);
+        }else{
+            update(2*i+2, mid+1, r, targetInd);
+        }
+
+        seg[i] = 1LL * seg[2*i+1] + seg[2*i+2];
+    }
+
+
 
 
 };
@@ -42,32 +42,32 @@ public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
         int n = nums.size();
         vector<int> prefixSum(n+1, 0);
-
-        // getting the prefixSum
         for(int i=0; i<n; i++){
-            prefixSum[i+1] = prefixSum[i] + (nums[i] == target ? 1: -1);
+            prefixSum[i+1] = prefixSum[i] + (nums[i] == target ? 1 : -1);
+
         }
 
         set<int> values(prefixSum.begin(), prefixSum.end());
-
-        unordered_map<int,int> mp;
         int rank =0;
-        for(auto x: values){
-            mp[x] = rank++;
+        unordered_map<int,int> mp;
 
+        for(auto it : values){
+            mp[it] = rank++;
         }
 
-        long long ans;
+        // we need to be careful that the set and segment tree don;t share the same name
         SegmentTree st(rank);
-
-        for(int i =0; i<=n; i++){
+        long long ans = 0;
+        for(int i=0; i<=n; i++){
             int currInd = mp[prefixSum[i]];
-            long long val = st.getVal(0,0,rank-1,0,currInd-1);
-            ans += val;
-            st.update(0,0,rank-1,currInd,1);
+            ans += st.getVal(0,0,rank,0,currInd-1);
+            st.update(0,0,rank,currInd);
         }
 
         return ans;
+
+
+
         
     }
 };
