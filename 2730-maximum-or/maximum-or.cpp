5b601e2afa2195ca3d0186f0152497ddffe05488
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-02 18:33:18 +0530

    Time: 4 ms (84.02%) | Memory: 100.1 MB (15.50%) - LeetSync

--- 2730-maximum-or/maximum-or.cpp
+++ 2730-maximum-or/maximum-or.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     long long maximumOr(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<long long> prefix(n+1,0);
-        vector<long long> suffix(n+1,0);
+        vector<long long> prefix(n+1);
+        vector<long long> suffix(n+1);
+        prefix[0] = 0;
+        suffix[n] = 0;
 
+        // calculating the prefix
         for(int i=0; i<n; i++){
-            prefix[i+1] = 1LL * (prefix[i] | nums[i]);
+            prefix[i+1] = 1LL * prefix[i] | nums[i];
         }
 
+        // calculating the suffix
         for(int i=n-1; i>=0; i--){
-            suffix[i] = 1LL * (suffix[i+1] | nums[i]);
+            suffix[i] = 1LL * suffix[i+1] | nums[i];
         }
-
-        long long maxSum = 0;
+        long long ans = 0;
         for(int i=0; i<n; i++){
-            long long currSum =1LL * (prefix[i] | suffix[i+1] | (1LL *nums[i]<<k));
-            maxSum = max(maxSum, currSum);
+            long long tempval = (1LL * nums[i]<<k);
+            long long currVal = 1LL * prefix[i] | tempval | suffix[i+1];
+            ans = max(ans, currVal);
         }
-        return maxSum;
+        return ans;
+        
     }
 };
