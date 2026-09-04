e35a43e358e61115d12db2f5ad753fc44dd3cc34
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-04 18:25:17 +0530

    Time: 4 ms (93.73%) | Memory: 9.2 MB (91.85%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,37 +1,30 @@
 class Solution {
 private:
     int getLen(int i, int j, string &s){
-        // int len = 1;
         int n = s.size();
-        while(i>= 0 && j < n && s[i] == s[j]){
-            i--;
-            j++;
+        while(i>=0 && j<n){
+            if(s[i] == s[j]){
+                i--;
+                j++;
+            }else{
+                return j-i-1;
+            }
         }
+
         return j-i-1;
     }
 public:
     string longestPalindrome(string s) {
-        int startInd = 0;
-        int maxLen = 1;
-        int n = s.size();
-
-        for(int i=0; i<n; i++){
-            int oddLen = getLen(i,i,s);
-
-            int evenLen = getLen(i,i+1, s);
-
-            int currLen = max(oddLen, evenLen);
-
+        int startPoint = -1;
+        int maxLen = 0;
+        for(int i =0; i<s.size(); i++){
+            int currLen = max(getLen(i,i,s), getLen(i,i+1,s));
             if(currLen > maxLen){
                 maxLen = currLen;
-                startInd =i-((currLen-1)/2);
+                startPoint = (i - (maxLen-1)/2);
             }
-
         }
-
-        // cout<<startInd<<"   "<<maxLen<<endl;
-
-        return s.substr(startInd, maxLen);
         
+        return s.substr(startPoint, maxLen);
     }
 };
