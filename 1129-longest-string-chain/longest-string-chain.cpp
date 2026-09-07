cc5b4e95be53bfd3896a1fec28e8eb86b70311b7
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-07 17:59:40 +0530

    Time: 169 ms (30.12%) | Memory: 105.9 MB (30.40%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,51 +1,62 @@
 class Solution {
 private:
-    static bool compare(const string &a, const string &b){
-        return a.size() < b.size();
+    static bool compare(string &s1, string &s2){
+        return s1.size() < s2.size();
     }
-
-    bool findEach(string longer, string tofind){
-        for(int i =0; i<longer.size(); i++){
-            string newS = longer.substr(0,i) + longer.substr(i+1, longer.size());
-            if(newS == tofind){
-                return true;
+    bool isPossible(string& s1, string& s2) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+        if (n2 != n1 + 1)
+            return false;
+        int k = 1;
+        int i = 0;
+        int j = 0;
+        while (i < n1) {
+            if (s1[i] == s2[j]) {
+                i++;
+                j++;
+            } else {
+                if (k) {
+                    k--;
+                    j++;
+                } else {
+                    return false;
+                }
             }
         }
-        return false;
-
+        return true;
     }
 
-
-
-    int solve(int ind, int prevInd, vector<vector<int>> &dp, vector<string> &words){
+    int solve(int ind, int prevInd, vector<vector<int>>& dp,
+              vector<string>& words) {
         // base case
-        if(ind == words.size()){
+        if (ind == words.size()) {
             return 0;
         }
 
-        if(dp[ind][prevInd+1] != -1) return dp[ind][prevInd+1];
+        if (dp[ind][prevInd + 1] != -1)
+            return dp[ind][prevInd + 1];
 
-        int notTake = solve(ind+1, prevInd, dp, words);
+        int notTake = solve(ind + 1, prevInd, dp, words);
 
         int take = 0;
-        
-        if(prevInd == -1 || 
-            (words[prevInd].size() + 1 == words[ind].size()
-                             && findEach(words[ind], words[prevInd]) )){
-            take = 1 + solve(ind+1, ind, dp, words);
+
+        if (prevInd == -1 || isPossible(words[prevInd], words[ind])) {
+            take = 1 + solve(ind + 1, ind, dp, words);
         }
 
-        return dp[ind][prevInd+1] = max(take, notTake);
+        return dp[ind][prevInd + 1] = max(take, notTake);
     }
+
 public:
     int longestStrChain(vector<string>& words) {
         sort(words.begin(), words.end(), compare);
 
-        for(auto &it : words){
-            cout<<it<<" ";
-        }
+        // for (auto& it : words) {
+        //     cout << it << " ";
+        // }
         int n = words.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
         return solve(0, -1, dp, words);
     }
 };
