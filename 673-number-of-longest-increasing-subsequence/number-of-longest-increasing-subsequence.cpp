3c2e8d12fb22ee13b6c91f0293d2f8dbb233b878
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-07 17:28:56 +0530

    Time: 31 ms (54.27%) | Memory: 17.5 MB (67.28%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -2,35 +2,40 @@ class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 1);
-        vector<int> count(n,1);
-        int lenLIS =1;
+        vector<pair<int, int>> dp(n, {1, 1});
+        int globalMaxLen = 0;
+        int globalMaxCount = 0;
 
-        for(int i =0; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(nums[j] < nums[i]){
-                    if(dp[j]+1 > dp[i]){
-                        dp[i] =  dp[j] + 1;
-                        count[i] = count[j];
-                    }else if(dp[j]+1 == dp[i]){
-                        count[i] += count[j];
+        for (int i = 0; i < n; i++) {
+            int maxLen = 1;
+            int maxCount = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    int currLen = dp[j].first;
+                    if (currLen + 1 > maxLen) {
+                        maxLen = currLen + 1;
+                        maxCount = dp[j].second;
+                    } else if (currLen + 1 == maxLen) {
+                        maxCount += dp[j].second;
                     }
                 }
             }
 
-            if(dp[i] > lenLIS){
-                lenLIS = dp[i];
+            dp[i] = {maxLen, maxCount};
+
+            if (maxLen > globalMaxLen) {
+                globalMaxLen = maxLen;
             }
         }
 
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            if(dp[i] == lenLIS){
-                ans += count[i];
+        // cout<<globalMaxLen<<end;
+
+        for (int i = 0; i < n; i++) {
+            if (dp[i].first == globalMaxLen) {
+                globalMaxCount += dp[i].second;
             }
         }
 
-        return ans;
-        
+        return globalMaxCount;
     }
 };
