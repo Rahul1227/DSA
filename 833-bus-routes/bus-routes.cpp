5e6b46b6559a38ca8ed63bde6b3985fbddc3f098
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-09 12:35:52 +0530

    Time: 444 ms (25.97%) | Memory: 59.5 MB (81.28%) - LeetSync

--- 833-bus-routes/bus-routes.cpp
+++ 833-bus-routes/bus-routes.cpp
@@ -1,62 +1,43 @@
 class Solution {
 public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-
-        if (source == target)
-            return 0;
-
-        // stop -> buses that visit this stop
+        if(source == target) return 0;
         unordered_map<int, vector<int>> busStopToBus;
-
-        for (int i = 0; i < routes.size(); i++) {
-            for (int j = 0; j < routes[i].size(); j++) {
+        
+        for(int i=0; i<routes.size(); i++){
+            for(int j=0; j<routes[i].size(); j++){
                 busStopToBus[routes[i][j]].push_back(i);
             }
         }
 
-        // visited buses
         unordered_set<int> visited;
-
+        
         queue<int> q;
-
-        // Start with every bus that contains source
-        for (int bus : busStopToBus[source]) {
-            q.push(bus);
-            visited.insert(bus);
+        for(auto v : busStopToBus[source]){
+            q.push(v);
+            visited.insert(v);
         }
 
         int ans = 1;
-
-        while (!q.empty()) {
-
+        while(!q.empty()){
             int len = q.size();
-
-            while (len--) {
-
-                int busInd = q.front();
+            for(int i=0; i<len; i++){
+                int bus = q.front();
                 q.pop();
-
-                // Explore every stop of this bus
-                for (int stop : routes[busInd]) {
-
-                    // We can reach target using this bus
-                    if (stop == target)
-                        return ans;
-
-                    // Find all buses that also visit this stop
-                    for (int nextBus : busStopToBus[stop]) {
-
-                        if (!visited.count(nextBus)) {
-                            visited.insert(nextBus);
-                            q.push(nextBus);
+                for(auto busStop : routes[bus]){
+                    if(busStop == target) return ans;
+                    for(auto v : busStopToBus[busStop]){
+                        if(!visited.count(v)){
+                            q.push(v);
+                            visited.insert(v);
                         }
                     }
                 }
             }
-
             ans++;
-        }
 
+        }
         return -1;
+        
     }
 };
