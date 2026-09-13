2daf4f5eba05f963d5ae46b1f3af489c48f23cd8
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-13 18:26:04 +0530

    Time: 31 ms (43.59%) | Memory: 18.5 MB (76.14%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,75 +1,79 @@
 class DisjointSet{
-    private:
-        vector<int> parent;
-        vector<int> size;
-    
-    public:
-        DisjointSet(int n){
-            parent.resize(n);
-            size.resize(n,1);
+private:   
+    vector<int> parent;
+    vector<int> size;
+public:
+    DisjointSet(int n){
+        parent.resize(n);
+        size.resize(n, 1);
+        for(int i=0; i<n; i++){
+            parent[i] = i;
+        }
+    }
 
-            for(int i =0; i<n; i++){
-                parent[i] = i;
-
-            }
+    int findUltimateParent(int u){
+        if(parent[u] == u){
+            return u;
         }
 
-        int findUltimateParent(int u){
-            if(parent[u] == u){
-                return u;
-            }
+        return parent[u] = findUltimateParent(parent[u]);
+    }
 
-            return parent[u] = findUltimateParent(parent[u]);
+
+    void unionBySize(int u, int v){
+        int parentU = findUltimateParent(u);
+        int parentV = findUltimateParent(v);
+
+        if(parentU == parentV) return;
+
+        if(size[parentU] > size[parentV]){
+            parent[parentV] = parentU;
+            size[parentU] += size[parentV];
+        }else{
+            parent[parentU] = parentV;
+            size[parentV] += size[parentU];
         }
+    }
 
-        void unionBySize(int u, int v){
-            int parentU = findUltimateParent(u);
-            int parentV = findUltimateParent(v);
 
-            if(parentU == parentV) return;
-
-            if(size[parentU] >= size[parentV]){
-                parent[parentV] = parentU;
-                size[parentU] += size[parentV];
-            }else{
-                parent[parentU] = parentV;
-                size[parentV] += size[parentU];
-            }
+    vector<int> getParent(){
+        for(int i =0; i<parent.size(); i++){
+            parent[i] = findUltimateParent(i);
         }
-
-        int getResult(){
-            int count = 0;
-            for(int i =0; i< parent.size(); i++){
-                if(parent[i] == i){
-                    count++;
-                }
-            }
-
-            return count;
-        }
-
+        return parent;
+    }
 };
+
+
 class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
+
         DisjointSet ds(n);
 
-        for(int i =0; i<n; i++){
-            int row = stones[i][0];
-            int col = stones[i][1];
+        for(int i=0; i<n; i++){
+            int currRow = stones[i][0];
+            int currCol = stones[i][1];
 
             for(int j=i+1; j<n; j++){
-                int currRow = stones[j][0];
-                int currCol = stones[j][1];
+                int nextRow = stones[j][0];
+                int nextCol = stones[j][1];
 
-                if(currRow == row || currCol == col){
+                if(currRow == nextRow || currCol == nextCol){
                     ds.unionBySize(i,j);
                 }
             }
         }
 
-        int count = ds.getResult();
+        vector<int> par = ds.getParent();
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(par[i] == i){
+                count++;
+            }
+        }
+
         return n - count;
         
     }
