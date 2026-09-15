c29f68ab2b04f0cc1aeaae360d309d0f876830e3
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-15 18:35:52 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (47.18%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int newStart = newInterval[0];
-        int newEnd = newInterval[1];
         vector<vector<int>> ans;
-        bool inserted = false;
-        for(auto interval : intervals){
-            int currStart = interval[0];
-            int currEnd = interval[1];
-            if(inserted){
-                ans.push_back(interval);
+        int targetStart = newInterval[0];
+        int targetEnd = newInterval[1];
+        bool placed = false;
+
+        for(int i =0; i<intervals.size(); i++){
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
+            if(placed || currEnd < targetStart){
+                ans.push_back({currStart, currEnd});
             }else{
-                if(currEnd < newStart){
-                    ans.push_back(interval);
-                }else if(newEnd < currStart){
-                    if(!inserted){
-                        inserted = true;
-                        ans.push_back({newStart, newEnd});
-                    }
-                    ans.push_back(interval);
+                if(currEnd >= targetStart && currStart <= targetEnd){
+                    targetStart = min(targetStart, currStart);
+                    targetEnd = max(targetEnd, currEnd);
+
                 }else{
-                    newStart = min(newStart, currStart);
-                    newEnd = max(currEnd, newEnd);
+                    ans.push_back({targetStart, targetEnd});
+                    placed = true;
+                    ans.push_back({currStart, currEnd});
                 }
             }
-
-
-
         }
-
-        if(!inserted){
-            ans.push_back({newStart, newEnd});
+        if(!placed){
+            ans.push_back({targetStart, targetEnd});
         }
 
         return ans;
