14f791b5df687d418c4270e22437b9ac1168d5fc
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-15 17:29:00 +0530

    Time: 7 ms (39.64%) | Memory: 23.8 MB (82.88%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        int i =0;
-        int j = i+1;
-        // int startTime = intervals[i][0];
-        // int endTime = intervals[i][1];
+        sort(intervals.begin(), intervals.end());
+        int i = 0;
+        int j = 1;
+        int n = intervals.size();
+        while (i < n) {
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
 
-        while(i<n){
-            int startTime = intervals[i][0];
-            int endTime = intervals[i][1];
-            while(j<n && intervals[j][0] <= endTime){
-                endTime = max(endTime, intervals[j][1]);
+            while (j < n && currEnd >= intervals[j][0]) {
+                currEnd = max(currEnd, intervals[j][1]);
                 j++;
             }
-            ans.push_back({startTime, endTime});
+            ans.push_back({currStart, currEnd});
             i = j;
-            j++;
-
         }
 
+        // cout<<"printing something"<<endl;
         return ans;
-        
     }
 };
