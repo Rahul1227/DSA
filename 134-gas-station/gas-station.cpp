bfec5dc4964280fc81ef58953aa11375ae13692b
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-15 19:00:55 +0530

    Time: 0 ms (100.00%) | Memory: 112.7 MB (13.01%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int ans = 0;
-        int totalfuel = 0;
-        int totalgas = accumulate(gas.begin(), gas.end(), 0);
-        int totalcost = accumulate(cost.begin(), cost.end(), 0);
-        if(totalcost > totalgas) return -1;
-        for(int i =0; i< gas.size()-1; i++){
-            totalfuel += gas[i];
-            totalfuel -= cost[i];           
-            if(totalfuel < 0){
-                ans = i+1;
-                totalfuel = 0;
-            } 
-           
-            
+        int gasSum = accumulate(gas.begin(), gas.end(), 0);
+        int costSum = accumulate(cost.begin(), cost.end(), 0);
+        if(gasSum < costSum) return -1;
+
+        int startInd = 0;
+        int totalGas = 0;
+
+        for(int i =0; i< gas.size(); i++){
+            totalGas += gas[i];
+            if(totalGas < cost[i]){
+                totalGas = 0;
+                startInd = i+1;
+            }else{
+                totalGas -= cost[i];
+            }
         }
 
-        return ans;
-        
+        return startInd;
     }
 };
