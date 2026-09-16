e3a6635289a4828a4b3888cbf34af33a8ffe78e6
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-16 21:19:33 +0530

    Time: 3 ms (17.02%) | Memory: 35.8 MB (18.01%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,31 +1,41 @@
 class Solution {
 private:
-    bool solve(int ind, vector<int> &arr, vector<int> &visited){
+    bool solve(int ind, vector<int> &arr, vector<pair<int,int>> &ranges, vector<int> &visited){
         int n = arr.size();
+        if(ind < 0 || ind >= n) return false;
         if(arr[ind] == 0) return true;
 
-        int left = ind - arr[ind];
-        int right = ind + arr[ind];
+        // if(dp[ind] != -1) return dp[ind];
 
-        bool forward = false;
-        bool backward = false;
-        if(left >=0 && !visited[left]){
-            visited[left] = true;
-            backward = solve(left, arr, visited);
+        bool left = false;
+        if(ranges[ind].first >= 0 && !visited[ranges[ind].first]){
+            visited[ranges[ind].first] = 1;
+            left = solve(ranges[ind].first, arr, ranges, visited);
+            
+           
+        }
+        bool right = false;
+        if(ranges[ind].second <n && !visited[ranges[ind].second]){
+            visited[ranges[ind].second] = 1;
+            right = solve(ranges[ind].second, arr, ranges, visited);
+           
+           
         }
 
-        if(right < n && !visited[right]){
-            visited[right] = true;
-            forward = solve(right, arr, visited);
-        }
-
-        return (backward || forward);
-    }
+        return left || right;
+}
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
-        vector<int> visited(n, 0);
-        visited[start] = 1;
-        return solve(start, arr, visited);
+        vector<pair<int,int>> ranges;
+        for(int i=0; i<n; i++){
+            int left = i - arr[i];
+            int right = i+ arr[i];
+            ranges.push_back({left, right});
+        }
+        vector<int> dp(n, 0);
+
+        return solve(start, arr, ranges, dp);
+        
     }
 };
