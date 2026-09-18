68f72302cbfb53e35eec11aa26d93a677dc16eab
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-18 12:55:06 +0530

    Time: 963 ms (5.53%) | Memory: 15.8 MB (26.61%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,33 +1,37 @@
 class Solution {
 private:
-    vector<int> getLPS(string &s){
+    int getMatch(string &s){
         int n = s.size();
         vector<int> lps(n, 0);
-        int len = 0;
         int i =1;
+        int len = 0;
         while(i<n){
             if(s[i] == s[len]){
                 len++;
                 lps[i] = len;
                 i++;
-            }else if(len != 0){
+            }else if(len !=  0){
                 len = lps[len-1];
             }else{
                 i++;
             }
         }
-        return lps;
+        for(auto num: lps){
+            cout<<num<<" ";
+        }
+        return lps[n-1];
     }
 public:
     string shortestPalindrome(string s) {
-        string original = s;
-        reverse(s.begin(), s.end());
-        string str = original + '$' + s;
-        vector<int> lps = getLPS(str);
-        int n = str.size();
-        string result = s.substr(0, original.size() - lps[n -1]);
-        return result + original;
-        
+        int n = s.size();
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        string newStr = s + '$' + rev;
+        int matching = getMatch(newStr);
+        // cout<<matching<<endl;
+
+        string firstHalf = rev.substr(0, n-matching);
+        return firstHalf + s;
         
     }
 };
