6b706f2dfade0e7d8559466fc41c82a2f9324d1d
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-21 19:10:03 +0530

    Time: 2 ms (3.84%) | Memory: 8.3 MB (10.53%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.cpp
+++ 233-number-of-digit-one/number-of-digit-one.cpp
@@ -1,28 +1,25 @@
 class Solution {
-private:    
-    int solve(string &s, int ind, int tight, int count, int dp[11][2][11]){
-        if(ind == s.size()) return count;
-
-        if(dp[ind][tight][count] != -1) return dp[ind][tight][count];
-
-        int limit = (tight ? s[ind] - '0' : 9);
-        int ans = 0;
-
-        for(int i =0; i<=limit; i++){
-            int updatedCount = count + (i == 1);
-            ans += solve(s, ind+1, (tight & (i == s[ind] - '0')), updatedCount, dp);
-        }
-
-        return dp[ind][tight][count] = ans;
-    }
 public:
+    int dp[15][2][15];
+
+    int solve(string num, int ind, bool tight, int count){
+        int n = num.size();
+        if(ind == n){
+            return count;
+        }
+        if(dp[ind][tight][count] != -1) return dp[ind][tight][count];
+        int limit = (tight ? num[ind] -'0' : 9);
+        int result = 0;
+        for(int i =0; i<= limit; i++){
+            int newTight = tight & (i == num[ind] -'0');
+            int updatedCount = count +  (i == 1);
+            result += solve(num, ind+1, newTight, updatedCount);
+        }
+        return dp[ind][tight][count] = result;
+    }
     int countDigitOne(int n) {
-        int dp[11][2][11];
-        memset(dp,  -1,  sizeof(dp));
         string num = to_string(n);
-        return solve(num, 0,1,0, dp);
-
-
-        
+        memset(dp,-1, sizeof(dp));
+        return solve(num, 0, true, 0);
     }
 };
