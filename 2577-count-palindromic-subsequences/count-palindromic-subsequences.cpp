1dead863a71f753023d95bdbcddd744c344f1310
RAHUL YADAV <95612712+Rahul1227@users.noreply.github.com>
2026-09-23 11:40:19 +0530

    Time: 120 ms (49.86%) | Memory: 41.6 MB (45.04%) - LeetSync

--- 2577-count-palindromic-subsequences/count-palindromic-subsequences.cpp
+++ 2577-count-palindromic-subsequences/count-palindromic-subsequences.cpp
@@ -1,46 +1,49 @@
-#define ll long long
-ll dp[10001][11][11][6];
+const int MOD = 1e9+7;
 class Solution {
-public:
-    ll mod;
-    ll dfs(int ind, int first, int second, int i, string &s) {
-        if(i == 5) return 1; //found a subsequence
-        if(ind == s.length()) return 0;
-        if(dp[ind][first][second][i] != -1) return dp[ind][first][second][i];
-        
-        //option of not choosing current digit
-        ll res = dfs(ind + 1, first, second, i, s);
+private:
+    int dp[10004][11][11][6];
 
-        if(i == 0) {
-            //option of choosing the first digit of the subsequence
-            res += dfs(ind + 1, s[ind] - '0', second, i + 1, s);
-            res %= mod;
-        } else if(i == 1) {
-            //option of choosing the second digit of the subsequence
-            res += dfs(ind + 1, first, s[ind] - '0', i + 1, s);
-            res %= mod;
-        } else if(i == 2) {
-            //option of choosing the third digit of the subsequence
-            res += dfs(ind + 1, first, second, i + 1, s);
-            res %= mod;
-        } else if(i == 3) {
-            //option of choosing the fourth digit of the subsequence if it matches with the second digit
-            if(s[ind] - '0' == second) {
-                res += dfs(ind + 1, first, second, i + 1, s);
-                res %= mod;
-            }
-        } else if(i == 4) {
-            //option of choosing the fifth digit of the subsequence if it matches with the first digit
-            if(s[ind] - '0' == first) {
-                res += dfs(ind + 1, first, second, i + 1, s);
-                res %= mod;
-            }
-        }
-        return dp[ind][first][second][i] = res;
+
+    int solve(int ind, int first, int second, int pos, string &s){
+        if(pos == 5) return 1;
+        int n = s.size();
+        if(ind == n) return 0;
+
+        if(dp[ind][first][second][pos] != -1) return dp[ind][first][second][pos];
+
+        int result = 0;
+        // skipping the current eleemnt
+        result = (result + (solve(ind+1, first, second, pos, s)) % MOD) % MOD;
+        
+        
+        // taking the first element
+        if(pos == 0)
+            result = (result + (solve(ind+1, s[ind] -'0', second, pos+1, s)) % MOD) % MOD;
+
+        // taking the second element
+        if(pos == 1)
+            result = (result + (solve(ind+1, first, s[ind] -'0', pos + 1, s)) % MOD) % MOD;
+
+        // taking the third element
+        if(pos == 2)
+            result = (result + (solve(ind+1, first, second, pos + 1, s)) % MOD) % MOD;
+        
+        // taking the fourth element
+        if(pos == 3 && s[ind] -'0' == second)
+            result = (result + (solve(ind+1, first, second, pos + 1, s)) % MOD) % MOD;
+
+        // taking the fifth element
+        if(pos == 4 && s[ind] -'0' == first)
+            result = (result + (solve(ind+1, first, second, pos + 1, s)) % MOD) % MOD;
+
+
+        return dp[ind][first][second][pos] = result;
     }
+
+public:
     int countPalindromes(string s) {
-        memset(dp, -1, sizeof dp); 
-        mod = 1e9 + 7;
-        return dfs(0, 10, 10, 0, s);
+        memset(dp, -1, sizeof(dp));
+        return solve(0,0,0,0,s);
+        
     }
 };
